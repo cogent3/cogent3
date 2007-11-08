80c7743b466b5c8da20880714dfb0f1075d16a3b
Gavin Huttley <gavin.huttley@anu.edu.au>
2007-11-08 02:52:05 +0000

    [NEW] added matrix log by taylor expansion.

--- tests/test_maths/test_matrix_logarithm.py
+++ tests/test_maths/test_matrix_logarithm.py
@@ -2,11 +2,11 @@
 """Unit tests for matrix logarithm."""
 from numpy import array
 from cogent.util.unit_test import TestCase, main
-from cogent.maths.matrix_logarithm import logm
+from cogent.maths.matrix_logarithm import logm, logm_taylor
 
 __author__ = "Rob Knight"
 __copyright__ = "Copyright 2007, The Cogent Project"
-__credits__ = ["Rob Knight"]
+__credits__ = ["Rob Knight", "Gavin Huttley"]
 __license__ = "GPL"
 __version__ = "1.0.1"
 __maintainer__ = "Rob Knight"
@@ -28,6 +28,19 @@ class logarithm_tests(TestCase):
        [ 0.01405019, -0.07652296,  0.02252941,  0.03994336],
        [ 0.05365208,  0.15569116, -0.22588966,  0.01654642],
        [ 0.35144866,  0.31279003,  0.10478999, -0.76902868]]))
+    
+    def test_logm_taylor(self):
+        """logm_taylor should return same result as logm"""
+        q_eig = logm([[ 0.86758487,  0.05575623,  0.0196798 ,  0.0569791 ],
+                       [ 0.01827347,  0.93312148,  0.02109664,  0.02750842],
+                       [ 0.04782582,  0.1375742 ,  0.80046869,  0.01413129],
+                       [ 0.23022035,  0.22306947,  0.06995306,  0.47675713]])
+        q_taylor = logm_taylor([[0.86758487, 0.05575623, 0.0196798, 0.0569791],
+                  [ 0.01827347,  0.93312148,  0.02109664,  0.02750842],
+                  [ 0.04782582,  0.1375742 ,  0.80046869,  0.01413129],
+                  [ 0.23022035,  0.22306947,  0.06995306,  0.47675713]])
+        self.assertFloatEqual(q_taylor, q_eig)
+    
 
 if __name__ == '__main__':
     main()
