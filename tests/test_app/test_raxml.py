931e093fa4d18ecdb5f5fe202b64f96bb7d5733c
Micah Hamady <hamady@colorado.edu>
2007-11-21 21:03:11 +0000

    Updated tests to actualy work. Again, this was supposed to be task for Mike Robeson

--- tests/test_app/test_raxml.py
+++ tests/test_app/test_raxml.py
@@ -4,6 +4,7 @@ from os import getcwd, remove, rmdir, mkdir
 from cogent.util.unit_test import TestCase, main
 from cogent.util.misc import flatten
 from cogent.app.raxml import Raxml,raxml_alignment
+from cogent.app.util import ApplicationError
 from cogent.parse.phylip import get_align_for_phylip
 from StringIO import StringIO
 
@@ -24,24 +25,20 @@ class GenericRaxml(TestCase):
             'GCGGCUAUUAGAUCGUA']
         self.labels1 = ['>1','>2','>3']
         self.lines1 = flatten(zip(self.labels1,self.seqs1))
-        #self.dnd1 = DND1
 
-        try:
-            mkdir('/tmp/ct')
-        except OSError: #dir already exists
-            pass
-
-        try:
-            #create sequence files
-            f = open('/tmp/ct/seq1.txt','w')
-            f.write('\n'.join(self.lines1))
-            f.close()
-
-        except OSError:
-            pass
+        self.test_model = "GTRCAT"
 
         self.align1 = get_align_for_phylip(StringIO(PHYLIP_FILE))
 
+        self.test_fn1 = "/tmp/raxml_test1.txt"
+        self.test_fn2 = "raxml_test1.txt"
+        self.test_fn1_space = "/tmp/raxml test1.txt"
+
+    def writeTmp(self, outname):
+        """Write data to temp file"""
+        t = open(outname, "w+")
+        t.write(PHYLIP_FILE)
+        t.close()
 
 
 class RaxmlTests(GenericRaxml):
@@ -51,35 +48,88 @@ class RaxmlTests(GenericRaxml):
         """raxml BaseCommand should return the correct BaseCommand"""
         r = Raxml()
         self.assertEqual(r.BaseCommand, \
-            ''.join(['cd ',getcwd(),'/; ','raxmlHPC -e 0.1 -f d -c 50']))
+            ''.join(['cd \"',getcwd(),'/\"; ','raxmlHPC -e 0.1 -f d -c 50']))
         r.Parameters['-s'].on('seq.nexus')
         self.assertEqual(r.BaseCommand,\
-            ''.join(['cd ',getcwd(),'/; ',\
+            ''.join(['cd \"',getcwd(),'/\"; ',\
             'raxmlHPC -e 0.1 -f d -c 50 -s seq.nexus']))
 
-   
-class raxmlTests(GenericRaxml):
-    """Tests for module level functions in raxml.py"""
-   
+
+    def test_raxml_params(self):
+        """raxml should raise exception if missing required params"""
+
+        r = Raxml(WorkingDir="/tmp")
+
+        r.SuppressStdout = True
+        r.SuppressStderr = True
+        # raise error by default
+        self.assertRaises(ValueError, r)
+
+        # specify output name 
+        r.Parameters['-n'].on("test_name")
+        self.assertRaises(ApplicationError, r)
+
+        # specify model 
+        r.Parameters['-m'].on("GTRCAT")
+        self.assertRaises(ApplicationError, r)
+
+        r.Parameters['-s'].on(self.test_fn1)
+        self.assertRaises(ApplicationError, r)
+
+
+        self.writeTmp(self.test_fn1)
+
+        o = r()
+        o.cleanUp()
+
+        remove(self.test_fn1)
+
+    
+    def test_raxml_from_file(self):
+        """raxml should run correctly using filename"""
+        r = Raxml(WorkingDir="/tmp")
+
+        r.Parameters['-s'].on(self.test_fn1)
+        r.Parameters['-m'].on("GTRCAT")
+        r.Parameters['-n'].on("test_me")
        
+        # test with abs filename
+        cur_out = self.test_fn1
+        self.writeTmp(cur_out)
+        out = r()
+        out.cleanUp()
+        remove(cur_out)
+
+        # test with rel + working dir 
+        r.Parameters['-s'].on(self.test_fn2)
+        r.Parameters['-n'].on("test_me2")
+        r.Parameters['-w'].on("/tmp/")
+        self.writeTmp(self.test_fn1)
+        out = r()
+        out.cleanUp()
+        remove(self.test_fn1)
+
+        r.Parameters['-s'].on("\"%s\"" % self.test_fn1_space)
+        r.Parameters['-n'].on("test_me3")
+        r.Parameters['-w'].on("/tmp/")
+        #print r.BaseCommand
+        self.writeTmp(self.test_fn1_space)
+        out = r()
+        out.cleanUp()
+        remove(self.test_fn1_space)
+
     def test_raxml_alignment(self):
         """raxml_alignment should work as expected"""
         phy_node, parsimony_phy_node, log_likelihood, total_exec \
             = raxml_alignment(self.align1)
 
-        #note: we get different results on different runs so can't test this
-        #formally, but the above shouldn't raise an error
-        #
-        #print phy_node
-        #print parsimony_phy_node
-        #print log_likelihood
-        #print total_exec
-        
+
+   
 PHYLIP_FILE= """ 7 50
 Species001   UGCAUGUCAG UAUAGCUUUA GUGAAACUGC GAAUGGCUCA UUAAAUCAGU
 Species002   UGCAUGUCAG UAUAGCUUUA GUGAAACUGC GAAUGGCUNN UUAAAUCAGU
 Species003   UGCAUGUCAG UAUAGCAUUA GUGAAACUGC GAAUGGCUCA UUAAAUCAGU
-Species004   UGCAUGUCAG UAUAACUUUG GUGAAACUGC GAAUGGCUCA UUAAAUCAGU
+Species004   UCCAUGUCAG UAUAACUUUG GUGAAACUGC GAAUGGCUCA UUAAAUCAGG
 Species005   NNNNNNNNNN UAUAUCUUAU GUGAAACUUC GAAUGCCUCA UUAAAUCAGU
 Species006   UGCAUGUCAG UAUAGCUUUG GUGAAACUGC GAAUGGCUCA UUAAAUCAGU
 Species007   UGCAUGUCAG UAUAACUUUG GUGAAACUGC GAAUGGCUCA UUAAAUCAGU
