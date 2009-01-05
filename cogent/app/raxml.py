c76c31483b4d5fab486eb2ae48b2399ba607143e
Gavin Huttley <gavin.huttley@anu.edu.au>
2009-01-05 01:25:57 +0000

    updated __versionn__ to 1.3.0.dev
    [CHANGED] also, check on cogent version number now only looks at integers
              after splitting on '.'

--- cogent/app/raxml.py
+++ cogent/app/raxml.py
@@ -18,7 +18,7 @@ __copyright__ = "Copyright 2007-2008, The Cogent Project"
 __credits__ = ["Micah Hamady", "Catherine Lozupone", "Rob Knight", \
                "Daniel McDonald"]
 __license__ = "GPL"
-__version__ = "1.2"
+__version__ = "1.3.0.dev"
 __maintainer__ = "Micah Hamady"
 __email__ = "hamady@colorado.edu"
 __status__ = "Prototype"
--- cogent/core/sequence.py
+++ cogent/core/sequence.py
@@ -35,7 +35,7 @@ __copyright__ = "Copyright 2007-2008, The Cogent Project"
 __credits__ = ["Rob Knight", "Peter Maxwell", "Gavin Huttley",
                     "Matthew Wakefield"]
 __license__ = "GPL"
-__version__ = "1.2"
+__version__ = "1.3.0.dev"
 __maintainer__ = "Rob Knight"
 __email__ = "rob@spot.colorado.edu"
 __status__ = "Production"
