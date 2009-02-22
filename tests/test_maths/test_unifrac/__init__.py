256b43209f554e77242406d2358ac57de635a1db
Micah Hamady <hamady@colorado.edu>
2009-02-22 01:39:01 +0000

    Adding fast unifrac prototype

---
+++ tests/test_maths/test_unifrac/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+__all__ = ['test_fast_tree',
+           'test_fast_unifrac'
+           ] 
+__author__ = ""
+__copyright__ = "Copyright 2007-2009, The Cogent Project"
+__credits__ = ["Rob Knight", "Micah Hamady"]
+__license__ = "All rights reserveed"
+__version__ = "0.1"
+__maintainer__ = "Micah Hamady"
+__email__ = "hamady@colorado.edu"
+__status__ = "Prototype"
