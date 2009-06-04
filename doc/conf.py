f9c6f4278c274ef27639c1d7e40ba42a711598a2
Gavin Huttley <gavin.huttley@anu.edu.au>
2009-06-04 18:35:09 +0000

    set pngmath preference in Sphinx conf.py file
    [NEW] allows using the math directive for inclusion in html docs

--- doc/conf.py
+++ doc/conf.py
@@ -22,7 +22,9 @@ import sys, os
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.todo', 'sphinx.ext.doctest']
+extensions = ['sphinx.ext.todo', 'sphinx.ext.doctest', 'sphinx.ext.pngmath']
+
+# todo_include_todos=True # to expose the TODOs, uncomment this line
 
 # Add any paths that contain templates here, relative to this directory.
 # templates_path = ['_templates']
