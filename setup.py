9fbaca350bf34f8064130eaace2161042cacdc3b
Daniel McDonald <mcdonadt@colorado.edu>
2009-06-17 00:05:47 +0000

    Missed updating version tag

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ __copyright__ = "Copyright 2007-2009, The Cogent Project"
 __contributors__ = ["Peter Maxwell", "Gavin Huttley", "Matthew Wakefield", 
                     "Greg Caporaso"]
 __license__ = "GPL"
-__version__ = "1.3.0.dev"
+__version__ = "1.4.0.dev"
 __maintainer__ = "Peter Maxwell"
 __email__ = "pm67nz@gmail.com"
 __status__ = "Production"
