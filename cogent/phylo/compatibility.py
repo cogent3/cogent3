59099aab26706792bd089cacfa8f73d3ceddc4d6
Peter Maxwell <pm67nz@gmail.com>
2009-07-01 00:58:30 +0000

    Improvements to partimatrix partition ordering in compatibility.py.  - Ignore incomplete alignment columns when scoring partitions.  - Use similarity based partition ordering to break ties in conflict score based ordering.  - Use similarity measure S as in JWE1997, not just observed similarity.  - min_edges() between a partition and itself is 1, not 0.

--- cogent/phylo/compatibility.py
+++ cogent/phylo/compatibility.py
@@ -8,14 +8,24 @@ import numpy
 import numpy.random
 import operator
 
-def order_to_cluster_similar(S):
+def order_to_cluster_similar(S, elts=None, start=None):
     """Order so as to keep the most similar parts adjacent to each other
     S is expected to be a square matrix, and the returned list of 
     ordinals is len(S) long."""
-    similarity = [numpy.unravel_index(p, S.shape) 
-                for p in numpy.argsort(S, axis=None)]
     position = {}
     unavailable = set()
+    if elts is not None:
+        if len(elts) < 2:
+            return elts
+        elts = set(elts)
+        for x in range(len(S)):
+            if x is not start and x not in elts:
+                unavailable.add(x)
+    if start is not None:
+        position[start] = (False, [start])
+                
+    similarity = [numpy.unravel_index(p, S.shape) 
+                for p in numpy.argsort(S, axis=None)]
     for (x, y) in similarity[::-1]:
         if x==y or x in unavailable or y in unavailable:
             continue
@@ -34,8 +44,27 @@ def order_to_cluster_similar(S):
         run = x_run + y_run
         position[run[0]] = (False, run)
         position[run[-1]] = (True, run)
-    assert len(run) == len(S)
+    if start is not None:
+        if run[-1] == start:
+            run.reverse()
+        run = run[1:]
     return run
+
+def order_tied_to_cluster_similar(S, scores):
+    """Use similarity measure S to make similar elements
+    adjacent, but only to break ties in the primary order
+    defined by the list of scores"""
+    assert S.shape == (len(scores), len(scores))
+    (pos,) = numpy.where(numpy.diff(scores) != 0)
+    pos = numpy.concatenate(([0],pos+1,[len(scores)]))
+    new_order = []
+    start = None
+    for (a,b) in zip(pos[:-1],pos[1:]):
+        sub_order = order_to_cluster_similar(S, range(a,b), start)
+        new_order.extend(sub_order)
+        start = new_order[-1]
+    assert set(new_order) == set(range(len(scores))) 
+    return new_order
 
 def bit_encode(x, _bool2num=numpy.array(["0","1"]).take):
     """Convert a boolean array into an integer"""
@@ -81,7 +110,7 @@ def min_edges(columns):
     If N of these 4 possibilities are found then there must be at least 
     N-1 tree edges on which mutations occured"""  
     N = len(columns)
-    result = numpy.zeros([N, N], int)
+    result = numpy.ones([N, N], int)
     for i in range(0, N-1):
         (a, mask_a) = columns[i]
         for j in range(i+1, N):
@@ -89,7 +118,8 @@ def min_edges(columns):
             mask = mask_a & mask_b
             (na, nb) = (~a, ~b)
             combos = [c & mask for c in [a&b, a&nb, na&b, na&nb]]
-            result[i,j] = result[j,i] = len([c for c in combos if c]) - 1
+            combos = [c for c in combos if c]
+            result[i,j] = result[j,i] = len(combos) - 1
     return result
 
 def neighbour_similarity_score(matrix):
@@ -154,27 +184,44 @@ def main(alignment, display=False, samples=0):
     mask = 2**alignment.getNumSeqs()-1
     complete = [i for (i,(x, xz)) in enumerate(partitions) if xz==mask]
     partimatrix = partimatrix[:,complete]
+    # For scoring/ordering purposes, also remove the incomplete sequences
+    complete_columns = [i for (i,c) in enumerate(columns) if c in complete]
+    scoreable_partimatrix = partimatrix[complete_columns, :]
     
-    # Order partitions for visual grouping
-    support = (partimatrix == 0).sum(axis=0)
-    consist = (partimatrix <= 2).sum(axis=0)
-    conflict = (partimatrix > 2).sum(axis=0)
-    if "advanced ordering":
-        pp1 = (partimatrix <= 2).astype(int).T
-        npp = (partimatrix > 2).astype(int).T
-        O = numpy.inner(pp1, pp1) + numpy.inner(npp, npp)
-        order = order_to_cluster_similar(O)
+    # Order partitions by increasing conflict score
+    conflict = (scoreable_partimatrix > 2).sum(axis=0)
+    conflict_order = numpy.argsort(conflict)
+    partimatrix = partimatrix[:, conflict_order]
+    scoreable_partimatrix = partimatrix[complete_columns, :]
+    support = (scoreable_partimatrix == 1).sum(axis=0)
+    consist = (scoreable_partimatrix <= 2).sum(axis=0)
+    conflict = (scoreable_partimatrix > 2).sum(axis=0)
+    
+    # Similarity measure between partitions
+    pp1 = (scoreable_partimatrix <= 2).astype(int).T
+    npp = (scoreable_partimatrix > 2).astype(int).T
+    O = numpy.inner(pp1, pp1) + numpy.inner(npp, npp)
+    s = 1.0*len(complete_columns)
+    O = O.astype(float) / s
+    p,q = consist/s, conflict/s
+    E = numpy.outer(p,p) + numpy.outer(q,q)
+    S = (O-E)/numpy.sqrt(E*(1-E)/s)
+    
+    # Order partitions for better visual grouping
+    if "order_by_conflict":
+        order = order_tied_to_cluster_similar(S, conflict)
+    else:
+        order = order_to_cluster_similar(S)
         half = len(order) // 2
         if sum(conflict[order[:half]]) > sum(conflict[order[half:]]):
             order.reverse()
-    else:
-        order = numpy.argsort(conflict)
+    
     partimatrix = partimatrix[:, order]
             
     if display:
         import pylab
         # 3-colour code: support, compatible, conflict
-        partishow = numpy.array([5,4,4,0]).take(partimatrix)
+        partishow = numpy.array([5,4,0]).take(partimatrix-1)
         pylab.matshow(partishow, cmap=pylab.cm.gray)
         pylab.show()
     
