963179aee8886b6653b31e7ef739e8d704a35efd
Kyle Bittinger <kylebittinger@gmail.com>
2009-08-07 22:29:20 +0000

    Changed cogent.app.RdpClassifier to search for jar file in /usr/local/app/rdp_classifier/rdp_classifier-2.0.jar

--- tests/test_app/test_rdp_classifier.py
+++ tests/test_app/test_rdp_classifier.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 """Tests for the rdp_classifier_2.0.1 application controller"""
 
-from os import getcwd
+from os import getcwd, environ, remove
 from shutil import rmtree
+from cogent.app.util import ApplicationNotFoundError, get_tmp_filename
 from cogent.app.rdp_classifier import RdpClassifier
 from cogent.util.unit_test import TestCase, main
 
@@ -16,6 +17,18 @@ __email__ = "kylebittinger@gmail.com"
 __status__ = "Prototype"
 
 class RdpClassifierTests(TestCase):
+    def setUp(self):
+        # adjust environment variables
+        self.user_rdp_jar_path = environ.pop('RDP_JAR_PATH', None)
+        environ['RDP_JAR_PATH'] = 'rdp_classifier-2.0.jar'
+
+    def tearDown(self):
+        # reconstitute user's environment variables
+        if self.user_rdp_jar_path is None:
+            environ.pop('RDP_JAR_PATH')
+        else:
+            environ['RDP_JAR_PATH'] = self.user_rdp_jar_path
+
     def test_default_java_vm_parameters(self):
         """RdpClassifier should store default arguments to Java VM."""
         a = RdpClassifier()
@@ -26,13 +39,7 @@ class RdpClassifierTests(TestCase):
         a = RdpClassifier()
         parameters = a.Parameters.keys()
         parameters.sort()
-        self.assertEqual(parameters, ['-Xmx', '-jar', '-training-data'])
-
-    def test_jar_parameters_list(self):
-        a = RdpClassifier()
-        parameters = a.JarParameters.keys()
-        parameters.sort()
-        self.assertEqual(parameters, ['-jar'])
+        self.assertEqual(parameters, ['-Xmx', '-training-data'])
 
     def test_jvm_parameters_list(self):
         a = RdpClassifier()
@@ -59,13 +66,6 @@ class RdpClassifierTests(TestCase):
         exp = ''.join(['cd "', getcwd(), '/"; java -Xmx75M -jar "rdp_classifier-2.0.jar"'])
         self.assertEqual(app.BaseCommand, exp)
 
-    def test_assign_jar_parameters(self):
-        """RdpCalssifier should pass correct jar file to Java VM."""
-        app = RdpClassifier()
-        app.Parameters['-jar'].on('/jar/jar/binks.jar')
-        exp = ''.join(['cd "', getcwd(), '/"; java -Xmx1000m -jar "/jar/jar/binks.jar"'])
-        self.assertEqual(app.BaseCommand, exp)
-
     def test_basecommand_property(self):
         """RdpClassifier BaseCommand property should use overridden _get_base_command method."""
         app = RdpClassifier()
@@ -76,6 +76,32 @@ class RdpClassifierTests(TestCase):
         app = RdpClassifier()
         exp = ''.join(['cd "', getcwd(), '/"; java -Xmx1000m -jar "rdp_classifier-2.0.jar"'])
         self.assertEqual(app.BaseCommand, exp)
+        
+    def test_get_jar_fp(self):
+        """RdpClassifier should search for jar file in appropriate location"""
+        # Test 1: jar not in current dir, $RDP_JAR_PATH not set
+        environ.pop('RDP_JAR_PATH')
+        self.assertRaises(ApplicationNotFoundError, RdpClassifier)
+
+        # Test 2: Subclass RdpClassifier with different _command
+        # attribute, app should look there
+
+        # create a temporary jar file
+        tmp_jar_fp = get_tmp_filename(
+            prefix='RdpClassifierTest', suffix='.jar')
+        file = open(tmp_jar_fp, 'w')
+        file.write('some stuff')
+        file.close()
+
+        class TestRdpClassifier(RdpClassifier):
+            _command = tmp_jar_fp
+        app = TestRdpClassifier()
+        self.assertEqual(app._get_jar_fp(), tmp_jar_fp)
+
+        remove(tmp_jar_fp)
+
+        # reset environment variable
+        environ['RDP_JAR_PATH'] = 'not None'
 
     def test_change_working_dir(self):
         """RdpClassifier should run program in expected working directory."""
@@ -89,51 +115,66 @@ class RdpClassifierTests(TestCase):
 
     def test_sample_fasta(self):
         """RdpClassifier should classify its own sample data correctly"""
+        # Reconstitute user's $RDP_JAR_PATH variable
+        if self.user_rdp_jar_path is None:
+            environ.pop('RDP_JAR_PATH')
+        else:
+            environ['RDP_JAR_PATH'] = self.user_rdp_jar_path
+
         test_dir = '/tmp/RdpTest'
+        try:
+            app = RdpClassifier(WorkingDir=test_dir)
 
-        app = RdpClassifier(WorkingDir=test_dir)
-        app.Parameters['-jar'].on('/usr/local/app/rdp_classifier/rdp_classifier-2.0.jar')
-
-        results_file = app(rdp_sample_fasta)['StdOut']
+            results_file = app(rdp_sample_fasta)['StdOut']
         
-        id_line = results_file.readline()
-        self.failUnless(id_line.startswith('>X67228'))
+            id_line = results_file.readline()
+            self.failUnless(id_line.startswith('>X67228'))
 
-        classification_line = results_file.readline().strip()
-        def all_even_items(list):
-            return [x for (pos, x) in enumerate(list) if (pos % 2 == 0)]
-        obs = all_even_items(classification_line.split('; '))
-        exp = ['Root', 'Bacteria', 'Proteobacteria', 'Alphaproteobacteria', 'Rhizobiales', 'Rhizobiaceae', 'Rhizobium']
-        self.assertEqual(obs, exp)
+            classification_line = results_file.readline().strip()
+            def all_even_items(list):
+                return [x for (pos, x) in enumerate(list) if (pos % 2 == 0)]
+            obs = all_even_items(classification_line.split('; '))
+            exp = ['Root', 'Bacteria', 'Proteobacteria', 'Alphaproteobacteria', 'Rhizobiales', 'Rhizobiaceae', 'Rhizobium']
+            self.assertEqual(obs, exp)
 
-        rmtree(test_dir)
+            rmtree(test_dir)
+
+        finally:
+            # reset environment variable
+            environ['RDP_JAR_PATH'] = 'not None'
 
     def test_custom_training_data(self):
         """RdpClassifier should use sample training data"""
+        # Reconstitute user's $RDP_JAR_PATH variable
+        if self.user_rdp_jar_path is None:
+            environ.pop('RDP_JAR_PATH')
+        else:
+            environ['RDP_JAR_PATH'] = self.user_rdp_jar_path
+
         test_dir = '/tmp/RdpTest'
+        try:
+            app = RdpClassifier(WorkingDir=test_dir)
+            app.Parameters['-training-data'].on(
+                '/usr/local/app/rdp_classifier/mydata/rRNAClassifier.properties'
+                )
 
-        app = RdpClassifier(WorkingDir=test_dir)
-        app.Parameters['-jar'].on(
-            '/usr/local/app/rdp_classifier/rdp_classifier-2.0.jar'
-            )
-        app.Parameters['-training-data'].on(
-            '/usr/local/app/rdp_classifier/mydata/rRNAClassifier.properties'
-            )
-
-        results_file = app(rdp_sample_fasta)['StdOut']
+            results_file = app(rdp_sample_fasta)['StdOut']
         
-        id_line = results_file.readline()
-        self.failUnless(id_line.startswith('>X67228'))
+            id_line = results_file.readline()
+            self.failUnless(id_line.startswith('>X67228'))
 
-        classification_line = results_file.readline().strip()
-        def all_even_items(list):
-            return [x for (pos, x) in enumerate(list) if (pos % 2 == 0)]
-        obs = all_even_items(classification_line.split('; '))
-        exp = ['Root', 'Bacteria', 'Proteobacteria', 'Alphaproteobacteria', 'Rhizobiales', 'Rhizobiaceae', 'Rhizobium']
-        self.assertEqual(obs, exp)
+            classification_line = results_file.readline().strip()
+            def all_even_items(list):
+                return [x for (pos, x) in enumerate(list) if (pos % 2 == 0)]
+            obs = all_even_items(classification_line.split('; '))
+            exp = ['Root', 'Bacteria', 'Proteobacteria', 'Alphaproteobacteria', 'Rhizobiales', 'Rhizobiaceae', 'Rhizobium']
+            self.assertEqual(obs, exp)
 
-        rmtree(test_dir)
+            rmtree(test_dir)
 
+        finally:
+            # reset environment variable
+            environ['RDP_JAR_PATH'] = 'not None'
 
 # Sample data copied from rdp_classifier-2.0, which is licensed under
 # the GPL 2.0 and Copyright 2008 Michigan State University Board of
