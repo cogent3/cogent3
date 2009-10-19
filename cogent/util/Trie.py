45f5e8dbd6d5d94d236af8849643ab4392beb01f
Jens Reeder <jens.reeder@gmail.com>
2009-10-19 19:40:49 +0000

    Added Trie data structure for fast prefix matching.

---
+++ cogent/util/Trie.py
@@ -0,0 +1,357 @@
+#! /usr/bin/env python
+"""A trie and compressed trie data structure."""
+
+__author__ = "Jens Reeder"
+__copyright__ = "Copyright 2009, the PyCogent Project" 
+__credits__ = ["Jens Reeder"]
+__license__ = "GPL"
+__version__ = "0.1"
+__maintainer__ = "Jens Reeder"
+__email__ = "jreeder@gmail.com"
+__status__ = "Prototype"
+
+##################
+# A compressed Trie
+##################
+
+class _Compressed_Node:
+    """ A node in a compressed Trie."""
+    
+    def __init__(self, key, labels=None):
+        """Creates a new Node."""
+
+        self.labels = labels or []
+        self.key = key
+        self.children = {}
+  
+    def __nonzero__(self):
+        """Checks if Node contains any data."""
+        return (self.key!="" or len(self.labels)>0
+                or len(self.children.keys())>0)
+
+    def __len__(self):
+        """Counts the number of sequences in the Trie"""
+
+        l = 0 
+        for n in self.children.values():
+            l += len(n)
+        l += len(self.labels)
+        return l
+
+    def _to_string(self, depth=0):
+        """Debugging method to display the Node's content.
+
+        depth: indentation level
+        """
+
+        s = ["\n"+depth*'\t'+"key %s"%self.key]
+        
+        if (self.labels):
+            s.append("%s" % str(self.labels))
+        s.append("\n")
+ 
+        for n in self.children.values():
+            s.append(depth*'\t' +"{%s" %(n._to_string(depth+1)))
+            s.append(depth*'\t'+"}\n")
+        return "".join(s)
+
+    def size(self):
+        """Returns number of nodes."""
+        s = 1 
+        for n in self.children.values():
+            s += n.size()
+        return s
+
+    def insert(self, key, label):
+        """Insert a key into the Trie.
+
+        key: The key of the entry
+        label: The value that should be stored for this key
+        """
+
+        #follow the key into the tree
+        index = 0
+        node_key_len = len(self.key)
+
+        length = min(node_key_len, len(key))
+     
+        while(index < length):
+                if(key[index] != self.key[index]):
+                    #split up node
+                    new_key_node = _Compressed_Node(key[index:], [label])
+
+                    old_key_node = _Compressed_Node(self.key[index:], self.labels)
+                    old_key_node.children = self.children
+                    
+                    self.children = {key[index]: new_key_node,
+                                     self.key[index]: old_key_node}
+                    self.key = self.key[:index]
+                    self.labels = []
+                    return
+                index +=1
+
+        #new key matches node key exactly
+        if (index == len(self.key) and index== len(key)):
+            self.labels.append(label)
+            return
+
+        #Key shorter than node key
+        if(index < node_key_len):
+            lower_node = _Compressed_Node(self.key[index:], self.labels)
+            lower_node.children = self.children
+            
+            self.children = {self.key[index]: lower_node}
+            self.key = key
+            self.labels = [label]
+            return
+  
+        #new key longer than current node key
+        node = self.children.get(key[index])
+        if(node):
+            # insert into next node
+               node.insert(key[index:], label)
+        else:
+            # create new node
+            new_node = _Compressed_Node(key[index:], [label])
+            self.children[key[index]] = new_node
+        return   
+
+    def find(self, key):
+        """Searches for key and returns values stored for the key."""
+
+        #key exhausted
+        if(len(key) == 0):
+            return self.labels
+        
+        #find matching part of key and node_key
+        index = 0    
+        while(index < len(key) and index < len(self.key)):
+            if(key[index] != self.key[index]):
+                return []
+            index+=1
+
+        #key and node_key match exactly
+        if (index == len(key)):
+            return self.labels
+
+        node = self.children.get(key[index])
+        if(node):
+            # descend to next node
+            return node.find(key[index:])
+        else:
+            return []
+
+    def prefix_map(self):
+        """Builds a prefix map from sequences stored in Trie."""
+        
+        labels = []
+        mapping = {}
+        
+        # we have a leaf
+        if (len(self.children)==0):
+            mapping =  {self.labels[0]: self.labels[1:]}
+        # we are at an internal node
+        else: 
+            for child in self.children.values():
+                mapping.update(child.prefix_map())
+            # get largest group
+            n = -1
+            key_largest = None
+            for (key, value) in mapping.iteritems():
+                if (len(value) > n):
+                    n = len(value)
+                    key_largest = key
+            # append this node's values        
+            mapping[key_largest].extend(self.labels)
+
+        return mapping
+     
+
+class Compressed_Trie:
+    """ A compressed Trie"""
+
+    def __init__(self):
+        """ Return an empty Trie."""
+        self.root = _Compressed_Node("")
+        
+    def insert(self, key, label):
+        """Insert key with label in Trie."""
+        self.root.insert(key, label)
+
+    def find(self, key):
+        """Returns label for key in Trie."""
+        return self.root.find(key)
+        
+    def __str__(self):
+        """Ugly Trie string representation for debugging."""
+        return self.root._to_string()
+ 
+    def __nonzero__(self):
+        """Checks wheter the trie is empty or not."""
+        return (self.root.__nonzero__())
+    
+    def __len__(self):
+        """Returns number of sequences in Trie."""
+        return len(self.root)
+
+    def size(self):
+        """Returns number of nodes in Trie"""
+        return self.root.size()
+
+    def prefix_map(self):
+        """builds a prefix map of seqeunces stored in Trie."""
+        return self.root.prefix_map()
+
+
+###################
+# An atomic Trie
+###################
+
+class _Node:
+    """ A node in a Trie."""
+
+    def __init__(self, labels=None):
+        """Creates a new node with value label."""
+        self.labels = labels or []
+        self.children = {}
+
+    def insert(self, key, label):
+        """Insert key with value label into Trie.
+
+        key: The key of the entry
+        label: The value that should be stored for this key
+        """
+        curr_node = self
+        for ch in key:
+            curr_node = curr_node.children.setdefault(ch, _Node())
+        
+        curr_node.labels.append(label)
+
+    def _insert_unique(self, key, value):
+        """Insert key with value if key not already in Trie.
+
+        key: The key of the entry
+        label: The value that should be stored for this key
+        
+        Returns label of key if unique or label of containing key.
+        Note: Tries build with this function will only have labels on their leaves.
+        """
+        
+        curr_node = self
+        labels = []
+        #descend along key and collect all internal node labels
+        for ch in key:
+            if curr_node.labels != []:
+                labels.extend(curr_node.labels)
+                curr_node.labels = []
+            curr_node = curr_node.children.setdefault(ch, _Node())                
+
+        if (curr_node.children=={} and curr_node.labels==[]):
+            # we have a new prefix
+            curr_node.labels = [value]
+            return (curr_node.labels[0], labels)
+        else:
+            # we are at an internal node or a labeled leaf
+            # descend to the "leftmost" leaf 
+            while (curr_node.children != {}):
+                curr_node = curr_node.children.values()[0]
+            return (curr_node.labels[0], [])
+
+    def find(self, key):
+        """Retrieves node with key in Trie."""
+
+        next_node = self
+        for ch in key:
+            try:
+                next_node = next_node.children[ch]
+            except KeyError:
+                return None
+        return next_node
+
+class Trie:
+    """ A Trie data structure."""
+
+    def __init__(self):
+        """Inits an empty Trie."""
+        self.root = _Node()
+        
+    def insert(self, key, label):
+        """Insert key with label into Trie.
+
+        key: The key of the entry
+        label: The value that should be stored for this key
+        """
+        return self.root.insert(key, label)
+    
+    def _insert_unique(self, key, label):
+        """Insert unique keys into Trie, skip redundant ones.
+
+        key: The key of the entry
+        label: The value that should be stored for this key
+
+        Returns: list of labels removed rom Trie
+        """
+        return self.root._insert_unique(key, label)
+    
+    def find(self, key):
+        """Retrieves  labels of key in Trie."""
+        node = self.root.find(key)
+
+        if (node ==None):
+            return []
+        return node.labels
+
+def _build_prefix_map(seqs):
+    """Builds a prefix map.
+
+    seqs: dict like sequence collection
+    """
+    mapping = {}
+    t = Trie()
+    for label,seq in seqs:
+        (label2, prefix_labels) = t._insert_unique(seq, label)
+        if (label2==label):
+            #seq got inserted and is new word
+            mapping[label] = prefix_labels[:]
+            #delete old mappings and transfer old mappings to new label
+            for l in prefix_labels:
+                mapping[label].extend(mapping[l])
+                del(mapping[l])
+        else:
+            # seq is already in tree and is prefix of label2
+            mapping[label2].append(label)
+
+    return mapping
+
+
+def build_trie(seqs, classname=Compressed_Trie):
+    """ build a Trie for a list of (label,seq) pairs.
+
+    seqs: list of (label,seq) pairs
+
+    classname: Constructor to use for tree building
+               Compressed_Trie (default) or Trie
+     `"""
+    if (not(classname==Trie or classname==Compressed_Trie)):
+        raise ValueError, "Wrong classname for build_trie."
+    t = classname()
+    for (label, seq) in seqs:
+            t.insert(seq, label)
+    return t
+
+
+def build_prefix_map(seqs, classname=Compressed_Trie):
+    """Builds prefix map from seqs.
+ 
+    seqs: list of (label,seq) pairs
+
+    classname: Constructor to use for tree building
+               Compressed_Trie (default) or Trie
+    """
+    if (not(classname==Trie or classname==Compressed_Trie)):
+        raise ValueError, "Wrong classname for build_trie."
+    if (classname == Compressed_Trie):
+        t = build_trie(seqs)
+        return t.prefix_map()
+    else:
+        return _build_prefix_map(seqs)
---
+++ tests/test_util/test_Trie.py
@@ -0,0 +1,330 @@
+#!/usr/bin/env python
+
+"""tests for Trie and compressed Trie class."""
+
+__author__ = "Jens Reeder"
+__copyright__ = "Copyright 2009, the PyCogent Project" 
+__credits__ = ["Jens Reeder"]
+__license__ = "GPL"
+__version__ = "0.1"
+__maintainer__ = "Jens Reeder"
+__email__ = "jreeder@gmail.com"
+__status__ = "Prototype"
+
+
+from cogent.util.unit_test import TestCase, main
+
+from Trie import Trie, Compressed_Trie, build_prefix_map, build_trie, \
+    _build_prefix_map
+
+class TrieTests(TestCase):
+    
+    def setUp(self):
+        self.data = dict({"0": "ab", "1":"abababa", "2":"abab",
+                          "3":"baba", "4":"ababaa","5":"a", "6":"abababa",
+                          "7":"bab", "8":"babba"})
+    
+    def test_init(self):
+        """Trie init should create an empty trie."""
+        
+        t = Trie()
+        self.assertEqual(t.root.labels, [])
+        self.assertEqual(t.root.children, {})
+        
+    def test_insert_find(self):
+        """An added key should be found by find."""
+
+        data = self.data
+        
+        t = Trie()
+        for (label, seq) in data.iteritems():
+            t.insert(seq, label)
+           
+        for  (label, seq) in data.iteritems():
+            self.assertEqual(label in t.find(seq), True)
+        self.assertEqual(t.find("cacgchagc"), [])
+        self.assertEqual(t.find("abababa"), ["1","6"])
+
+    def test_insert_unique(self):
+        """insert_unique should insert only unique words."""
+
+        data = self.data
+        
+        t = Trie()
+        for (label, seq) in data.iteritems():
+            t._insert_unique(seq, label)
+                       
+        self.assertEqual(t.find("ab"), [])
+        self.assertEqual(t.find("cacgchagc"), [])
+        self.assertEqual(t.find("abababa"), ["1"])
+
+
+    def test_build_prefix_map(self):
+        """prefix_map should map prefix strings."""
+        
+        self.assertEqual(dict(_build_prefix_map(self.data.iteritems())),
+                         {'1': ['0', '2', '5', '6'],
+                          '8': [],
+                          '3': ['7'],
+                          '4': []})
+
+class Compressed_Trie_Tests(TestCase):
+    
+    def setUp(self):
+        self.data = dict({"0": "ab", "1":"abababa", "2":"abab",
+                          "3":"baba", "4":"ababaa","5":"a", "6":"abababa",
+                          "7":"bab", "8":"babba"})
+
+        self.trie = build_trie(self.data.iteritems())
+    
+    def test_init(self):
+        """Trie init should create an empty trie."""
+        
+        t = Compressed_Trie()
+        self.assertEqual(t.root.labels, [])
+        self.assertEqual(t.root.children, {})
+        self.assertEqual(t.root.key, "")
+        
+    def test_non_zero(self):
+        """__non_zero__ should cehck for any data in the trie."""
+        t = Compressed_Trie()
+        self.assertEqual(t.__nonzero__(), False)
+        self.assertEqual(self.trie.__nonzero__(), True)
+
+    def test_len(self):
+        """__len__ should return the number of seqs in the trie."""
+        
+        self.assertEqual(len(self.trie), 9)
+        t = Compressed_Trie()
+        self.assertEqual(len(t), 0)
+
+    def test_size(self):
+        """size should return the number of nodes in the trie."""
+        
+        self.assertEqual(self.trie.size(), 10)
+
+        #empty trie contins only root node
+        t = Compressed_Trie()
+        self.assertEqual(size(t), 1)
+
+    def test_to_string(self):
+        """_to_string should create a string representation."""
+
+        string_rep = """
+key 
+{
+\tkey a['5']
+\t{
+\t\tkey b['0']
+\t\t{
+\t\t\tkey ab['2']
+\t\t\t{
+\t\t\t\tkey a
+\t\t\t\t{
+\t\t\t\t\tkey a['4']
+\t\t\t\t}
+\t\t\t\t{
+\t\t\t\t\tkey ba['1', '6']
+\t\t\t\t}
+\t\t\t}
+\t\t}
+\t}
+}
+{
+\tkey bab['7']
+\t{
+\t\tkey a['3']
+\t}
+\t{
+\t\tkey ba['8']
+\t}
+}
+"""
+        self.assertEqual(str(self.trie), string_rep)
+        
+    def test_insert_find(self):
+        """An added key should be found by find."""
+
+        data = self.data
+        t = Compressed_Trie()
+        for (label, seq) in data.iteritems():
+            t.insert(seq, label)
+    
+        for  (label, seq) in data.iteritems():
+            self.assertEqual(label in t.find(seq), True)
+        self.assertEqual(t.find("abababa"), ["1","6"])
+        self.assertEqual(t.find("cacgchagc"), [])
+
+
+    def test_prefix_map(self):
+        """prefix_map (Compressed_Trie) should map prefix strings."""
+        
+        self.assertEqual(self.trie.prefix_map(),
+                         {'1': ['6', '2', '0', '5'],
+                          '8': ['7'],
+                          '3': [], 
+                          '4': []})
+    
+    def test_init(self):
+        """Trie init should create an empty trie."""
+        
+        t = Trie()
+        self.assertEqual(t.root.labels, [])
+        self.assertEqual(t.root.children, {})
+        
+    def test_insert_find(self):
+        """An added key should be found by find."""
+
+        data = self.data
+        
+        t = Trie()
+        for (label, seq) in data.iteritems():
+            t.insert(seq, label)
+           
+        for  (label, seq) in data.iteritems():
+            self.assertEqual(label in t.find(seq), True)
+        self.assertEqual(t.find("cacgchagc"), [])
+        self.assertEqual(t.find("abababa"), ["1","6"])
+
+    def test_insert_unique(self):
+        """insert_unique should insert only unique words."""
+
+        data = self.data
+        
+        t = Trie()
+        for (label, seq) in data.iteritems():
+            t._insert_unique(seq, label)
+                       
+        self.assertEqual(t.find("ab"), [])
+        self.assertEqual(t.find("cacgchagc"), [])
+        self.assertEqual(t.find("abababa"), ["1"])
+
+
+    def test_build_prefix_map(self):
+        """prefix_map should map prefix strings."""
+        
+        self.assertEqual(dict(_build_prefix_map(self.data.iteritems())),
+                         {'1': ['0', '2', '5', '6'],
+                          '8': [],
+                          '3': ['7'],
+                          '4': []})
+   
+    def test_build_trie(self):
+        """Build_trie should build trie from seqs."""
+        
+        t = build_trie(self.data.iteritems(), Trie)
+        self.assertTrue(isinstance(t, Trie))
+        for (label, seq) in self.data.iteritems():
+            self.assertContains(t.find(seq), label)
+
+        self.assertEqual(t.find(""), [])
+        self.assertEqual(t.find("ccc"), [])
+
+
+class Compressed_Trie_Tests(TestCase):
+    
+    def setUp(self):
+        self.data = dict({"0": "ab", "1":"abababa", "2":"abab",
+                          "3":"baba", "4":"ababaa","5":"a", "6":"abababa",
+                          "7":"bab", "8":"babba"})
+
+        self.trie = build_trie(self.data.iteritems())
+    
+    def test_init(self):
+        """Trie init should create an empty trie."""
+        
+        t = Compressed_Trie()
+        self.assertEqual(t.root.labels, [])
+        self.assertEqual(t.root.children, {})
+        self.assertEqual(t.root.key, "")
+        
+    def test_non_zero(self):
+        """__non_zero__ should cehck for any data in the trie."""
+        t = Compressed_Trie()
+        self.assertEqual(t.__nonzero__(), False)
+        self.assertEqual(self.trie.__nonzero__(), True)
+
+    def test_len(self):
+        """__len__ should return the number of seqs in the trie."""
+        
+        self.assertEqual(len(self.trie), 9)
+
+    def test_size(self):
+        """size should return the number of nodes in the trie."""
+        
+        self.assertEqual(self.trie.size(), 10)
+
+    def test_to_string(self):
+        """_to_string should create a string representation."""
+
+        string_rep = """
+key 
+{
+\tkey a['5']
+\t{
+\t\tkey b['0']
+\t\t{
+\t\t\tkey ab['2']
+\t\t\t{
+\t\t\t\tkey a
+\t\t\t\t{
+\t\t\t\t\tkey a['4']
+\t\t\t\t}
+\t\t\t\t{
+\t\t\t\t\tkey ba['1', '6']
+\t\t\t\t}
+\t\t\t}
+\t\t}
+\t}
+}
+{
+\tkey bab['7']
+\t{
+\t\tkey a['3']
+\t}
+\t{
+\t\tkey ba['8']
+\t}
+}
+"""
+        self.assertEqual(str(self.trie), string_rep)
+        
+    def test_insert_find(self):
+        """An added key should be found by find."""
+
+        data = self.data
+        t = Compressed_Trie()
+        for (label, seq) in data.iteritems():
+            t.insert(seq, label)
+    
+        for  (label, seq) in data.iteritems():
+            self.assertEqual(label in t.find(seq), True)
+        self.assertEqual(t.find("abababa"), ["1","6"])
+        self.assertEqual(t.find("cacgchagc"), [])
+
+
+    def test_prefix_map(self):
+        """prefix_map (Compressed_Trie) should map prefix strings."""
+        
+        self.assertEqual(self.trie.prefix_map(),
+                         {'1': ['6', '2', '0', '5'],
+                          '8': ['7'],
+                          '3': [], 
+                          '4': []})
+
+    def test_build_trie(self):
+        """Build_trie should build trie from seqs."""
+        
+        t = build_trie(self.data.iteritems())
+        
+        self.assertTrue(isinstance(t, Compressed_Trie))
+        
+        for (label, seq) in self.data.iteritems():
+            self.assertContains(t.find(seq), label)
+
+        self.assertEqual(t.find(""), [])
+        self.assertEqual(t.find("ccc"), [])
+
+
+if __name__ == "__main__":
+    main()
