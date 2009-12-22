3fdf32a3c78fac2bb71e50e1661ef25bcc9e66d5
Greg Caporaso <gregcaporaso@gmail.com>
2009-12-22 17:50:22 +0000

    Added test_build_blast_db_from_fasta_file function which takes an open file object containing the reference seqs as a fasta file.

--- cogent/app/formatdb.py
+++ cogent/app/formatdb.py
@@ -123,6 +123,37 @@ def build_blast_db_from_fasta_path(fasta_path,is_protein=False,\
             pass
     return db_name, db_filepaths
     
+def build_blast_db_from_fasta_file(fasta_file,is_protein=False,\
+    output_dir=None,HALT_EXEC=False):
+    """Build blast db from fasta_path; return db name and list of files created
+    
+        **If using to create temporary blast databases, you can call
+        cogent.util.misc.remove_files(db_filepaths) to clean up all the
+        files created by formatdb when you're done with the database.
+    
+        fasta_path: path to fasta file of sequences to build database from
+        is_protein: True if working on protein seqs (default: False)
+        output_dir: directory where output should be written
+         (default: directory containing fasta_path)
+        HALT_EXEC: halt just before running the formatdb command and
+         print the command -- useful for debugging
+    """
+    output_dir = output_dir or '.'
+    fasta_path = get_tmp_filename(\
+     tmp_dir=output_dir, prefix="BLAST_temp_db_", suffix=".fasta")
+    
+    fasta_f = open(fasta_path,'w')
+    for line in fasta_file:
+        fasta_f.write('%s\n' % line.strip())
+    fasta_f.close()
+    
+    blast_db, db_filepaths = build_blast_db_from_fasta_path(\
+     fasta_path,is_protein=False,output_dir=None,HALT_EXEC=False)
+     
+    db_filepaths.append(fasta_path)
+    
+    return blast_db, db_filepaths
+    
 def build_blast_db_from_seqs(seqs,is_protein=False,\
     output_dir='./',HALT_EXEC=False):
     """Build blast db from seqs; return db name and list of files created
