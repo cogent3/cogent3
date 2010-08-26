34a57fab4e975383036f813d730d8c2ccd36403d
Gavin Huttley <gavin.huttley@anu.edu.au>
2010-08-26 01:19:13 +0000

    best likelihood tests now using unit test methods rather

--- tests/test_evolve/test_best_likelihood.py
+++ tests/test_evolve/test_best_likelihood.py
@@ -45,20 +45,20 @@ class TestGoldman93(TestCase):
                    ['G','T','A'],['G','T','A'],['G','T','A'],
                    ['T','A','C'],['T','A','C'],['T','A','C'],
                    ['A','C','G']]
-        assert obs == expect, (obs, expect)
+        self.assertEqual(obs, expect)
         obs = aligned_columns_to_rows(self.gapped_aln[:-1], 3, allowed_chars='ACGT')
         expect = [['TTT','TAT','TTT']]
-        assert obs == expect, (obs, expect)
+        self.assertEqual(obs, expect)
         
         obs = aligned_columns_to_rows(self.ambig_aln, 2, exclude_chars=IUPAC_DNA_ambiguities)
         expect = [['AA','CC','CA'],['CC','CC','CC'],['TT','TT','TG']]
-        assert obs == expect, (obs, expect)
+        self.assertEqual(obs, expect)
     
     def test_count_column_freqs(self):
         columns = aligned_columns_to_rows(self.aln, 1)
         obs = count_column_freqs(columns)
         expect = {'A C G' : 4, 'C G T' : 3, 'G T A' : 3, 'T A C' : 3}
-        assert obs == expect, (obs, expect)
+        self.assertEqual(obs, expect)
         
         columns = aligned_columns_to_rows(self.aln[:-1], 2)
         obs = count_column_freqs(columns)
@@ -87,7 +87,7 @@ class TestGoldman93(TestCase):
         for pattern, lnL, freq in obs:
             self.assertFloatEqual(lnL, expect[pattern])
             sum += lnL
-            assert lnL >= 0
+            self.assertTrue(lnL >= 0)
         self.assertFloatEqual(sum, 1)
     
     def test_get_G93_lnL_from_array(self):
