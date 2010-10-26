51915dedaeb4d23696ae524dc93b20feeaa50c3d
Gavin Huttley <gavin.huttley@anu.edu.au>
2010-10-26 23:03:45 +0000

    change method name for period testing
    [CHANGED] permutation_test changed to blockwise_bootstrap because it's not
        a permutation of the original data (sampling without replacement) but a
        sampling with replacement.

--- cogent/maths/stats/period.py
+++ cogent/maths/stats/period.py
@@ -144,8 +144,8 @@ def sampled_places(block_size, length):
     assert len(result) == length, len(result)
     return result
 
-def permutation_test(signal, calc, block_size, num_reps, seq_to_symbols=None, num_stats=None):
-    """returns observed statistic and the probability from the permutation
+def blockwise_bootstrap(signal, calc, block_size, num_reps, seq_to_symbols=None, num_stats=None):
+    """returns observed statistic and the probability from the bootstrap
     test of observing more `power' by chance than that estimated from the
     observed signal
     
