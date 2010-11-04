9f9071530f7b1a1b463e17fb6583873209d3f7cd
Jesse Zaneveld <zaneveld@gmail.com>
2010-11-04 04:05:09 +0000

    Parser for KEGG .pos files

---
+++ cogent/parse/kegg_pos.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+__author__ = "Jesse Zaneveld"
+__copyright__ = "Copyright 2007-2010, The Cogent Project"
+__credits__ = ["Jesse Zaneveld", "Rob Knight"]
+__license__ = "GPL"
+__version__ = "0.1.0.dev"
+__maintainer__ = "Jesse Zaneveld"
+__email__ = "zaneveld@gmail.com"
+__status__ = "Development"
+
+"""
+Parser for kegg 
+"""
+# Pos file parsers
+def parse_pos_file(fname):
+    """Opens fname, extracts pos fields and prepends filename"""
+    curr_file = open(fname,"U")
+    for line in parse_pos_lines(curr_file,fname):
+        yield line
+
+def parse_pos_lines(lines, file_name):
+    """Parse lines from a KEGG .pos file, yielding tab-
+    delimited strings
+    
+    file name -- the file name, for deriving the 
+    species for the pos file (this is not available within
+    the pos file, but important for mapping to other KEGG
+    data)
+    """
+    species_name = file_name.split('/')[-1].rsplit('.',1)[0]
+    for line in lines:
+        yield species_name + '\t' + line[:-1] + "\n"
+
+if __name__ == '__main__':
+    from sys import argv
+    filename = argv[1]
+    for result_line in parse_pos_file(filename):
+        print result_line
