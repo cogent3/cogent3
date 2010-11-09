abf5835b7f320bcb4ea784205fe760e0a5addef6
Jeremy Widmann <jeremy.widmann@colorado.edu>
2010-11-09 01:22:59 +0000

    Made changes to Cmsearch parser and cmsearch app controller to account for changes in latest version of Infernal.  Parser and app controller now only support Infernal versions 1.0 and 1.0.2.  Corresponding tests were changed as well.

--- cogent/parse/infernal.py
+++ cogent/parse/infernal.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 #file cogent.parse.infernal.py
-"""Parses various Infernal output formats."""
+"""Parses various Infernal output formats for the commandline version of:
+Infernal 1.0 and 1.0.2 only."""
 
 from cogent.parse.table import ConvertFields, SeparatorFormatParser,is_empty
 
@@ -31,16 +32,19 @@ def CmsearchParser(lines):
     """
     # Converting indices and %GC to integers and bit score to float.
     # Since E-value is only present if CM is calibrated, leaving as string.
-    conversion_fields = [(1,int),(2,int),(3,int),(4,int),(5,float),(7,int)]
+    conversion_fields = [(2,int),(3,int),(4,int),(5,int),(6,float),(8,int)]
     cmsearch_converter = ConvertFields(conversion_fields)
-    #Ignore hash characters and empty lines
-    ignore_funct = lambda x: x.startswith('#') or is_empty(x)
+    #Ignore hash characters
+    good_lines = []
+    for l in lines:
+        if not l.startswith('#'):
+            good_lines.append(l)
     #make parser
     cmsearch_parser = SeparatorFormatParser(with_header=False,\
                                             converter=cmsearch_converter,\
-                                            ignore=ignore_funct,\
+                                            ignore=None,\
                                             sep=None)
     
-    return cmsearch_parser(lines)
+    return cmsearch_parser(good_lines)
 
     
