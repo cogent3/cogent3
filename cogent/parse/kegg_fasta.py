f6a13e9e2b4cc632f9cfb7e58853a4ac32994746
Jesse Zaneveld <zaneveld@gmail.com>
2010-11-09 20:03:29 +0000

    Parse for KEGG fasta files relying on MinimalFastaParser and a custom label line parser

---
+++ cogent/parse/kegg_fasta.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+from string import strip
+from cogent.parse.fasta import MinimalFastaParser
+
+__author__ = "Jesse Zaneveld"
+__copyright__ = "Copyright 2007-2010, The Cogent Project"
+__credits__ = ["Jesse Zaneveld", "Rob Knight"]
+__license__ = "GPL"
+__version__ = "1.6.0.dev"
+__maintainer__ = "Jesse Zaneveld"
+__email__ = "zaneveld@gmail.com"
+__status__ = "Release"
+
+"""
+Parser for KEGG fasta files 
+
+This code is useful for parsing the KEGG .nuc or .pep files
+"""
+
+def parse_fasta(lines):
+    """lightweight parser for KEGG FASTA format sequences"""
+    for label, seq in MinimalFastaParser(lines):
+        yield '\t'.join(list(kegg_label_fields(label)) \
+          + [seq] + ["\n"])
+
+def kegg_label_fields(line):
+    """Splits line into KEGG label fields.
+
+    Format is species:gene_id [optional gene_name]; description.
+    """
+    fields = map(strip, line.split(None, 1))
+    id_ = fields[0] 
+    species, gene_id = map(strip, id_.split(':',1))
+    #check if we got a description
+    gene_name = description = ''
+    if len(fields) > 1:
+        description = fields[1]
+        if ';' in description:
+            gene_name, description = map(strip, description.split(';',1))
+    return id_, species, gene_id, gene_name, description
+
+
+
+if __name__ == '__main__':
+    from sys import argv
+    filename = argv[1]
+    for result_line in parse_fasta(open(filename)):
+        print result_line
