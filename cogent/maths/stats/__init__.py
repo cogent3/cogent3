9f16c1aed436c05a7bb3c51d1e6bbe38197ab302
Gavin Huttley <gavin.huttley@anu.edu.au>
2010-11-26 06:41:59 +0000

    added information_criteria into the __all__ attribute for stats

--- cogent/maths/stats/__init__.py
+++ cogent/maths/stats/__init__.py
@@ -4,9 +4,9 @@
 Also provides NumberList and FrequencyDistribution, two classes for
 working with statistical data.
 """
-__all__ = ['alpha_diversity', 'distribution', 'histogram', 'kendall', 'ks', 
-        'rarefaction', 'special', 'test',
-           'util']
+__all__ = ['alpha_diversity', 'distribution', 'histogram',
+    'information_criteria', 'kendall', 'ks',  'rarefaction', 'special', 'test',
+    'util']
 
 # GAH: this is a temporary introduction, so users get notice of structure change and
 # renaming of this function
