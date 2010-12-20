9a7f381bd0223f6cce86c6676d21fe0160215975
Gavin Huttley <gavin.huttley@anu.edu.au>
2010-12-20 00:37:25 +0000

    optimiser funcs can now return evaluation count
    [NEW] return_eval_count argument added to maximise, minimise functions. When
        True, the vector and the number of function evaluations are returned,
        otherwise just the vector is returned.

--- tests/test_maths/test_optimisers.py
+++ tests/test_maths/test_optimisers.py
@@ -19,13 +19,14 @@ def quartic(x):
     # http://www.wolframalpha.com/input/?i=x**2*%283*x**2%2B8*x-48%29
     # Scaled down 10-fold to avoid having to change init_temp
     return x**2*(3*x**2+8*x-48)
-   
+
 class NullFile(object):
     def write(self, x):
         pass
     def isatty(self):
         return False
-        
+    
+
 def quiet(f, *args, **kw):
     # Checkpointer still has print statements
     orig = sys.stdout
@@ -36,27 +37,28 @@ def quiet(f, *args, **kw):
         sys.stdout = orig
     return result
 
-#def quiet(f, *args, **kw):
-#    return f(*args, **kw)
-    
+def MakeF():
+    evals = [0]
+    last = [0]
+    def f(x):
+        evals[0] += 1
+        last[0] = x
+        # Scaled down 10-fold to avoid having to change init_temp
+        return -0.1 * quartic(x)
+    return f, last, evals
+
 class OptimiserTestCase(TestCase):
     def _test_optimisation(self, target=-4, xinit=1.0, bounds=([-10,10]), **kw):
         local = kw.get('local', None)
         max_evaluations = kw.get('max_evaluations', None)
-
-        evals = [0]
-        last = [0]
-        def f(x):
-            evals[0] += 1
-            last[0] = x
-            # Scaled down 10-fold to avoid having to change init_temp
-            return -0.1 * quartic(x)
+        
+        f, last, evals = MakeF()
         
         x = quiet(maximise, f, [xinit], bounds, **kw)
         self.assertEqual(x, last[0]) # important for Calculator
         error = abs(x[0] - target)
         self.assertTrue(error < .0001, (kw, x, target, x))
-        
+    
     def test_global(self):
         # Should find global minimum
         self._test_optimisation(local=False, seed=1)
@@ -65,15 +67,26 @@ class OptimiserTestCase(TestCase):
         # Global minimum out of bounds, so find secondary one
         # numpy.seterr('raise')
         self._test_optimisation(bounds=([0.0],[10.0]), target=2, seed=1)
-            
+    
     def test_local(self):
         # Global minimum not the nearest one
         self._test_optimisation(local=True, target=2)
-
+    
     def test_limited(self):
         self.assertRaises(MaximumEvaluationsReached, 
             self._test_optimisation, max_evaluations=5)
-                
+    
+    # def test_limited_warning(self):
+    #     """optimiser warning if max_evaluations exceeded"""
+    #     self._test_optimisation(max_evaluations=5, limit_action='warn')
+    
+    def test_get_max_eval_count(self):
+        """return the evaluation count from optimisation"""
+        f, last, evals = MakeF()
+        x, e = quiet(maximise, f, xinit=[1.0], bounds=([-10,10]),
+                    return_eval_count=True)
+        self.assertTrue(e > 500)
+    
     def test_checkpointing(self):
         filename = 'checkpoint.tmp.pickle'
         if os.path.exists(filename):
@@ -84,6 +97,7 @@ class OptimiserTestCase(TestCase):
                 filename=filename, seed=1, init_temp=3.21)
         if os.path.exists(filename):
             os.remove(filename)
-        
+    
+
 if __name__ == '__main__':
     main()
