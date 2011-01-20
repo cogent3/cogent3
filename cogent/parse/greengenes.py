504062a91366d08f58c8e63026e4a3956cc22b4b
Daniel McDonald <mcdonadt@colorado.edu>
2011-01-20 23:52:43 +0000

    [ADDED]     Parsers for raw 16S Greengenes records including cmdline interface
        Note: The returned object is a GenericRecord. It may in the future make
        sense to add a MappedRecord. But, the simple parser is now out of the way.

---
+++ cogent/parse/greengenes.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python
+
+"""Parse the Greengenes formatted sequence data records
+
+The script is intended to be used with the following input:
+http://greengenes.lbl.gov/Download/Sequence_Data/Greengenes_format/greengenes16SrRNAgenes.txt.gz
+"""
+
+from cogent.parse.record_finder import DelimitedRecordFinder
+from cogent.parse.record import DelimitedSplitter, GenericRecord
+
+__author__ = "Daniel McDonald"
+__copyright__ = "Copyright 2007-2-11, The Cogent Project"
+__credits__ = ["Daniel McDonald"] 
+__license__ = "GPL"
+__version__ = "1.6.0.dev"
+__maintainer__ = "Daniel McDonald"
+__email__ = "daniel.mcdonald@colorado.edu"
+__status__ = "Prototype"
+
+def make_ignore_f(start_line):
+    """Make an ignore function that ignores bad gg lines"""
+    def ignore(line):
+        """Return false if line is bad"""
+        return not line or ['',''] == line or [start_line,''] == line
+    return ignore
+
+def DefaultDelimitedSplitter(delimiter):
+    """Wraps delimited splitter to handle empty records"""
+    parser = DelimitedSplitter(delimiter=delimiter)
+    def f(line):
+        parsed = parser(line)
+        if len(parsed) == 1:
+            parsed.append('')
+        return parsed
+    return f
+
+def MinimalGreengenesParser(lines,LineDelim="=",RecStart="BEGIN",RecEnd="END"):
+    """Parses raw Greengeens 16S rRNA Gene records
+   
+    lines  :  open records file
+    LineDelim  :  individual line delimiter, eg foo=bar
+    RecStart  :  start identifier for a record
+    RecEnd  :  end identifier for a record
+    """
+    line_parser = DefaultDelimitedSplitter(delimiter=LineDelim)
+
+    # parse what the ending record looks like so it can match after being split
+    RecordDelim = line_parser(RecEnd)
+
+    # make sure to ignore the starting record
+    ignore = make_ignore_f(RecStart)
+
+    parser = DelimitedRecordFinder(RecordDelim, constructor=line_parser, 
+                                   keep_delimiter=False, ignore=ignore)
+
+    for record in parser(lines):
+        yield GenericRecord(record)
+
+def SpecificGreengenesParser(lines, fields, **kwargs):
+    """Yield specific fields from successive Greengenes records
+
+    Returns tuples in 'fields' order
+    """
+    parser = MinimalGreengenesParser(lines, **kwargs)
+    for record in parser:
+        yield tuple([record[field] for field in fields])
+
+def main():
+    from optparse import make_option
+    from cogent.util.misc import parse_command_line_parameters
+    from sys import exit, stdout
+    
+    script_info = {}
+    script_info['brief_description'] = "Parse raw Greengenes 16S records"
+    script_info['script_description'] = """Parse out specific fields from raw Greengenes 16S records. These records are rich but often only a subset of each record is required for downstream processing."""
+    script_info['script_usage'] = []
+    script_info['script_usage'].append(("""Example:""","""Greengenes taxonomy and raw sequences are needed:""","""python greengenes.py -i greengenes16SrRNAgenes.txt -o gg_seq_and_tax.txt -f prokMSA_id,greengenes_tax_string,aligned_seq"""))
+    script_info['script_usage'].append(("""Example:""","""Spitting out the available fields from Greengenes:""","""python greengenes.py -i greengenes16SrRNAgenes.txt --print-fields"""))
+    script_info['output_description'] = """The resulting output file will contain a header that is prefixed with a # and delimited by the specified delimiter (default is tab). All records will follow in the same order with the same delimiter. It is possible for some key/value pairs within a record to lack a value. In this case, the value placed will be ''"""
+    script_info['required_options']=[make_option('--input','-i',dest='input',\
+                  help='Greengenes Records')]
+    script_info['optional_options']=[\
+               make_option('--output','-o',dest='output',help='Output file'),
+               make_option('--fields','-f',dest='fields',\
+                  help='Greengenes fields to keep'),
+               make_option('--delim','-d',dest='delim',help='Output delimiter',\
+                       default="\t"),
+               make_option('--print-fields','-p',dest='print_fields',\
+                  help='Prints available fields from first Greengenes Record',\
+                  action='store_true',default=False)]
+    script_info['version'] = __version__
+    option_parser, opts, args = parse_command_line_parameters(**script_info)
+
+    if opts.print_fields:
+        gg_parser = MinimalGreengenesParser(open(opts.input))
+        rec = gg_parser.next()
+        print '\n'.join(sorted(rec.keys()))
+        exit(0)
+
+    if not opts.fields:
+        print option_parser.usage()
+        print
+        print "Greengenes fields must be specified!"
+        exit(1)
+
+    if not opts.output:
+        output = stdout
+    else:
+        output = open(opts.output,'w')
+
+    fields = opts.fields.split(',')
+    output.write("#%s\n" % opts.delim.join(fields))
+
+    gg_parser = SpecificGreengenesParser(open(opts.input), fields)
+
+    for record in gg_parser:
+        output.write(opts.delim.join(record))
+        output.write('\n')
+
+    if opts.output:
+        output.close()
+    
+if __name__ == '__main__':
+    main()
---
+++ tests/test_parse/test_greengenes.py
@@ -0,0 +1,233 @@
+#!/usr/bin/env python
+
+from cogent.util.unit_test import TestCase, main
+from cogent.parse.greengenes import MinimalGreengenesParser, make_ignore_f,\
+        DefaultDelimitedSplitter, SpecificGreengenesParser
+
+__author__ = "Daniel McDonald"
+__copyright__ = "Copyright 2007-2011, The Cogent Project" #consider project name
+__credits__ = ["Daniel McDonald"] #remember to add yourself if you make changes
+__license__ = "GPL"
+__version__ = "1.6.0.dev"
+__maintainer__ = "Daniel McDonald"
+__email__ = "daniel.mcdonald@colorado.edu"
+__status__ = "Prototype"
+
+class ParseGreengenesRecordsTests(TestCase):
+    def setUp(self):
+        pass
+
+    def test_MinimalGreengenesParser_mock(self):
+        """Test MinimalGreengenesParser against mock data"""
+        res = MinimalGreengenesParser(mock_data.splitlines(), RecStart="my_starting", \
+                RecEnd="my_ending")
+
+        records = list(res)
+
+        exp = [{'a':'1','b':'2','c':'3','d':'','e':'5'},
+               {'q':'asdasd','c':'taco'}]
+        
+        self.assertEqual(records, exp)
+
+    def test_MinimalGreengenesParser_real(self):
+        """Test MinimalGreengenesParser against real data"""
+        res = MinimalGreengenesParser(real_data.splitlines())
+        record1, record2 = list(res)
+       
+        self.assertEqual(record1['G2_chip_tax_string'],'Unclassified')
+        self.assertEqual(record1['authors'],'Hernanandez-Eugenio,G., Silva-Rojas,H.V., Zelaya-Molina,L.X.')
+        self.assertEqual(record1['bel3_div_ratio'],'')
+        self.assertEqual(len(record1), 72)
+
+        self.assertEqual(record2['ncbi_acc_w_ver'],'FJ832719.1')
+        self.assertEqual(record2['timestamp'],'2010-03-23 14:08:27')
+        self.assertEqual(record2['title'],'Developmental Microbial Ecology of the Crop of the Folivorous Hoatzin')
+    
+    def test_SpecificGreengenesParser_real(self):
+        """Test SpecificGreengenesParser against real data"""
+        fields = ['prokMSA_id','journal']
+        res = SpecificGreengenesParser(real_data.splitlines(), fields)
+        records = list(res)
+        exp = [('604868',''),('604867','ISME J (2010) In press')]
+        self.assertEqual(records, exp)
+
+    def test_make_ignore_f(self):
+        """Properly ignore empty records and the start line"""
+        f = make_ignore_f('testing')
+        self.assertFalse(f(['asasdasd','']))
+        self.assertFalse(f(['test','']))
+        self.assertFalse(f(['testing2','']))
+        self.assertFalse(f(['testing','asd']))
+        self.assertTrue(f(['','']))
+        self.assertTrue(f(None))
+        self.assertTrue(f(['','']))
+        self.assertTrue(f(['testing','']))
+
+mock_data = """my_starting
+a=1
+b=2
+c=3
+d=
+e=5
+my_ending
+
+my_starting
+q=asdasd
+c=taco
+my_ending
+"""
+
+real_data = """BEGIN
+G2_chip_tax_string=Unclassified
+G2_chip_tax_string_format_2=Unclassified
+HOMD_tax_string=
+HOMD_tax_string_format_2=
+Hugenholtz_tax_string=Unclassified
+Hugenholtz_tax_string_format_2=Unclassified
+Ludwig_tax_string=Unclassified
+Ludwig_tax_string_format_2=Unclassified
+Pace_tax_string=Unclassified
+Pace_tax_string_format_2=Unclassified
+RDP_tax_string=Unclassified
+RDP_tax_string_format_2=Unclassified
+Silva_tax_string=Unclassified
+Silva_tax_string_format_2=Unclassified
+authors=Hernanandez-Eugenio,G., Silva-Rojas,H.V., Zelaya-Molina,L.X.
+bel3_div_ratio=
+bellerophon=
+blast_perc_ident_to_template=
+clone=51a 
+contact_info=Irrigacion, Universidad Autonoma Chapingo, Carretera Mexico-Texcoco Km 37.5, Texcoco, Mexico 56230, Mexico
+core_set_member=
+core_set_member2=
+country=Mexico: Mexico City 
+create_date=21-NOV-2009
+db_name=
+decision=clone
+description=Uncultured bacterium clone 51a 16S ribosomal RNA gene, partial sequence
+email=
+gold_id=
+img_oid=
+isolate=
+isolation_source=mesophilic anaerobic reactor fed with effluent from the chemical industry 
+journal=
+longest_insertion=
+medline_ids=
+ncbi_acc=
+ncbi_acc_w_ver=FJ461956.1
+ncbi_gi=213390944
+ncbi_seq_length=1512
+ncbi_tax_id=77133
+ncbi_tax_string=Bacteria; environmental samples
+ncbi_tax_string_format_2=Unclassified
+non_ACGT_count=
+non_ACGT_percent=
+note=
+organism=uncultured bacterium
+perc_ident_to_invariant_core=
+prokMSA_id=604868
+prokMSAname=Microbial ecology industrial digestor mesophilic anaerobic reactor fed effluent chemical industry clone 51a
+pubmed_ids=
+remark=
+replaced_by=
+single_nt_runs_over_7=
+small_gap_intrusions=
+source=uncultured bacterium
+span_aligned=1..2
+specific_host=
+status=0
+strain=
+study_id=38002
+sub_species=
+submit_date=24-OCT-2008
+template=
+timestamp=2010-03-23 14:08:27
+title=Microbial ecology of industrial anaerobic digestor
+unaligned_length=
+update_date=21-NOV-2009
+warning=
+wigeon95=
+wigeon99=
+wigeon_std_dev=
+aligned_seq=unaligned
+END
+
+BEGIN
+G2_chip_tax_string=Unclassified
+G2_chip_tax_string_format_2=Unclassified
+HOMD_tax_string=
+HOMD_tax_string_format_2=
+Hugenholtz_tax_string=Unclassified
+Hugenholtz_tax_string_format_2=Unclassified
+Ludwig_tax_string=Unclassified
+Ludwig_tax_string_format_2=Unclassified
+Pace_tax_string=Unclassified
+Pace_tax_string_format_2=Unclassified
+RDP_tax_string=Unclassified
+RDP_tax_string_format_2=Unclassified
+Silva_tax_string=Unclassified
+Silva_tax_string_format_2=Unclassified
+authors=Brodie,E.L., Dominguez-Bello,M.G., Garcia-Amado,M.A., Godoy-Vitorino,F., Goldfarb,K.C., Michelangeli,F.
+bel3_div_ratio=
+bellerophon=
+blast_perc_ident_to_template=
+clone=J3Q101_11C02 
+contact_info=Biology, University of Puerto Rico, Rio Piedras Campus, PO Box 23360, San Juan, PR 00931-3360, USA
+core_set_member=
+core_set_member2=
+country=Venezuela 
+create_date=10-DEC-2009
+db_name=
+decision=clone
+description=Uncultured bacterium clone J3Q101_11C02 16S ribosomal RNA gene, partial sequence
+email=
+gold_id=
+img_oid=
+isolate=
+isolation_source=crop contents 
+journal=ISME J (2010) In press
+longest_insertion=
+medline_ids=
+ncbi_acc=
+ncbi_acc_w_ver=FJ832719.1
+ncbi_gi=226447371
+ncbi_seq_length=1326
+ncbi_tax_id=77133
+ncbi_tax_string=Bacteria; environmental samples
+ncbi_tax_string_format_2=Unclassified
+non_ACGT_count=
+non_ACGT_percent=
+note=
+organism=uncultured bacterium
+perc_ident_to_invariant_core=
+prokMSA_id=604867
+prokMSAname=Microbial Ecology Crop Folivorous Hoatzin crop contents clone J3Q101_11C02
+pubmed_ids=
+remark=
+replaced_by=
+single_nt_runs_over_7=
+small_gap_intrusions=
+source=uncultured bacterium
+span_aligned=1..2
+specific_host=
+status=0
+strain=
+study_id=37901
+sub_species=
+submit_date=16-MAR-2009
+template=
+timestamp=2010-03-23 14:08:27
+title=Developmental Microbial Ecology of the Crop of the Folivorous Hoatzin
+unaligned_length=
+update_date=10-DEC-2009
+warning=
+wigeon95=
+wigeon99=
+wigeon_std_dev=
+aligned_seq=unaligned
+END
+"""
+
+if __name__ == '__main__':
+    main()
+
