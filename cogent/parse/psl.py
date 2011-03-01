6b4b10ff152fe40aded5a81d3fd241b88997dc6b
Gavin Huttley <gavin.huttley@anu.edu.au>
2011-03-01 04:30:54 +0000

    adding new parsers
    [NEW] a parser for bowtie output
    [NEW] a parser for BLATs default PSL format
    
    Contributed by Anuj Pahwa. Thanks Anuj!

---
+++ cogent/parse/psl.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+"""Parser for PSL format (default output by blat).
+   Compatible with blat v.34
+"""
+
+from cogent import LoadTable
+from cogent.parse.table import ConvertFields
+
+__author__ = "Gavin Huttley, Anuj Pahwa"
+__copyright__ = "Copyright 2007-2011, The Cogent Project"
+__credits__ = ["Rob Knight","Peter Maxwell", "Gavin Huttley", "Anuj Pahwa"]
+__license__ = "GPL"
+__version__ = "1.6.0.dev"
+__maintainer__ = "Gavin Huttley"
+__email__ = "gavin.huttley@anu.edu.au"
+__status__ = "Development"
+
+def make_header(lines):
+    """returns one header line from multiple header lines"""
+    lengths = map(len, lines)
+    max_length = max(lengths)
+    for index, line in enumerate(lines):
+        if lengths[index] != max_length:
+            for i in range(lengths[index], max_length):
+                line.append('')
+    
+    header = []
+    for t, b in zip(*lines):
+        if t.strip().endswith('-'):
+            c = t.strip()+b
+        else:
+            c = ' '.join([t.strip(), b.strip()])
+        header += [c.strip()]
+    return header
+
+int_series = lambda x: map(int, x.replace(',',' ').split())
+
+row_converter = ConvertFields([(i, int) for i in range(8)]+\
+                              [(i, int) for i in range(10, 13)]+\
+                              [(i, int) for i in range(14, 18)]+\
+                              [(i, int_series) for i in range(18, 21)])
+
+def MinimalPslParser(data, row_converter=row_converter):
+    """returns version, header and rows from data"""
+    if type(data) == str:
+        data = open(data)
+    
+    psl_version = None
+    header = None
+    rows = []
+    
+    for record in data:
+        if psl_version is None:
+            assert 'psLayout version' in record
+            psl_version = record.strip()
+            yield psl_version
+            continue
+        
+        if not record.strip():
+            continue
+        
+        if header is None and record[0] == '-':
+            header = make_header(rows)
+            yield header
+            rows = []
+            continue
+        
+        rows += [record.rstrip().split('\t')]
+        if header is not None:
+            yield row_converter(rows[0])
+            rows = []
+        
+    
+
+def PslToTable(data):
+    """converts psl format to a table"""
+    parser = MinimalPslParser(data)
+    version = parser.next()
+    header = parser.next()
+    rows = [row for row in parser]
+    table = LoadTable(header=header, rows=rows, title=version)
+    return table
---
+++ tests/test_parse/test_bowtie.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+"""Unit tests for the bowtie default output parser.
+   Compatible with bowtie version 0.12.5
+"""
+
+from cogent.parse.bowtie import BowtieOutputParser, BowtieToTable
+from cogent.util.unit_test import TestCase, main
+from cogent import LoadTable
+
+__author__ = "Gavin Huttley, Anuj Pahwa"
+__copyright__ = "Copyright 2007-2011, The Cogent Project"
+__credits__ = ["Rob Knight","Peter Maxwell", "Gavin Huttley", "Anuj Pahwa"]
+__license__ = "GPL"
+__version__ = "1.6.0.dev"
+__maintainer__ = "Gavin Huttley"
+__email__ = "gavin.huttley@anu.edu.au"
+__status__ = "Development"
+
+fname = 'data/bowtie_output.map'
+expected = [['GAPC_0015:6:1:1283:11957#0/1', '-', 'Mus', 66047927, 'TGTATATATAAACATATATGGAAACTGAATATATATACATTATGTATGTATATATGTATATGTTATATATACATA', 'IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII', 0, ['55:A>G', '64:C>A']],
+        ['GAPC_0015:6:1:1394:18813#0/1', '+', 'Mus', 77785518, 'ATGAAATTCCTAGCCAAATGGATGGACCTGGAGGGCATCATC', 'IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII', 447, []],
+        ['GAPC_0015:6:1:1560:18056#0/1', '+', 'Mus', 178806665, 'TAGATAAAGGCTCTGTTTTTCATCATTGAGAAATTGTTATTTTTCTGATGTTATA', 'IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII', 0, ['9:T>G']],
+        ['GAPC_0015:6:1:1565:19849#0/1', '+', 'Mus', 116516430, 'ACCATTTGCTTGGAAAATTGTTTTCCAGCCTTTCACTCTGAG', 'IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII', 141, []],
+        ['GAPC_0015:6:1:1591:17397#0/1', '-', 'Mus', 120440696, 'TCTAAATCTGTTCATTAATTAAGCCTGTTTCCATGTCCTTGGTCTTAAGACCAATCTGTTATGCGGGTGTGA', 'IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII', 0, ['70:A>C', '71:G>T']]]
+
+class BowtieOutputTest(TestCase):
+    
+    def test_parsing(self):
+        """make sure that the bowtie output file is parsed properly"""
+        parser = BowtieOutputParser(fname)
+        header = parser.next()
+        index = 0
+        for row in parser:
+            self.assertEqual(row, expected[index])
+            index += 1
+    
+    def test_psl_to_table(self):
+        """make sure that the table is built without any errors"""
+        table = BowtieToTable(fname)
+    
+    def test_getting_seq_coords(self):
+        """get correct information from the table"""
+        table = BowtieToTable(fname)
+        index = 0
+        for row in table:
+            query_name = row['Query Name']
+            strand_direction = row['Strand Direction']
+            query_offset = row['Offset']
+            self.assertEqual(query_name, expected[index][0])
+            self.assertEqual(strand_direction, expected[index][1])
+            self.assertEqual(query_offset, expected[index][3])
+            index += 1
+        
+    
+
+if __name__ == "__main__":
+    main()
