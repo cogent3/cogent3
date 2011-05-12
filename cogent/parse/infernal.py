800154bcbb23ed5bd98188ff77c91bd26b4e2ea0
Jeremy Widmann <jeremy.widmann@colorado.edu>
2011-05-12 19:13:51 +0000

    Added CmalignScoreParser and tests.

--- cogent/parse/infernal.py
+++ cogent/parse/infernal.py
@@ -47,4 +47,39 @@ def CmsearchParser(lines):
     
     return cmsearch_parser(good_lines)
 
+def CmalignScoreParser(lines):
+    """Parser for tabfile format cmalign score result.
     
+        - IMPORTANT: Will only parse standard output from cmalign.
+                
+        - NOTE: Will only work with search result files with a single CM
+            as a query.  Will not work with multiple alignment result files
+            that have been concatenated.
+        
+        - Result will be list of hits with following order:
+        [seq idx, seq name, seq len, total bit score, struct bit score,
+            avg prob, elapsed time]
+        
+    """
+    # Converting indices and %GC to integers and bit score to float.
+    # Since E-value is only present if CM is calibrated, leaving as string.
+    conversion_fields = [(0,int),(2,int),(3,float),(4,float),(5,float)]
+    cmalign_score_converter = ConvertFields(conversion_fields)
+    #Ignore hash characters
+    good_lines = []
+    for l in lines:
+        line = l.strip()
+        if line.startswith('# STOCKHOLM 1.0'):
+            break
+        if line and (not line.startswith('#')):
+            good_lines.append(l)
+    #make parser
+    cmalign_score_parser = SeparatorFormatParser(with_header=False,\
+                                            converter=cmalign_score_converter,\
+                                            ignore=None,\
+                                            sep=None)
+    
+    return cmalign_score_parser(good_lines)
+
+    
+
--- tests/test_parse/test_infernal.py
+++ tests/test_parse/test_infernal.py
@@ -2,7 +2,7 @@
 # test_infernal.py
 
 from cogent.util.unit_test import TestCase, main
-from cogent.parse.infernal import CmsearchParser
+from cogent.parse.infernal import CmsearchParser,CmalignScoreParser
 
 __author__ = "Jeremy Widmann"
 __copyright__ = "Copyright 2007-2011, The Cogent Project"
@@ -62,6 +62,47 @@ class CmsearchParserTests(TestCase):
         parser = CmsearchParser(SEARCH_DATA.split('\n'))
         self.assertEqual(list(parser),self.search_res)
 
+class CmalignScoreParserTests(TestCase):
+    """Tests for CmalignScoreParser.
+    """
+    
+    def setUp(self):
+        """setup for CmalignScoreParserTests.
+        """
+
+        self.basic_results_hits = """# command: data
+# date:
+#
+# cm summary
+        1  Target_1     83     55.02      2.94     0.956  00:00:00.01
+        2  Target_2     84     53.31      4.42     0.960  00:00:00.01
+# post alignment summary
+        """
+        self.basic_res = [[1,'Target_1',83,55.02,2.94,0.956,'00:00:00.01'],\
+                          [2,'Target_2',84,53.31,4.42,0.960,'00:00:00.01']]
+        
+        self.search_res = \
+            [[1,'AABL01002928.1/2363-2445',83,55.02,2.94,0.956,'00:00:00.01'],\
+             [2,'AACV01025780.1/26051-26134',84,53.31,4.42,0.960,'00:00:00.01']]
+    
+    def test_cmalign_score_parser_no_data(self):
+        """CmalignScoreParser should return correct result given no data.
+        """
+        parser = CmalignScoreParser([])
+        self.assertEqual(list(parser),[])
+        
+    def test_cmalign_score_parser_basic(self):
+        """CmalignScoreParser should return correct result given basic output.
+        """
+        parser = CmalignScoreParser(self.basic_results_hits.split('\n'))
+        self.assertEqual(list(parser),self.basic_res)
+        
+    def test_cmalign_score_parser_full(self):
+        """CmalignScoreParser should return correct result given cmalign output.
+        """
+        parser = CmalignScoreParser(ALIGN_DATA.split('\n'))
+        self.assertEqual(list(parser),self.search_res)
+
 
 SEARCH_DATA = """# command:    cmsearch -T 0.0 --tabfile /tmp/tmpQGr0PGVeaEvGUkw2TM3e.txt --informat FASTA /tmp/tmp40hq0MqFPLn2lAymQeAD.txt /tmp/tmplTEQNgv0UA7sFSV0Z2RL.txt
 # date:       Mon Nov  8 13:51:12 2010
@@ -96,6 +137,40 @@ SEARCH_DATA = """# command:    cmsearch -T 0.0 --tabfile /tmp/tmpQGr0PGVeaEvGUkw
 # -----------
 #    00:00:00"""
 
+ALIGN_DATA = """# cmalign :: align sequences to an RNA CM
+# INFERNAL 1.0.2 (October 2009)
+# Copyright (C) 2009 HHMI Janelia Farm Research Campus
+# Freely distributed under the GNU General Public License (GPLv3)
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# command: cmalign orig_alignments/RF01057_3NPQ_chain_A_results.cm RF01057_two_seqs.fasta
+# date:    Thu May 1 13:00:32 2011
+#
+# cm name                    algorithm  config  sub  bands     tau
+# -------------------------  ---------  ------  ---  -----  ------
+# RF01057_3NPQ_chain_A_resu    opt acc  global   no    hmm   1e-07
+#
+#                                                 bit scores                           
+#                                             ------------------                       
+# seq idx  seq name                      len     total    struct  avg prob      elapsed
+# -------  --------------------------  -----  --------  --------  --------  -----------
+        1  AABL01002928.1/2363-2445       83     55.02      2.94     0.956  00:00:00.01
+        2  AACV01025780.1/26051-26134     84     53.31      4.42     0.960  00:00:00.01
+
+# STOCKHOLM 1.0
+#=GF AU Infernal 1.0.2
+
+AABL01002928.1/2363-2445   CGCGCCGAGGAGCGCUGCGACGGCCCG...UCGAGGGCCGCCAGGCUCGG
+AACV01025780.1/26051-26134 CCUGCCGAGGGGCGCUGCGACCGGAUCcaaUGAGGCCCGGCCAGGCUCGG
+#=GC SS_cons               :::::::::<--<<<--<--<<_____...________>>->--------
+#=GC RF                    CuuuCCGAGGAGCGCUGcAACgGgcuc...uuacggcccGCcAGGCUCGG
+
+AABL01002928.1/2363-2445   CGGGG...ACAAucgguUUUCCAACGGCGSUCUGUUUAU
+AACV01025780.1/26051-26134 UAAGGuggCUUU.....GUAACAACGGCGCCCGGCUAGA
+#=GC SS_cons               -----...----.....--------->>>-->:::::::
+#=GC RF                    aaagG...uaaa.....ccuaCAACGGCGCUCAcuCaca
+//
+#
+# CPU time: 0.02u 0.00s 00:00:00.02 Elapsed: 00:00:00"""
 
 if __name__ == '__main__':
     main()
