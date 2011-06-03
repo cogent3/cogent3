04870c1f04403b1ca7744b99e25343a0d0a1f245
Kyle Bittinger <kylebittinger@gmail.com>
2011-06-03 19:45:55 +0000

    Now testing Sfffile app controller by parsing the output sff files, using cogent.parse.binary_sff

--- tests/test_app/test_sfffile.py
+++ tests/test_app/test_sfffile.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 # test_sfffile.py
 
-import hashlib
 import os
-import shutil
 import tempfile
 from cogent.util.unit_test import TestCase, main
+from cogent.parse.binary_sff import parse_binary_sff
 from cogent.app.util import ApplicationError
 from cogent.app.sfffile import Sfffile
+
 
 
 __author__ = "Kyle Bittinger"
@@ -23,10 +23,19 @@ __status__ = "Prototype"
 class SfffileTests(TestCase):
     """Test the Sfffile application controller.
     """
-
     def setUp(self):
         test_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
         self.sff_fp = os.path.join(test_dir, 'data', 'test.sff')
+
+    def _check_unmodified_sff_contents(self, sff_file):
+        """Extracting repeated code from sfffile tests"""
+        sff_file.seek(0)
+        header, reads_gen = parse_binary_sff(sff_file)
+        reads = list(reads_gen)
+
+        self.assertEqual(header["number_of_reads"], 1)
+        self.assertEqual(len(reads), 1)
+        self.assertEqual(reads[0]['Name'], 'FA6P1OK01CGMHQ')
 
     def test_exit_status(self):
         """Sfffile should raise ApplicationError if exit status is nonzero."""
@@ -37,12 +46,7 @@ class SfffileTests(TestCase):
         """Simple sfffile call should produce expected output."""
         a = Sfffile()
         app_results = a(self.sff_fp)
-
-        # compare md5 digests
-        expected = hashlib.md5(open(self.sff_fp, 'rb').read()).hexdigest()
-        observed = hashlib.md5(app_results['sff'].read()).hexdigest()
-        self.assertEqual(observed, expected)
-
+        self._check_unmodified_sff_contents(app_results['sff'])
         app_results.cleanUp()
 
     def test_call_with_output_path(self):
@@ -52,14 +56,8 @@ class SfffileTests(TestCase):
         a.Parameters['-o'].on(output_fp)
         app_results = a(self.sff_fp)
 
-        expected = hashlib.md5(open(self.sff_fp, 'rb').read()).hexdigest()
-        observed = hashlib.md5(open(output_fp, 'rb').read()).hexdigest()
-        self.assertEqual(observed, expected)
-
-        # Double check that app_results are still valid
-        observed = hashlib.md5(app_results['sff'].read()).hexdigest()
-        self.assertEqual(observed, expected)
-
+        self._check_unmodified_sff_contents(open(output_fp))
+        self._check_unmodified_sff_contents(app_results['sff'])
         app_results.cleanUp()
 
     def test_call_with_included_accession_numbers(self):
@@ -72,10 +70,7 @@ class SfffileTests(TestCase):
         a.Parameters['-i'].on(accno_file.name)
         app_results = a(self.sff_fp)
 
-        expected = hashlib.md5(open(self.sff_fp, 'rb').read()).hexdigest()
-        observed = hashlib.md5(app_results['sff'].read()).hexdigest()
-        self.assertEqual(observed, expected)
-
+        self._check_unmodified_sff_contents(app_results['sff'])
         app_results.cleanUp()
 
     def test_call_with_excluded_accession_numbers(self):
@@ -88,10 +83,11 @@ class SfffileTests(TestCase):
         a.Parameters['-e'].on(accno_file.name)
         app_results = a(self.sff_fp)
 
-        expected = hashlib.md5(open(self.sff_fp, 'rb').read()).hexdigest()
-        observed = hashlib.md5(app_results['sff'].read()).hexdigest()
-        self.assertNotEqual(observed, expected)
+        header, reads_gen = parse_binary_sff(app_results['sff'])
+        reads = list(reads_gen)
 
+        self.assertEqual(header["number_of_reads"], 0)
+        self.assertEqual(len(reads), 0)
         app_results.cleanUp()
 
 
