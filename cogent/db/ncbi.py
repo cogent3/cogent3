eb1af0a67307029f9555370d6b0c29595dac7b66
Mike Robeson <mike.robeson@colorado.edu>
2011-08-04 19:02:38 +0000

    I changed lines 256-269 in ncbi.py to like this:
    ##### BEGIN NEW CODE #####
    try:
                self.query_key = search_result.QueryKey
                self.WebEnv = search_result.WebEnv
            except AttributeError:
                #The query_key and/or WebEnv not Found!
                #move on to extract UID list directly from the search result
                try:
                    self.id = ','.join(search_result.IdList)
                except AttributeError:
                    raise QueryNotFoundError, \
                    "Query %s returned no results.\nURL was:\n%s" % \
                    (repr(query),str(search_query))
    
            count = search_result.Count
    
    ##### END NEW CODE #####
    
    
    ##### BEGIN ORIGINAL CODE #####
    try:
                self.query_key = search_result.QueryKey
            except AttributeError:
                    raise QueryNotFoundError, \
                    "Query %s returned no results.\nURL was:\n%s" % \
                    (repr(query),str(search_query))
    
            count = search_result.Count
    ##### END ORIGINAL CODE #####

--- cogent/db/ncbi.py
+++ cogent/db/ncbi.py
@@ -255,11 +255,17 @@ class EUtils(object):
             print search_result
         try:
             self.query_key = search_result.QueryKey
+            self.WebEnv = search_result.WebEnv
         except AttributeError:
-            raise QueryNotFoundError, \
-            "Query %s returned no results.\nURL was:\n%s" % \
-            (repr(query),str(search_query))
-        self.WebEnv = search_result.WebEnv
+            #The query_key and/or WebEnv not Found!
+            #move on to extract UID list directly from the search result
+            try:
+                self.id = ','.join(search_result.IdList) 
+            except AttributeError:
+                raise QueryNotFoundError, \
+                "Query %s returned no results.\nURL was:\n%s" % \
+                (repr(query),str(search_query))
+
         count = search_result.Count
 
         #wrap the fetch in a loop so we get all the results
