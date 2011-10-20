c18a5852c533e776510365f9f9703d6dd9ff3569
Jesse Stombaugh <jistombaugh@gmail.com>
2011-10-20 15:44:57 +0000

    updated and removed the __call__ override in guppy

--- cogent/app/guppy.py
+++ cogent/app/guppy.py
@@ -1,14 +1,5 @@
 #!/usr/bin/env python
 """Application controller for guppy 1.1"""
-
-from cogent.app.parameters import ValuedParameter, FlagParameter
-from cogent.app.util import CommandLineApplication, FilePath, system, \
-       CommandLineAppResult, ResultPath, remove, ApplicationError
-from cogent.core.alignment import Alignment
-from os.path import splitext,split,join
-from os import listdir
-from cogent.parse.tree import DndParser
-from cogent.core.tree import PhyloNode
        
 __author__ = "Jesse Stombaugh"
 __copyright__ = "Copyright 2007-2011, The Cogent Project"
@@ -18,6 +9,15 @@ __version__ = "1.6.0.dev"
 __maintainer__ = "Jesse Stombaugh"
 __email__ = "jesse.stombaugh@colorado.edu"
 __status__ = "Prototype"
+
+from cogent.app.parameters import ValuedParameter, FlagParameter
+from cogent.app.util import CommandLineApplication, FilePath, system, \
+       CommandLineAppResult, ResultPath, remove, ApplicationError
+from cogent.core.alignment import Alignment
+from os.path import splitext,split,join
+from os import listdir
+from cogent.parse.tree import DndParser
+from cogent.core.tree import PhyloNode
 
 class Guppy(CommandLineApplication):
     """guppy Application Controller
@@ -121,69 +121,24 @@ class Guppy(CommandLineApplication):
         '-help': FlagParameter('-', Name='help'),
     }
  
-    def __call__(self,fname=None, remove_tmp=True):
-        """Run the application with the specified kwargs on data
-        
-            data: anything that can be cast into a string or written out to
-                a file. Usually either a list of things or a single string or 
-                number. input_handler will be called on this data before it 
-                is passed as part of the command-line argument, so by creating
-                your own input handlers you can customize what kind of data
-                you want your application to accept
-
-            remove_tmp: if True, removes tmp files
-
-            NOTE: Override of the base class to handle redirected output
-        """
-        input_handler = self.InputHandler
-        suppress_stdout = self.SuppressStdout
-        suppress_stderr = self.SuppressStderr
-
-        outfile = self.getTmpFilename(self.TmpDir)
-        self._outfile = outfile
-        
-        if suppress_stderr:
-            errfile = FilePath('/dev/null')
-        else:
-            errfile = FilePath(self.getTmpFilename(self.TmpDir))
-        
-        if fname is None:
-            self._input_filename = ''
-        else:
-            self._input_filename = fname
-        
-        # Build up the command, consisting of a BaseCommand followed by
-        # input and output (file) specifications
-        command = self._command_delimiter.join(filter(None,\
-            [self.BaseCommand,str(self._input_filename),'>',str(outfile),'2>',\
-                str(errfile)]))
-
-        if self.HaltExec:
-            raise AssertionError, "Halted exec with command:\n" + command
-        # The return value of system is a 16-bit number containing the signal 
-        # number that killed the process, and then the exit status. 
-        # We only want to keep the exit status so do a right bitwise shift to 
-        # get rid of the signal number byte
-        exit_status = system(command) >> 8
-
-        # Determine if error should be raised due to exit status of 
-        # appliciation
-        if not self._accept_exit_status(exit_status):
-            raise ApplicationError, \
-             'Unacceptable application exit status: %s, command: %s'\
-                % (str(exit_status),command)
-
-        out = open(outfile,"r")
-        err = None
-        if not suppress_stderr:
-            err = open(errfile,"r")
-        
-        result =  CommandLineAppResult(out,err,exit_status,\
-            result_paths=self._get_result_paths())
-        
-        return result
-
-    def _get_result_paths(self):
+    def getTmpFilename(self, tmp_dir="/tmp",prefix='tmp',suffix='.json',\
+           include_class_id=False,result_constructor=FilePath):
+        """ Define Tmp filename to contain .json suffix, since guppy requires
+            the suffix to be .json """
+            
+        return super(Guppy,self).getTmpFilename(tmp_dir=tmp_dir,
+                                    prefix=prefix,
+                                    suffix=suffix,
+                                    include_class_id=include_class_id,
+                                    result_constructor=result_constructor)
+                                    
+    def _handle_app_result_build_failure(self,out,err,exit_status,result_paths):
+        """ Catch the error when files are not produced """
+        raise ApplicationError, \
+         'Guppy failed to produce an output file due to the following error: \n\n%s ' \
+         % err.read()
+    
+    def _get_result_paths(self,data):
         basepath,basename=split(splitext(self._input_filename)[0])
         outfile_list=listdir(split(self._input_filename)[0])
         result = {}
@@ -195,15 +150,15 @@ class Guppy(CommandLineApplication):
         return result
     
 def build_tree_from_json_using_params(fname,output_dir='/tmp/',params={}):
-    """Returns a tree from Alignment object aln.
-
-    aln: an xxx.Alignment object, or data that can be used to build one.
-
-    moltype: cogent.core.moltype.MolType object
-
+    """Returns a tree from a json.
+    
+    fname: filepath to input json 
+    
+    output_dir: location of output files
+    
     params: dict of parameters to pass in to the RAxML app controller.
 
-    The result will be an xxx.Alignment object, or None if tree fails.
+    The result will be a Tree.
     """
 
     # convert aln to fasta in case it is not already a fasta file
@@ -214,12 +169,24 @@ def build_tree_from_json_using_params(fname,output_dir='/tmp/',params={}):
                       InputHandler=ih,
                       WorkingDir=output_dir,
                       SuppressStderr=True,
-                      SuppressStdout=True)
+                      SuppressStdout=True,
+                      HALT_EXEC=False)
                       
-    guppy_result = guppy_app(fname)
+    guppy_result = guppy_app(open(fname).read())
     
-    new_tree=guppy_result['result'].read()
-
+    try:
+        new_tree=guppy_result['result'].read()
+    except:
+        # catch the error of not producing any results and print the command
+        # run so user can check error
+        guppy_cmd=Guppy(params=params,
+                      InputHandler=ih,
+                      WorkingDir=output_dir,
+                      SuppressStderr=True,
+                      SuppressStdout=True,
+                      HALT_EXEC=True)
+        out_msg=guppy_cmd(open(fname).read())
+        
     tree = DndParser(new_tree, constructor=PhyloNode)
     
     guppy_result.cleanUp()
