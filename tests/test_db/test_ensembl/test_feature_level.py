a2cea8949db163824cf02929a41376f6863690b8
Gavin Huttley <gavin.huttley@anu.edu.au>
2012-02-02 11:22:35 +0000

    forgot to revert the tested Ensembl release to 65 after checking earlier release still passed.

--- tests/test_db/test_ensembl/test_feature_level.py
+++ tests/test_db/test_ensembl/test_feature_level.py
@@ -16,7 +16,7 @@ __maintainer__ = "Gavin Huttley"
 __email__ = "Gavin.Huttley@anu.edu.au"
 __status__ = "alpha"
 
-Release = 64
+Release = 65
 
 if 'ENSEMBL_ACCOUNT' in os.environ:
     args = os.environ['ENSEMBL_ACCOUNT'].split()
