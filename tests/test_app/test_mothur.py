58f27d7a5aba1165ba28e50c729d1aa9be4db0a7
Jai Ram Rideout <jai.rideout@gmail.com>
2012-08-31 03:50:25 +0000

    Included test_formatdb and test_mothur in alltests.py. Synced test_mothur with QIIME's pycogent backports.

--- tests/test_app/test_mothur.py
+++ tests/test_app/test_mothur.py
@@ -4,25 +4,20 @@
 
 from __future__ import with_statement
 from cStringIO import StringIO
-from os import remove, rmdir, listdir, path
-import shutil
-from tempfile import mkdtemp, mkstemp, NamedTemporaryFile
-
+from os import remove, rmdir
+from tempfile import mkdtemp, mkstemp
 from cogent.util.unit_test import TestCase, main
-from cogent.app.mothur import (
-    Mothur, mothur_from_file,
-    MothurClassifySeqs, parse_mothur_assignments, mothur_classify_file, 
-    )
+from cogent.app.mothur import Mothur, mothur_from_file
 
 
 __author__ = "Kyle Bittinger"
 __copyright__ = "Copyright 2007-2012, The Cogent Project"
-__credits__ = ["Kyle Bittinger"]
+__credits__ = ["Kyle Bittinger", "Jose Carlos Clemente Litran"]
 __license__ = "GPL"
 __version__ = "1.6.0dev"
 __maintainer__ = "Kyle Bittinger"
 __email__ = "kylebittinger@gmail.com"
-__status__ = "Prototype"
+__status__ = "Development"
 
 
 class MothurTests(TestCase):
@@ -155,190 +150,6 @@ class MothurTests(TestCase):
         f.seek(0)
         parsed_otus = mothur_from_file(f)
         self.assertEquals(parsed_otus, self.small_otus_parsed)
-
-
-class MothurClassifySeqsTests(TestCase):
-    def setUp(self):
-        self.ref_seqs_file = NamedTemporaryFile(suffix=".fasta")
-        self.ref_seqs_file.write(ref_seqs)
-        self.ref_seqs_file.seek(0)
-        
-        self.taxonomy_file = NamedTemporaryFile(suffix=".tax")
-        self.taxonomy_file.write(taxonomy)
-        self.taxonomy_file.seek(0)
-
-        self.params = {
-            "reference": self.ref_seqs_file.name,
-            "taxonomy": self.taxonomy_file.name,
-            }
-    
-    def test_compile_mothur_script(self):
-        app = MothurClassifySeqs({
-            "reference": "/var/data/ref seqs.fasta",
-            "taxonomy": "/var/data/taxonomy.txt",
-            })
-        app._input_filename = 'test.fasta'
-        observed_script = app._compile_mothur_script()
-        expected_script = (
-            '#classify.seqs(fasta=test.fasta, '
-            'reference=/var/data/ref seqs.fasta, '
-            'taxonomy=/var/data/taxonomy.txt)'
-            )
-        self.assertEqual(observed_script, expected_script)
-
-    def test_call_with_multiline_string(self):
-        """__call__() should return correct classifications for input as single string"""
-        work_dir = mkdtemp()
-        app = MothurClassifySeqs(self.params, WorkingDir=work_dir)
-        result = app(query_seqs)
-
-        obs_summary = result["summary"].read()
-        self.assertEqual(obs_summary, expected_summary)
-
-        for line in result["assignments"]:
-            self.assertEqual(len(line.split("\t")), 2)
-
-            query_id, taxa_str = line.split("\t")
-            self.assertTrue(query_id.startswith("Seq"))
-            self.assertEqual(taxa_str.count(";"), 6)
-
-        result.cleanUp()
-        rmdir(work_dir)
-
-    def test_cutoff_iters_ksize(self):
-        work_dir = mkdtemp()
-        self.params["cutoff"] = 99
-        self.params["iters"] = 352
-        self.params["ksize"] = 5
-        app = MothurClassifySeqs(self.params, WorkingDir=work_dir)
-        result = app(query_seqs)
-
-        # Only exact match will remain with 99% cutoff
-        f = result["assignments"]
-        self.assertTrue(next(f).startswith("SeqA	Bacteria(100)"))
-        self.assertTrue(next(f).startswith("SeqB	unknown"))
-        self.assertTrue(next(f).startswith("SeqC	unknown"))
-
-        result.cleanUp()
-        rmdir(work_dir)
-
-    def test_parse_mothur_assignments(self):
-        assignments = StringIO(
-            "SeqA	Bacteria(100);Firmicutes(100);Clostridia(100);"
-            "Clostridiales(100);Ruminococcaceae(100);unclassified;\n"
-            "SeqB	Bacteria(80);Firmicutes(80);Bacilli(71);"
-            "Lactobacillales(71);Streptococcaceae(58);Streptococcus(58);\n"
-            "SeqC	Archaea(66);Euryarchaeota(66);Methanobacteria(66);"
-            "Methanobacteriales(66);Methanobrevibacter(66);unclassified;\n\n"
-            )
-        expected = [
-            ("SeqA", ["Bacteria", "Firmicutes", "Clostridia",
-              "Clostridiales", "Ruminococcaceae"], 1.0),
-            ("SeqB", ["Bacteria", "Firmicutes", "Bacilli",
-              "Lactobacillales", "Streptococcaceae", "Streptococcus"], 0.58),
-            ("SeqC", ["Archaea", "Euryarchaeota", "Methanobacteria",
-              "Methanobacteriales", "Methanobrevibacter"], 0.66),
-            ]
-        observed = list(parse_mothur_assignments(assignments))
-        self.assertEqual(observed, expected)
-
-    def test_mothur_classify_file(self):
-        assignments = mothur_classify_file(
-            StringIO(query_seqs),
-            self.ref_seqs_file.name,
-            self.taxonomy_file.name)
-        self.assertEqual(list(sorted(assignments)), ["SeqA", "SeqB", "SeqC"])
-
-    def test_mothur_classify_filepath_with_dash(self):
-        temp_dir = mkdtemp()
-
-        ref_fp = path.join(temp_dir, "my-refs.fasta")
-        ref_file = open(ref_fp, "w")
-        ref_file.write(ref_seqs)
-        ref_file.close()
-
-        try:
-            assignments = mothur_classify_file(
-                StringIO(query_seqs),
-                ref_fp,
-                self.taxonomy_file.name)
-            seq_ids = assignments.keys()
-            seq_ids.sort()
-            self.assertEqual(seq_ids, ["SeqA", "SeqB", "SeqC"])
-        finally:
-            shutil.rmtree(temp_dir)
-
-ref_seqs = """\
->100
-GGTTCCAAGGTTCCAAGGTTCCAAGGTTCCAAGGTTCCAAGGTTCCAAGGTTCCAA
-GGTTCCAAGGTTCCAAGGTTCCAAGGTTCCAAGGTTCCAAGGTTCCAAGGTTCCAA
-GGTTCCAAGGTTCCAAGGTTCCAAGGTTCCAAGGTTCCAAGGTTCCAAGGTTCCAA
-GGTTCCAAGGTTCCAAGGTTCCAAGGTTCCAAGGTTCCAAGGTTCCAAGGTTCCAA
->101
-CTGAAGTCCTGAAGTCCTGAAGTCCTGAAGTCCTGAAGTCCTGAAGTCCTGAAGTC
-CTGAAGTCCTGAAGTCCTGAAGTCCTGAAGTCCTGAAGTCCTGAAGTCCTGAAGTC
-CTGAAGTCCTGAAGTCCTGAAGTCCTGAAGTCCTGAAGTCCTGAAGTCCTGAAGTC
-CTGAAGTCCTGAAGTCCTGAAGTCCTGAAGTCCTGAAGTCCTGAAGTCCTGAAGTC
->102
-ATTTGCCCATTTGCCCATTTGCCCATTTGCCCATTTGCCCATTTGCCCATTTGCCC
-ATTTGCCCATTTGCCCATTTGCCCATTTGCCCATTTGCCCATTTGCCCATTTGCCC
-ATTTGCCCATTTGCCCATTTGCCCATTTGCCCATTTGCCCATTTGCCCATTTGCCC
-ATTTGCCCATTTGCCCATTTGCCCATTTGCCCATTTGCCCATTTGCCCATTTGCCC
->103
-TGTGACTATGTGACTATGTGACTATGTGACTATGTGACTATGTGACTATGTGACTA
-TGTGACTATGTGACTATGTGACTATGTGACTATGTGACTATGTGACTATGTGACTA
-TGTGACTATGTGACTATGTGACTATGTGACTATGTGACTATGTGACTATGTGACTA
-TGTGACTATGTGACTATGTGACTATGTGACTATGTGACTATGTGACTATGTGACTA
->104
-CAGGTATTCAGGTATTCAGGTATTCAGGTATTCAGGTATTCAGGTATTCAGGTATT
-CAGGTATTCAGGTATTCAGGTATTCAGGTATTCAGGTATTCAGGTATTCAGGTATT
-CAGGTATTCAGGTATTCAGGTATTCAGGTATTCAGGTATTCAGGTATTCAGGTATT
-CAGGTATTCAGGTATTCAGGTATTCAGGTATTCAGGTATTCAGGTATTCAGGTATT
-"""
-
-taxonomy = """\
-100	Bacteria;Firmicutes;Clostridia;Clostridiales;Ruminococcaceae;
-101	Bacteria;Firmicutes;Clostridia;Clostridiales;Ruminococcaceae;Genus_1;
-102	Bacteria;Firmicutes;Bacilli;Lactobacillales;
-103	Bacteria;Firmicutes;Bacilli;Lactobacillales;Streptococcaceae;Streptococcus;
-104	Archaea;Euryarchaeota;Methanobacteria;Methanobacteriales;Methanobrevibacter;smithii
-"""
-
-query_seqs = """\
->SeqA
-GGTTCCAAGGTTCCAAGGTTCCAAGGTTCCAAGGTTCCAAGGTTCCAAGGTTCCAA
-GGTTCCAAGGTTCCAAGGTTCCAAGGTTCCAAGGTTCCAAGGTTCCAAGGTTCCAA
-GGTTCCAAGGTTCCAAGGTTCCAAGGTTCCAAGGTTCCAAGGTTCCAAGGTTCCAA
-GGTTCCAAGGTTCCAAGGTTCCAAGGTTCCAAGGTTCCAAGGTTCCAAGGTTCCAA
-GGTTCCAAGGTTCCAAGGTTCCAAGGTTCCAAGGTTCCAAGGTTCCAAGGTTCCAA
-GGTTCCAAGGTTCCAAGGTTCCAAGGTTCCAAGGTTCCAAGGTTCCAAGGTTCCAA
->SeqB
-ATTTGCCCATTTGCCCTGTGACTATGTGACTACTGAAGTCATTTGCCC
->SeqC
-CAGGTATTCAGGTATTCAGGTATTGGTTCCAACTGAAGTCATTTGCCC
-"""
-
-expected_summary = """\
-taxlevel\t rankID\t taxon\t daughterlevels\t total\t
-0\t0\tRoot\t2\t3\t
-1\t0.1\tArchaea\t1\t1\t
-2\t0.1.1\tEuryarchaeota\t1\t1\t
-3\t0.1.1.1\tMethanobacteria\t1\t1\t
-4\t0.1.1.1.1\tMethanobacteriales\t1\t1\t
-5\t0.1.1.1.1.1\tMethanobrevibacter\t1\t1\t
-6\t0.1.1.1.1.1.1\tunclassified\t0\t1\t
-1\t0.2\tBacteria\t1\t2\t
-2\t0.2.1\tFirmicutes\t2\t2\t
-3\t0.2.1.1\tBacilli\t1\t1\t
-4\t0.2.1.1.1\tLactobacillales\t1\t1\t
-5\t0.2.1.1.1.1\tStreptococcaceae\t1\t1\t
-6\t0.2.1.1.1.1.1\tStreptococcus\t0\t1\t
-3\t0.2.1.2\tClostridia\t1\t1\t
-4\t0.2.1.2.1\tClostridiales\t1\t1\t
-5\t0.2.1.2.1.1\tRuminococcaceae\t1\t1\t
-6\t0.2.1.2.1.1.2\tunclassified\t0\t1\t
-"""
-
 
 if __name__ == '__main__':
     main()
