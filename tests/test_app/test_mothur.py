a862de9d4730a32c441dc336011c2fdafbb007d7
Kyle Bittinger <kylebittinger@gmail.com>
2012-10-01 15:56:17 +0000

    Trim MOTHUR taxonomy input to set of provided reference seqs, escape dashes in filenames, added tests.

--- tests/test_app/test_mothur.py
+++ tests/test_app/test_mothur.py
@@ -5,9 +5,12 @@
 from __future__ import with_statement
 from cStringIO import StringIO
 from os import remove, rmdir
-from tempfile import mkdtemp, mkstemp
+from tempfile import mkdtemp, mkstemp, NamedTemporaryFile
 from cogent.util.unit_test import TestCase, main
-from cogent.app.mothur import Mothur, mothur_from_file
+from cogent.app.mothur import (
+    Mothur, mothur_from_file,
+    MothurClassifySeqs, mothur_classify_file
+    )
 
 
 __author__ = "Kyle Bittinger"
@@ -150,6 +153,124 @@ class MothurTests(TestCase):
         f.seek(0)
         parsed_otus = mothur_from_file(f)
         self.assertEquals(parsed_otus, self.small_otus_parsed)
+
+
+class TestMothurClassifySeqs(TestCase):
+    def setUp(self):
+        self.ref_file = NamedTemporaryFile()
+        self.ref_file.write(mothur_ref_seqs)
+        self.ref_file.seek(0)
+
+        self.tax_file = NamedTemporaryFile()
+        self.tax_file.write(mothur_taxonomy)
+        self.tax_file.seek(0)
+
+    def test_app(self):
+        app = MothurClassifySeqs({
+            'reference': self.ref_file.name,
+            'taxonomy': self.tax_file.name,
+            })
+        res = app(mothur_seqs)
+        assignments = res['assignments'].read()
+        self.assertEqual(assignments, mothur_assignments)
+
+        summary = res['summary'].read()
+        self.assertEqual(summary, mothur_summary)
+        
+
+    def test_format_function_arguments(self):
+        app = MothurClassifySeqs({
+            'reference': '/home/myuser/ref-seqs.fasta',
+            'taxonomy': '/home/MyUser/data/tax.txt',
+            'cutoff': 80,
+            })
+        obs_args = app._format_function_arguments(
+            ['reference', 'taxonomy', 'cutoff', 'iters'])
+        exp_args = (
+            "reference=/home/myuser/ref\\-seqs.fasta, "
+            "taxonomy=/home/MyUser/data/tax.txt, cutoff=80")
+        self.assertEqual(obs_args, exp_args)
+
+    def test_compile_mothur_script(self):
+        app = MothurClassifySeqs({
+            'reference': '/home/myuser/ref-seqs.fasta',
+            'taxonomy': '/home/MyUser/data/tax.txt',
+            'cutoff': 80,
+            })
+        app._input_filename = "/my/input.fasta"
+        exp_script = (
+            "#classify.seqs(fasta=/my/input.fasta, "
+            "reference=/home/myuser/ref\-seqs.fasta, "
+            "taxonomy=/home/MyUser/data/tax.txt, "
+            "cutoff=80)")
+        self.assertEqual(app._compile_mothur_script(), exp_script)
+
+    def test_mothur_classify_file(self):
+        query_file = StringIO(mothur_seqs)
+        res = mothur_classify_file(
+            query_file,  self.ref_file.name, self.tax_file.name)
+        exp_res = {
+            'A': (['k__Bacteria', 'p__Firmicutes', 'c__Clostridia',
+                   'o__Clostridale', 'f__Eubacteriaceae', 'g__Eubacterium',
+                   's__Eubacteriumfoedans'], 1.0),
+            'Very': (['k__Bacteria', 'p__Bacteriodetes'], 1.0),
+            '01': (['k__Bacteria', 'p__Firmicutes'], 1.0),
+            }
+        self.assertEqual(res, exp_res)
+
+
+mothur_assignments = """\
+01	k__Bacteria(100);p__Firmicutes(100);unclassified;unclassified;unclassified;unclassified;unclassified;
+A	k__Bacteria(100);p__Firmicutes(100);c__Clostridia(100);o__Clostridale(100);f__Eubacteriaceae(100);g__Eubacterium(100);s__Eubacteriumfoedans(100);
+Very	k__Bacteria(100);p__Bacteriodetes(100);unclassified;unclassified;unclassified;unclassified;unclassified;
+"""
+
+mothur_summary = """\
+taxlevel	 rankID	 taxon	 daughterlevels	 total	
+0	0	Root	1	3	
+1	0.1	k__Bacteria	2	3	
+2	0.1.1	p__Bacteriodetes	1	1	
+3	0.1.1.1	unclassified	1	1	
+4	0.1.1.1.1	unclassified	1	1	
+5	0.1.1.1.1.1	unclassified	1	1	
+6	0.1.1.1.1.1.1	unclassified	1	1	
+7	0.1.1.1.1.1.1.1	unclassified	0	1	
+2	0.1.2	p__Firmicutes	2	2	
+3	0.1.2.1	c__Clostridia	1	1	
+4	0.1.2.1.1	o__Clostridale	1	1	
+5	0.1.2.1.1.1	f__Eubacteriaceae	1	1	
+6	0.1.2.1.1.1.1	g__Eubacterium	1	1	
+7	0.1.2.1.1.1.1.1	s__Eubacteriumfoedans	0	1	
+3	0.1.2.2	unclassified	1	1	
+4	0.1.2.2.1	unclassified	1	1	
+5	0.1.2.2.1.1	unclassified	1	1	
+6	0.1.2.2.1.1.1	unclassified	1	1	
+7	0.1.2.2.1.1.1.1	unclassified	0	1	
+"""
+
+mothur_seqs = """\
+>01
+GGAGTCTGGGCCGGTGTCGTCAAGGTCCCAATCTGGCTGGTCGGTCTCTCAACCCAGCTACCCATCATTGCCTTGGTAGGCCGTTACCCACCAACAAGCTAACAGGCCGCGGGCCCATCCCTCTCCGCCGGAGCTTTCTCGAGTCTTCCATGCGGAAGTCCCGAAGTATTCGGTATTATCCACGGTTTCCCGTGGCTATCCCAATGAGAGGGGCAGGTTGCCCACGTGTTACTCAGCCGTTCGCCACTTTATACACACCCGAAGGTGCTTTAATCGTTCGACTTGCATGTGTTAGGCGCGCCGCCAGCGTTCATC
+>A
+GGAGTCTGGGCCGTGTCTCAGTCCCAATGTGGCCGATCACCCTCTCAGGTCGGCTATGCATCACGGCCTTGGTGAGCCGTTACCTCACCAACTAGCTAATGCACCGCGGGTCCATCCATCAGCAGAAGCTTGCGCCTCTTTTCCTCTTCAAACCATGCGGTTCGAAGACCTATGCGGTTTTAGCATCCGTTTCCGAATGTTATCCCCCTCTGATGGGCAGGTTACCCACGTGTTACTCACCCGTTCGCCACTAGATTGACCAGTGCAAGCACCGGTCGCTCTCGTTCGACTTGCATGTATTAGGCACGCCGCCAGCGTTCGTC
+>Very long seq name with many spaces!
+GGAGTCTGGACCGTGTCTCAGTTCCAGTGTGACTGATCATCCTCTCAGACCAGTTATGCGTCATAGCCTTGGTGAGCCATTACCTCACCAACTAGCTGATACAATATAGCCTCATCCTACACCGAAAAACTTTCCCTATCTAACTTATGTTAGAGAGGAGTATAGAGTATTAGCAGTCGTTTCCAACTGTTGTCCTCTAGTGTAGGGCAGATTAGCTACACATTACTCACCCGTGCGCCACTAACTCATAAGAGCAAGCTCTTACTTGTCCGTTCGACTTGCATGTATTAGGCACGCCGCCAGCGTTCACT
+"""
+
+mothur_ref_seqs = """\
+>ref1
+GGAGTCTGGGCCGGTGTCGTCAAGGTCCCAATCTGGCTGGTCGGTCTCTCTGGTAGGCCGTTACCCACCAACAAGCTAACAGGCCGCGGGCCCATCCCTCTCCGCCGGAGCTTTCTCGAGTCTTCCATGCGGAAGTCCCTGCGGAAGTCCCGAAGTATTCGGTATTATCCACGGTTTCCCGTGGCTATCCCAATGAGAGGGGCAGGTTGCCCACGTGTTACTCAGCCGTTCGCCACTTTATACACACCCGAAGGTGCTTTAATCGTTCGACTTGCATGTGTTAGGCGCGCCGCCAGCGTTCATC
+>ref2
+GGAGTCTGGGCCGTGTCTCAGTCCCAATGTGGCCGATCACCCTCTCAGGTCGGCTATGCATCACGGCCTTGGTGAGCCGTTACCTCACCAACTAGCTACTCTTTTCCTCTTCAAACCATGCGGTTCGAAGACCTATGCGGTTTTAGCATCCGTAAACTTTCCCTATCTAACTTATGTTAGAGAGGAGTATAGAGTATTAGCAGTCGTTTCCAACTTCCGAATGTTATCCCCCTCTGATGGGCAGGTTACCCACGTGTTACTCACCCGTTCGCCACTAGATTGACCAGTGCAAGCACCGGTCGCTCTCGTTCGACTTGCATGTATTAGGCACGCCGCCAGCGTTCGTC
+>3333
+GGAGTCTGGACCGTGTCTCAGTTCCAGTGTGACTGATCATCCTCTCAGACAGTTATGCGTCATAGCCTTGGTGAGCCATTACCTCACCAACTAGCTGATACAATATAGCCTCATCCTACACCGAAAAACTTTCCCTATCTCTTATGTTAGAGAGGAGTATAGAGTATTAGCAGTCGTTTCCAACTGTTGTCCTCTAGTGTAGGGCAGATTAGCACACATTACTCACCCGTGCGCCACTAACTCATAAGAGCAAGCTCTTACTTGTCCGTTCGACTTGCATGTATTAGGCACGCCGCCAGCGTTCACT
+"""
+
+mothur_taxonomy = """\
+ref1	k__Bacteria;p__Firmicutes;
+ref2	k__Bacteria;p__Firmicutes;c__Clostridia;o__Clostridale;f__Eubacteriaceae;g__Eubacterium;s__Eubacteriumfoedans;
+3333	k__Bacteria;p__Bacteriodetes;
+"""
 
 if __name__ == '__main__':
     main()
