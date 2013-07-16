335ecfed17657ec005fb499df04b3615b91b31a3
mikerobeson <soilbdelloid@gmail.com>
2013-07-16 12:33:37 -0400

    added code to 'run_default_pandaseq'

--- cogent/app/pandaseq.py
+++ cogent/app/pandaseq.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
 # file: pandaseq.py
 
-# Application controller for pandaseq 
+# Application controller for pandaseq (v2.4) 
 # https://github.com/neufeld/pandaseq
 # 
 
@@ -19,17 +19,18 @@ __maintainer__ = "Michael Robeson"
 __email__ = "robesonms@ornl.gov"
 __status__ = "Development"
 
-# pandaseq 2.4 <andre@masella.name>
-# Usage: pandaseq -f forward.fastq -r reverse.fastq [-6] [-a] [-B] 
-#    [-C module1 -C module2 ...] [-d flags] [-F] [-j] [-L maxlen] 
-#    [-l minlen] [-N] [-o minoverlap] [-p forwardprimer] 
-#    [-q reverseprimer] [-T threads] [-t threshold] 
 
 class PandaSeq(CommandLineApplication):
     """pandaseq application controller for joining paired-end reads """
-    _command = 'SeqPrep'
+    _command = 'pandaseq'
     _parameters = {
-    # -6  Use PHRED+64 (CASAVA 1.3-1.7) instead of PHRED+33 (CASAVA 1.8+).
+    # pandaseq 2.4 <andre@masella.name>
+    # Usage: pandaseq -f forward.fastq -r reverse.fastq [-6] [-a] [-B] 
+    #    [-C module1 -C module2 ...] [-d flags] [-F] [-j] [-L maxlen] 
+    #    [-l minlen] [-N] [-o minoverlap] [-p forwardprimer] 
+    #    [-q reverseprimer] [-T threads] [-t threshold] > assembled.fastq
+
+   # -6  Use PHRED+64 (CASAVA 1.3-1.7) instead of PHRED+33 (CASAVA 1.8+).
     '-6':FlagParameter(Prefix='-', Name='6'),
 
     # -a  Strip the primers after assembly, rather than before.
@@ -54,7 +55,7 @@ class PandaSeq(CommandLineApplication):
     '-f':ValuedParameter(Prefix='-', Delimiter=' ', Name='f'),
 
     # -F  Output FASTQ instead of FASTA.
-    '-F':ValuedParameter(Prefix='-', Delimiter=' ', Name='F'),
+    '-F':FlagParameter(Prefix='-', Name='F'),
 
     # -j  Input files are bzipped.
     '-j':FlagParameter(Prefix='-', Name='j'),
@@ -91,8 +92,13 @@ class PandaSeq(CommandLineApplication):
     }
 
 
-    def _get_result_paths(self, data):
-        """Captures pandaseq output. """
+    #def _get_result_paths(self, data):
+    #    """Captures pandaseq output. """
+    #    result = {}
+        
+        # always output:
+    #    result[''] = 
+
 
     def getHelp(self):
         """pandaseq help"""
@@ -104,3 +110,61 @@ class PandaSeq(CommandLineApplication):
         Website:
             https://github.com/neufeld/pandaseq
         """
+
+
+def run_default_pandaseq(\
+    reads1_infile_name,
+    reads2_infile_name,
+    fastq=True,
+    params={},
+    working_dir='/tmp',
+    temp_dir='/tmp',
+    SuppressStderr=True,
+    SuppressStdout=False,
+    HALT_EXEC=False):
+    """ Runs pandaseq with default parameters to assemble paired-end reads.
+        -reads1_infile_path : reads1.fastq infile path
+        -reads2_infile_path : reads2.fastq infile path
+        -params : other optional pandaseq parameters
+        -fastq : output assembly as fastq (True)
+                 or Fasta (False)
+    """
+
+    file_paths = [reads1_infile_name, reads2_infile_name]
+
+    for p in file_paths:
+        if not path.exists(p):
+            raise IOError, 'File not found at: %s' % p
+        else:
+            try:
+                path.isabs(p)
+            except:
+                raise IOError, '\'%s\' not found and is not an absolute path' % p
+
+    # required by pandaseq to assemble
+    params['-f'] = reads1_infile_name
+    params['-r'] = reads2_infile_name
+   
+    # set up controller
+    pandaseq_app = PandaSeq(\
+        params=params,
+        WorkingDir=working_dir,
+        TmpDir=temp_dir,
+        SuppressStderr=SuppressStderr,
+        SuppressStdout=SuppressStdout,
+        HALT_EXEC=HALT_EXEC)
+    
+    # Fastq?
+    if fastq:
+        pandaseq_app.Parameters['-F'].on()
+    else:
+        pandaseq_app.Parameters['-F'].off()
+
+    # run assembler
+    result = pandaseq_app()
+    return result
+
+
+
+
+
