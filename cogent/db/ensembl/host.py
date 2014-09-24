87d69b68a247bfa71c2e02558f37e9b48f2daf01
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2014-09-24 20:21:50 +1000

    Add support for mysql-connector-python
    [NEW] This is now the official Python module for connecting to MySQL.
         Importantly, it's also pure Python! This eases the installation burden
         considerably. NOTE: You must use the latest version of SQLAlchemy.
    
    MySQL-python is still supported.

--- cogent/db/ensembl/host.py
+++ cogent/db/ensembl/host.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python
-import MySQLdb
+import warnings
 import sqlalchemy as sql
+
+try:
+    import mysql.connector as mysql_connect
+    connect_template = 'mysql+mysqlconnector://%(account)s/%(db_name)s?raise_on_warnings=False'
+    password_arg = 'password'
+    sql_version = tuple([int(v) for v in sql.__version__.split(".") if v.isdigit()])
+    if sql_version < (0, 9, 7):
+        warnings.warn('mysql.connector requires sqlalchemy >= 0.9.7\n')
+        raise ImportError
+except ImportError:
+    import MySQLdb as mysql_connect
+    connect_template = 'mysql+mysqldb://%(account)s/%(db_name)s'
+    password_arg = 'passwd'
 
 from cogent.util.table import Table
 from cogent.db.ensembl.species import Species
@@ -60,11 +73,12 @@ class EngineCache(object):
         pool_recycle = pool_recycle or 3600
         if account not in self._db_account.get(db_name, []):
             if db_name == "PARENT":
-                engine = MySQLdb.connect(host=account.host, user=account.user,
-                                    passwd=account.passwd, port=account.port)
+                args = {password_arg: account.passwd}
+                engine = mysql_connect.connect(host=account.host, user=account.user,
+                                    port=account.port, **args)
             else:
-                engine = sql.create_engine("mysql://%s/%s" % (account, 
-                                        db_name), pool_recycle=pool_recycle)
+                engine = sql.create_engine(connect_template % dict(account=account, 
+                                        db_name=db_name), pool_recycle=pool_recycle)
             if db_name not in self._db_account:
                 self._db_account[db_name] = {}
             self._db_account[db_name][account] = engine
