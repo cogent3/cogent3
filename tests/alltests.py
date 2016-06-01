67bb0399cf8c35920b182b8ff13202faef626622
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2016-06-01 13:22:08 +1000

    removed clearcut app controller and tests

--- tests/alltests.py
+++ tests/alltests.py
@@ -206,8 +206,7 @@ def suite():
         modules_to_test.append('test_draw.test_distribution_plots')
 
     #Try importing modules for app controllers
-    apps = [('clearcut', 'test_clearcut'),
-            ('cmalign', 'test_infernal'),
+    apps = [('cmalign', 'test_infernal'),
             ('cmfinder.pl', 'test_cmfinder'),
             ('comrna', 'test_comrna'),
             ('contrafold', 'test_contrafold'),
