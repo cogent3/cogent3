9d9e4f37afd0083b10e4dd076484ef14b564b798
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2016-06-01 20:36:19 +1000

    removed mothur app controller, parser and tests

--- tests/alltests.py
+++ tests/alltests.py
@@ -199,8 +199,7 @@ def suite():
         modules_to_test.append('test_draw.test_distribution_plots')
 
     #Try importing modules for app controllers
-    apps = [('mothur', 'test_mothur'),
-            ('muscle', 'test_muscle_v38'),
+    apps = [('muscle', 'test_muscle_v38'),
             ('msms', 'test_msms'),
             ('ParsInsert', 'test_parsinsert'),
             ('rdp_classifier-2.2.jar', 'test_rdp_classifier'),
