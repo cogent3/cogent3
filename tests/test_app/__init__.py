fa21bbe8f6a31c802e76e4cdbfb9f29e4db16ded
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2016-06-01 13:41:04 +1000

    removed cmfinder app controller and tests and parser

--- tests/test_app/__init__.py
+++ tests/test_app/__init__.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 __all__ = """
 test_dynalign       test_raxml          test_stride  
-test_blast          test_foldalign      test_rnaalifold
-test_carnac         test_ilm            test_rnaforester
-test_clustalw       test_knetfold       test_rnaview
-test_cmfinder       test_mfold          test_sfold
+test_foldalign      test_rnaalifold
+test_ilm            test_rnaforester
+test_knetfold       test_rnaview
+test_mfold          test_sfold
 test_comrna         test_muscle         test_unafold
 test_consan         test_nupack         test_util
 test_contrafold     test_parameters     test_vienna_package
