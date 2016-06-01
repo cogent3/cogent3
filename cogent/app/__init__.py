3cd972431d4eed6eea3b382ce8a5fbad7f2fe0df
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2016-06-01 19:24:30 +1000

    removed contrafold app controller, parser and tests

--- cogent/app/__init__.py
+++ cogent/app/__init__.py
@@ -2,8 +2,7 @@
 """apps: provides support libraries for controlling applications (local or web).
 """
 
-__all__ = ['contrafold',
-           'cove',
+__all__ = ['cove',
            'dialign',
            'dynalign',
            'fasttree',
