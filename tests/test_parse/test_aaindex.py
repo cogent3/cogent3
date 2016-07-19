850065c986b2a3e64396b2ae945124ca0a5dd6a7
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2016-07-19 11:03:04 +1000

    fixed re.split FutureWarning from aaindex parser

--- tests/test_parse/test_aaindex.py
+++ tests/test_parse/test_aaindex.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 """Tests of the AAIndex parser.
 """
-
 from cogent3.util.unit_test import TestCase, main
 from cogent3.parse.aaindex import AAIndex1Parser, AAIndex2Parser,\
     AAIndexRecord, AAIndex1Record, AAIndex2Record, AAIndex1FromFiles,\
