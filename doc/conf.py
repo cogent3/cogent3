973e64d79f0ed6467f47d17bca0f8c66d4f70b54
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2016-09-05 14:15:28 +1000

    revertint to sphinx.ext.pngmath to enable build at rtd

--- doc/conf.py
+++ doc/conf.py
@@ -22,7 +22,7 @@ import sys, os
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.todo', 'sphinx.ext.doctest', 'sphinx.ext.imgmath']
+extensions = ['sphinx.ext.todo', 'sphinx.ext.doctest', 'sphinx.ext.pngmath']
 
 # todo_include_todos=True # to expose the TODOs, uncomment this line
 
