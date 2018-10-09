e06f24a2e1a800c3e2370172bd30bf7005605b66
Benjamin Millar <u6386700@anu.edu.au>
2018-10-10 02:03:00 +1100

    Removed combinate

--- cogent3/util/misc.py
+++ cogent3/util/misc.py
@@ -154,16 +154,6 @@ class DepthExceededError(Exception):
     pass
 
 
-def combinate(items, n):
-    """Returns combinations of items."""
-    if n == 0:
-        yield []
-    else:
-        for i in range(len(items) - n + 1):
-            for cc in combinate(items[i + 1:], n - 1):
-                yield [items[i]] + cc
-
-
 def gzip_dump(object, filename, bin=2):
     """Saves a compressed object to file."""
     file = GzipFile(filename, 'wb')
