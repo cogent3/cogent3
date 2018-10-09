a9e49b638a310f567c700abd7662153813366b28
Benjamin Millar <u6386700@anu.edu.au>
2018-10-10 00:17:47 +1100

    Removed get_tmp_filename

--- cogent3/util/misc.py
+++ cogent3/util/misc.py
@@ -103,37 +103,6 @@ class FilePath(str):
         return FilePath(''.join([self, other]))
 
 
-def get_tmp_filename(tmp_dir=gettempdir(), prefix="tmp", suffix=".txt",
-                     result_constructor=FilePath):
-    """ Generate a temporary filename and return as a FilePath object
-
-        tmp_dir: the directory to house the tmp_filename (default: '/tmp')
-        prefix: string to append to beginning of filename (default: 'tmp')
-            Note: It is very useful to have prefix be descriptive of the
-            process which is creating the temporary file. For example, if
-            your temp file will be used to build a temporary blast database,
-            you might pass prefix=TempBlastDB
-        suffix: the suffix to be appended to the temp filename (default '.txt')
-        result_constructor: the constructor used to build the result filename
-            (default: cogent3.app.parameters.FilePath). Note that joining
-            FilePath objects with one another or with strings, you must use
-            the + operator. If this causes trouble, you can pass str as the
-            the result_constructor.
-    """
-    # check not none
-    if not tmp_dir:
-        tmp_dir = ""
-    # if not current directory, append "/" if not already on path
-    elif not tmp_dir.endswith("/"):
-        tmp_dir += "/"
-
-    chars = "abcdefghigklmnopqrstuvwxyz"
-    picks = chars + chars.upper() + "0123456790"
-    return result_constructor(tmp_dir) + result_constructor(prefix) +\
-        result_constructor("%s%s" %
-                           (''.join([choice(picks) for i in range(20)]), suffix))
-
-
 def safe_md5(open_file, block_size=2**20):
     """Computes an md5 sum without loading the file into memory
 
