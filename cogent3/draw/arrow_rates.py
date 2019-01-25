9c5d4ce42b5a6f623f27b268ef854a2e3c3817ce
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2019-01-25 15:00:29 +1100

    ENH: make_arrow_plot now allows turning off labelling rate arrows

--- cogent3/draw/arrow_rates.py
+++ cogent3/draw/arrow_rates.py
@@ -221,8 +221,11 @@ def make_arrow_plot(data, size=4, display='length', shape='right',
         M = array([[cx, sx], [-sx, cx]])
         coords = dot(orig_position, M) + [[x_pos, y_pos]]
         x, y = ravel(coords)
-        orig_label = rate_labels[pair]
-        label = '$%s_{_{\mathrm{%s}}}$' % (orig_label[0], orig_label[1:])
+        if rate_labels:
+            orig_label = rate_labels[pair]
+            label = '$%s_{_{\mathrm{%s}}}$' % (orig_label[0], orig_label[1:])
+        else:
+            label = ''
 
         text(x, y, label, size=label_text_size, ha='center', va='center',
              color=labelcolor or fc)
