d60a1b47453015fe46bc9581797da6e5c69cab81
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2019-03-08 17:45:08 +1100

    ENH: added cogent3.app.translate functions
    [NEW] composable function that processes sequences to return
        the translatable frame
    
    **NOTE:** This should be considered Alpha level code, the API is likely to change!

--- tests/test_app/test_composable.py
+++ tests/test_app/test_composable.py
@@ -1,5 +1,6 @@
 from unittest import TestCase, main
 from cogent3.app.composable import ComposableSeq, ErrorResult
+from cogent3.app.translate import select_translatable
 
 __author__ = "Gavin Huttley"
 __copyright__ = "Copyright 2007-2016, The Cogent Project"
@@ -24,6 +25,7 @@ class TestComposableBase(TestCase):
         got = str(comb)
         self.assertEqual(got, expect)
 
+
 class TestErrorResult(TestCase):
     def test_err_result(self):
         """excercise creation of ErrorResult"""
@@ -39,6 +41,20 @@ class TestErrorResult(TestCase):
             result = ErrorResult('SKIP', 'this', err.args[0])
 
         self.assertEqual(result.message, 'error message')
+
+    def test_str(self):
+        """str representation correctly represents parameterisations"""
+        func = select_translatable()
+        got = str(func)
+        self.assertEqual(got, "select_translatable(type='sequences', "
+                              "moltype='dna', gc='Standard Nuclear', "
+                              "allow_rc=False, trim_terminal_stop=True)")
+
+        func = select_translatable(allow_rc=True)
+        got = str(func)
+        self.assertEqual(got, "select_translatable(type='sequences', "
+                              "moltype='dna', gc='Standard Nuclear', "
+                              "allow_rc=True, trim_terminal_stop=True)")
 
 
 if __name__ == "__main__":
