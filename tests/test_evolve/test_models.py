b411d7520aee7ea62c5e7f23775268123e79ac31
James Kondilios <james.kondilios@anu.edu.au>
2019-03-25 17:10:57 +1100

    ENH: Implemented available_models(), Fixes #15
    [NEW] available_models, returns a table listing model type, abbreviation and description.
    optional argument model_type selects models for given purpose eg. codon, nucleotide, protein

--- tests/test_evolve/test_models.py
+++ tests/test_evolve/test_models.py
@@ -4,6 +4,11 @@ from cogent3.evolve.models import (JC69, F81, HKY85, TN93, GTR, GN, ssGN,
                                    DSO78, AH96, AH96_mtmammals, JTT92, WG01,
                                    CNFGTR, CNFHKY, GNC, WG01_matrix, WG01_freqs,
                                    get_model, models)
+from cogent3.evolve.models import nucleotide_models, codon_models, protein_models, models
+from cogent3.evolve.models import available_models
+from cogent3.evolve import models as models_module
+from itertools import chain
+
 
 __author__ = "Gavin Huttley"
 __copyright__ = "Copyright 2007-2016, The Cogent Project"
@@ -62,5 +67,51 @@ class CannedModelsTest(TestCase):
             _ = get_model('blah')
 
 
-if __name__ == '__main__':
+def get_sample_model_types(mod_type=None):
+    if mod_type == "nucleotide_models":
+        available_mods = nucleotide_models
+
+    if mod_type == "codon_models":
+        available_mods = codon_models
+
+    if mod_type == "protein_models":
+        available_mods = protein_models
+
+    else:
+        available_mods = models
+
+    return available_mods
+
+
+class AvailableModelsTest(TestCase):
+
+    def setUp(self):
+        self.nucleotide_models = get_sample_model_types("nucleotide_model")
+        self.codon_models = get_sample_model_types("codon_model")
+        self.protein_models = get_sample_model_types("protein_model")
+        self.all_models = get_sample_model_types()
+
+    def test_model_abbreviation(self):
+        """make sure getting model abbreviations that exist"""
+        got = set(available_models().tolist("Abbreviation"))
+        expect = set(['JC69', 'CNFGTR', 'DSO78'])
+        self.assertTrue(expect < got)
+
+    def test_model_by_type(self):
+        """correctly obtain models by type"""
+        for model_type in "codon_model nucleotide_model protein_model".split():
+            table = available_models(model_type)
+            got = table.distinct_values('Model Type')
+            self.assertEqual(got, {model_type})
+
+    def test_model_description(self):
+        """correctly grabs function descriptions"""
+        all_available = available_models()
+        for abbrev, desc in all_available.tolist(['Abbreviation', 'Description']):
+            func = getattr(models_module, abbrev)
+            doc = func.__doc__.split()
+            self.assertEqual(desc.split(), doc)
+
+
+if __name__ == "__main__":
     main()
