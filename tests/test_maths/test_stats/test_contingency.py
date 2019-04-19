7f0b337715f668b2f70903255aaf832bb86c8e16
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2019-04-19 17:43:43 +1000

    ENH: contingency.TestResult class returned by CategoryCounts tests
    [NEW] G-test's etc.. now return instances of this. It contains the
        observeds, expecteds, residuals, p-value as attributes on the
        object. Also provides a nice repr() display.

--- tests/test_maths/test_stats/test_contingency.py
+++ tests/test_maths/test_stats/test_contingency.py
@@ -19,9 +19,10 @@ class ContingencyTests(TestCase):
         """correctly compute chisq test"""
         table = CategoryCounts([[762, 327, 468],
                                 [484, 239, 477]])
-        chisq, df, p = table.chisq_test()
-        self.assertEqual(round(chisq, 5), 30.07015)
-        self.assertEqual(df, 2)
+        got = table.chisq_test()
+        self.assertEqual(round(got.chisq, 5), 30.07015)
+        self.assertEqual(got.df, 2)
+        assert_allclose(got.pvalue, 2.95358918321e-07)
 
     def test_residuals(self):
         """correctly calculate residuals"""
@@ -36,28 +37,28 @@ class ContingencyTests(TestCase):
         data = {'rest_of_tree': {'env1': 2, 'env3': 1, 'env2': 0},
                 'b': {'env1': 1, 'env3': 1, 'env2': 3}}
         table = CategoryCounts(data)
-        chisq, df, p = table.chisq_test()
-        assert_allclose(chisq, 3.02222222)
+        got = table.chisq_test()
+        assert_allclose(got.chisq, 3.02222222)
         data = {'AIDS': {'Males': 4, 'Females': 2, 'Both': 3},
                 'No_AIDS': {'Males': 3, 'Females': 16, 'Both': 2}}
         table = CategoryCounts(data)
-        chisq, df, p = table.chisq_test()
-        assert_allclose(chisq, 7.6568405139833722)
-        assert_allclose(p, 0.0217439383468)
+        got = table.chisq_test()
+        assert_allclose(got.chisq, 7.6568405139833722)
+        assert_allclose(got.pvalue, 0.0217439383468)
 
     def test_1D_counts(self):
         """correctly operate on a 1D count array"""
         table = CategoryCounts([762, 327])
-        chisq, df, p = table.chisq_test()
-        assert_allclose(chisq, 173.7603305785124)
-        self.assertLess(p, 2.2e-16)  # value from R
+        got = table.chisq_test()
+        assert_allclose(got.chisq, 173.7603305785124)
+        self.assertLess(got.pvalue, 2.2e-16)  # value from R
 
     def test_G_ind(self):
         """correctly produce G test of independence"""
         table = CategoryCounts([[762, 327, 468],
                                 [484, 239, 477]])
-        G, df, p = table.G_independence(williams=True)
-        self.assertEqual(df, 2)
+        got = table.G_independence(williams=True)
+        self.assertEqual(got.df, 2)
 
     def test_G_fit_with_expecteds(self):
         """compute G-fit with provided expecteds"""
@@ -67,17 +68,17 @@ class ContingencyTests(TestCase):
         table = CategoryCounts(dict(zip(keys, obs)),
                                expected=dict(zip(keys, exp)))
 
-        G, df, p = table.G_fit()
-        assert_allclose(G, 9.849234)
-        assert_allclose(p, 0.04304536)
+        got = table.G_fit()
+        assert_allclose(got.G, 9.849234)
+        assert_allclose(got.pvalue, 0.04304536)
 
     def test_shuffling(self):
         """resampling works for G-independence"""
         table = CategoryCounts([[762, 327], [750, 340]])
-        G, df, p = table.G_independence(shuffled=50)
-        self.assertTrue(0 < p < 1)  # a large interval
-        G, df, p = table.chisq_test(shuffled=50)
-        self.assertTrue(0 < p < 1)  # a large interval
+        got = table.G_independence(shuffled=50)
+        self.assertTrue(0 < got.pvalue < 1)  # a large interval
+        got = table.chisq_test(shuffled=50)
+        self.assertTrue(0 < got.pvalue < 1)  # a large interval
 
     def test_to_dict(self):
         """returns a dict of contents"""
@@ -130,6 +131,19 @@ class ContingencyTests(TestCase):
         assert_allclose(matrix.expected.array.tolist(),
                         [[1.875, 1.875, 1.25],
                          [1.125, 1.125, 0.75]])
+
+    def test_repr_str_html(self):
+        """exercising construction of different representations"""
+        table = CategoryCounts(
+            {'rest_of_tree': {'env1': 2, 'env3': 1, 'env2': 0},
+             'b': {'env1': 1, 'env3': 1, 'env2': 3}})
+        got_g1 = table.G_fit()
+        got_g2 = table.G_independence()
+        got_chisq = table.chisq_test()
+        for obj in (got_g1, got_g2, got_chisq):
+            str(obj)
+            repr(obj)
+            obj._repr_html_()
 
 
 if __name__ == '__main__':
