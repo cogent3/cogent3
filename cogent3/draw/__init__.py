7a92a045b6cd66bb3ae4baf1d9a3faa4b16afe08
Rahul Ghangas <u6157693@anu.edu.au>
2019-04-26 11:49:48 +1000

    ENH: Prototype module for dendrogram implementation in plot.ly

--- cogent3/draw/__init__.py
+++ cogent3/draw/__init__.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 
-__all__ = ['dendrogram', 'dotplot', 'linear', 'colors', 'TrackDefn',
+__all__ = ['dendrogram', 'dendrogram_2', 'dotplot', 'linear', 'colors', 'TrackDefn',
            'Display', 'DisplayPolicy', 'Area', 'Arrow', 'BluntArrow',
            'Box', 'Diamond'] + [
     'arrow_rates', 'dinuc', 'fancy_arrow', 'codon_usage', 'util']
 
 __copyright__ = "Copyright 2007-2016, The Cogent Project"
 __contributors__ = ["Peter Maxwell", "Gavin Huttley", "Rob Knight",
-                    "Zongzhi Liu", "Matthew Wakefield", "Stephanie Wilson"]
+                    "Zongzhi Liu", "Matthew Wakefield", "Stephanie Wilson",
+                    "Rahul Ghangas"]
 __license__ = "GPL"
 __version__ = "3.0a2"
 __status__ = "Production"
