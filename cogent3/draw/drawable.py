27653a6e71086a4e8088fba6669106ddf0579e16
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2019-06-05 13:37:07 +1000

    API: refactor of dotplot display and drawables
    [CHANGED] Display2D renamed Dotplot
    [NEW] dotplot.,get_dotplot_coords function that does the work of computing
        coordinates for a dotplot.
    [CHANGED] The calculation of dotplot lines now done on creation of the object.
        This means there is no longer a Dotplot._cache attribute, i.e. coordinates
        are calculated only once.
    [CHANGED] Dotplot.__init__ now has additional arguments for axis titles and the
        standard arguments for computing the dotplot (window size, threshold,
        etc..) and a show_progress argument. NOTE: that the figure height is computed
        via the ratio of the sequence lengths multiplied by the width.
    [CHANGED] the _build_fig() method now only accepts xaxis and yaxis designations
        (for when it's part of a composite figure)
    [NEW] once constructed, the figure is assigned to a ._figure attribute and accesible
        via the figure property.
    [CHANGED] annotation track creation the responsibility of the Alignment.
    [CHANGED] Drawable class gets arguments for xaxis and yaxis titles, and the layout
        (the latter used to update the fiugure layout property).
    [NEW] drawable.AnnotatedDrawable class now handles association of sequence annotations
        and another chart. This takes the 'core' drawable (e.g. Dotplot instance) and
        left chart and bottom chart drawables.
    [FIXED] the size of annotation display tracks adjusted for the number of different
        types.

--- cogent3/draw/drawable.py
+++ cogent3/draw/drawable.py
@@ -17,7 +17,8 @@ class Drawable:
     """container object for Plotly figures"""
 
     def __init__(self, title=None, traces=None, width=None, height=None,
-                 showlegend=True, visible_axes=True):
+                 showlegend=True, visible_axes=True, layout=None,
+                 xtitle=None, ytitle=None):
         self._traces = traces or []
         self._layout = go.Layout(title=title,
                                  font=dict(family='Balto', size=14),
@@ -31,6 +32,10 @@ class Drawable:
                                  plot_bgcolor='rgb(245,245,245)',
                                  margin=dict(l=50, r=50, t=50)
                                  )
+        layout = layout or {}
+        self._layout.update(**layout)
+        self.xtitle = xtitle
+        self.ytitle = ytitle
 
     def _repr_html_(self):
         self.iplot()
@@ -88,6 +93,8 @@ class Drawable:
     def figure(self):
         if not self.traces:
             self._build_fig()
+        self.layout.xaxis.title = self.xtitle
+        self.layout.yaxis.title = self.ytitle
         return dict(data=self.traces, layout=self.layout)
 
     def iplot(self, *args, **kwargs):
@@ -118,6 +125,258 @@ def bind_drawable(obj, drawable):
     obj.drawable = drawable
     obj.iplot = MethodType(_iplot_, obj)
     return obj
+
+
+class AnnotatedDrawable(Drawable):
+    """supports drawing with left and bottom tracks of annotations"""
+
+    def __init__(self, core, left_track=None, bottom_track=None, xtitle=None,
+                 ytitle=None, xrange=None, yrange=None, width=500, height=500,
+                 layout=None):
+        super(AnnotatedDrawable, self).__init__(visible_axes=True,
+                                                showlegend=True,
+                                                width=width,
+                                                height=height,
+                                                layout=layout)
+
+        self.xtitle = xtitle
+        self.ytitle = ytitle
+        self.yrange = yrange
+        self.xrange = xrange
+        self.core = core
+        self.left_track = left_track
+        self.bottom_track = bottom_track
+
+    def _build_fig(self, xaxis='x', yaxis='y'):
+        f = self.core.figure
+        try:
+            traces = f.traces
+            self.layout.update(f.layout)
+        except AttributeError:
+            traces = f['data']
+            self.layout.update(f['layout'])
+        for trace in traces:
+            trace.xaxis = xaxis
+            trace.yaxis = yaxis
+        self._traces = traces
+
+    def _build_2x2_fig(self):
+        if not self.traces:
+            self._build_fig(xaxis='x2', yaxis='y2')
+
+        space = 0.01
+        fig = tools.make_subplots(rows=2, cols=2,
+                                  specs=[[{}, {}],
+                                         [None, {}]],
+                                  horizontal_spacing=space,
+                                  vertical_spacing=space,
+                                  print_grid=False,
+                                  row_width=[0.1, 0.9],
+                                  column_width=[0.1, 0.9])
+
+        layout = fig.layout
+        layout.update(self.layout)
+
+        # common settings
+        ticks_off_kwargs = dict(showticklabels=False, mirror=True,
+                                showgrid=False,
+                                showline=True,
+                                zeroline=False,
+                                ticks='')
+        ticks_on_kwargs = dict(showticklabels=True, mirror=True, showgrid=False,
+                               showline=True,
+                               zeroline=False)
+
+        # dotpolot traces and layout
+        fig.add_traces(self.traces)
+
+        layout.xaxis2.update(range=self.xrange, **ticks_off_kwargs)
+        layout.yaxis2.update(range=self.yrange, **ticks_off_kwargs)
+
+        # left_track traces
+        seen_types = set()
+        max_x = 0
+        traces = []
+        for trace in self.left_track.traces:
+            traces.append(trace)
+            max_x = max(numpy.max(trace.x), max_x)
+            if trace.legendgroup in seen_types:
+                trace.showlegend = False
+            seen_types.add(trace.legendgroup)
+
+        left_range = [0, int(max_x) + 1]
+
+        # bottom_track traces
+        max_y = 0
+        for trace in self.bottom_track.traces:
+            trace.xaxis = 'x3'
+            trace.yaxis = 'y3'
+            traces.append(trace)
+            max_y = max(numpy.max(trace.y), max_y)
+            if trace.legendgroup in seen_types:
+                trace.showlegend = False
+            seen_types.add(trace.legendgroup)
+
+        bottom_range = [0, int(max_y) + 1]
+
+        # add all traces
+        fig.add_traces(traces)
+        # configure easy axes for titles, limits, border and ticks
+        layout.yaxis.update(title=self.ytitle,
+                            range=self.yrange,
+                            **ticks_on_kwargs)
+
+        layout.xaxis3.update(title=self.xtitle,
+                             range=self.xrange,
+                             **ticks_on_kwargs)
+
+        # adjust row width of left plot for number of feature tracks
+        min_range = min(left_range[1], bottom_range[1])
+        left_prop = left_range[1] / min_range
+
+        # first the top row
+        xaxis_domain = list(layout.xaxis.domain)
+        xaxis_domain[1] = left_prop * xaxis_domain[1]
+        layout.xaxis.update(title=None,
+                            range=left_range,
+                            domain=xaxis_domain,
+                            **ticks_off_kwargs)
+        layout.xaxis.update(title=None,
+                            range=left_range,
+                            domain=xaxis_domain,
+                            **ticks_off_kwargs)
+
+        layout.xaxis2.domain = (xaxis_domain[1] + space, 1.0)
+
+        # now the right column
+        bottom_prop = bottom_range[1] / min_range
+        yaxis_domain = list(layout.yaxis3.domain)
+        yaxis_domain[1] = bottom_prop * yaxis_domain[1]
+        layout.yaxis3.update(title=None,
+                             range=bottom_range,
+                             domain=yaxis_domain,
+                             **ticks_off_kwargs)
+
+        # and bottom of the boxes above
+        layout.yaxis.domain = (yaxis_domain[1] + space, 1.0)
+        layout.yaxis2.domain = (yaxis_domain[1] + space, 1.0)
+
+        return fig
+
+    def _build_2x1_fig(self):
+        """2 rows, one column, dotplot and seq1 annotated"""
+        if not self.traces:
+            self._build_fig()
+
+        fig = tools.make_subplots(rows=2, cols=1,
+                                  vertical_spacing=0.015,
+                                  print_grid=False,
+                                  row_width=[0.1, 0.9],
+                                  shared_xaxes=True)
+        layout = fig.layout
+        layout.update(self.layout)
+
+        # common settings
+        ticks_off_kwargs = dict(showticklabels=False,
+                                mirror=True,
+                                showgrid=False,
+                                showline=True)
+        ticks_on_kwargs = dict(showticklabels=True,
+                               mirror=True,
+                               showgrid=False,
+                               showline=True)
+
+        # dotpolot traces and layout
+        fig.add_traces(self.traces)
+
+        layout.xaxis.update(title=self.bottom_track.name,
+                            range=self.xrange,
+                            **ticks_on_kwargs)
+        layout.yaxis.update(title=self.left_track.name,
+                            range=self.yrange,
+                            **ticks_on_kwargs)
+
+        # seq1 traces
+        seen_types = set()
+        max_y = 0
+        traces = []
+        for trace in self.bottom_track.traces:
+            trace.yaxis = 'y2'
+            traces.append(trace)
+            max_y = max(numpy.max(trace.y), max_y)
+            if trace.legendgroup in seen_types:
+                trace.showlegend = False
+            seen_types.add(trace.legendgroup)
+
+        fig.add_traces(traces)
+        layout.yaxis2.update(title=None,
+                             range=[0, int(max_y) + 1],
+                             **ticks_off_kwargs)
+        return fig
+
+    def _build_1x2_fig(self):
+        if not self.traces:
+            self._build_fig(xaxis='x2')
+        fig = tools.make_subplots(rows=1, cols=2,
+                                  horizontal_spacing=0.01,
+                                  print_grid=False,
+                                  column_width=[0.1, 0.9],
+                                  shared_yaxes=True)
+        layout = fig.layout
+        layout.update(self.layout)
+
+        # common settings
+        ticks_off_kwargs = dict(showticklabels=False,
+                                mirror=True,
+                                showgrid=False,
+                                showline=True)
+        ticks_on_kwargs = dict(showticklabels=True,
+                               mirror=True,
+                               showgrid=False,
+                               showline=True)
+
+        # dotpolot traces and layout
+        fig.add_traces(self.traces)
+
+        layout.xaxis2.update(title=self.bottom_track.name,
+                             range=self.xrange,
+                             **ticks_on_kwargs)
+        layout.yaxis.update(title=self.left_track.name,
+                            range=self.yrange,
+                            **ticks_on_kwargs)
+
+        # seq2 traces
+        seen_types = set()
+        drawables = self.left_track.get_drawable(vertical=True)
+        max_x = 0
+        traces = []
+        for trace in drawables.traces:
+            traces.append(trace)
+            max_x = max(numpy.max(trace.x), max_x)
+            if trace.legendgroup in seen_types:
+                trace.showlegend = False
+            seen_types.add(trace.legendgroup)
+
+        fig.add_traces(traces)
+        layout.xaxis.update(title=None,
+                            range=[0, int(max_x) + 1],
+                            **ticks_off_kwargs)
+        return fig
+
+    @property
+    def figure(self):
+        if self.bottom_track and self.left_track:
+            func = self._build_2x2_fig
+        elif self.bottom_track:
+            func = self._build_2x1_fig
+        elif self.left_track:
+            func = self._build_1x2_fig
+        else:
+            func = self._build_fig
+
+        result = func()
+
+        return result
 
 
 class Shape:
