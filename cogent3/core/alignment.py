f9de7c3974d821d4e846bde42f63d44e1878513b
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2019-08-01 12:17:46 +1000

    STY: affecting recent jupyter alignment display

--- cogent3/core/alignment.py
+++ cogent3/core/alignment.py
@@ -2624,8 +2624,10 @@ class AlignmentI(object):
         table = ["<table>"]
         seq_ = "<td>%s</td>"
         label_ = '<td class="label">%s</td>'
-        num_row_ = ('<tr style="background-color:'
-                    'rgba(161, 195, 209, 0.5);"><td></td><td><b>{:,d}</b></td></tr>')
+        num_row_ = (
+            '<tr style="background-color:'
+            'rgba(161, 195, 209, 0.5);"><td></td><td><b>{:,d}</b></td></tr>'
+        )
         for i in range(0, seqlen, interleave_len):
             table.append(num_row_.format(i))
             seqblock = seqs[:, i : i + interleave_len].tolist()
