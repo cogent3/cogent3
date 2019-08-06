f8279b53fc7bbf19d9b9d7136caeac1c75e3ab45
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2019-08-06 16:42:46 +1000

    MAINT: added missing attribution/versions to some .py files

--- tests/test_core/test_features.py
+++ tests/test_core/test_features.py
@@ -6,7 +6,14 @@ from cogent3.core.annotation import Feature, Variable
 from cogent3.util.deserialise import deserialise_object
 
 
+__author__ = "Gavin Huttley"
+__copyright__ = "Copyright 2007-2019, The Cogent Project"
 __credits__ = ["Peter Maxwell", "Gavin Huttley"]
+__license__ = "BSD-3"
+__version__ = "2019.07.10a"
+__maintainer__ = "Gavin Huttley"
+__email__ = "Gavin.Huttley@anu.edu.au"
+__status__ = "Alpha"
 
 
 class FeaturesTest(TestCase):
--- tests/test_maths/test_stats/test_number.py
+++ tests/test_maths/test_stats/test_number.py
@@ -8,6 +8,16 @@ from cogent3 import LoadSeqs
 from cogent3.maths.stats import number
 
 
+__author__ = "Gavin Huttley"
+__copyright__ = "Copyright 2007-2019, The Cogent Project"
+__credits__ = ["Gavin Huttley"]
+__license__ = "BSD-3"
+__version__ = "2019.07.10a"
+__maintainer__ = "Gavin Huttley"
+__email__ = "Gavin.Huttley@anu.edu.au"
+__status__ = "Alpha"
+
+
 class TestNumber(TestCase):
     def test_construction(self):
         nums = number.CategoryCounter("AAAACCCGGGGT")
