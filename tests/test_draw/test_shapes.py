af9038bca899af2ff619cdf3a5d3398995cad258
Rahul Ghangas <u6157693@anu.edu.au>
2019-08-09 14:21:29 +1000

    TST: Added docstrings for shape tests

--- tests/test_draw/test_shapes.py
+++ tests/test_draw/test_shapes.py
@@ -17,6 +17,7 @@ __status__ = "Alpha"
 
 class Test_Shapes(TestCase):
     def test_shift(self):
+        """Tests if the shape remains consistent with a shift of coordinate system along the line x=y"""
         rectangle = drawable.Rectangle(5, 1, 7, 0.25)
         rectangle.shift(5, 5)
 
@@ -24,6 +25,7 @@ class Test_Shapes(TestCase):
         assert_allclose(rectangle.y, [12, 12.25, 12.25, 12, 12])
 
     def test_linear_transform_shift(self):
+        """Tests that the side lengths remain consistent with any arbitrary shift in the coordinate system"""
         rectangle = drawable.Rectangle(10, 5, 17, 3)
         rectangle.shift(2, 6)
 
@@ -31,6 +33,7 @@ class Test_Shapes(TestCase):
         assert_allclose(rectangle.y, [23, 26, 26, 23, 23])
 
     def test_height_property(self):
+        """Tests if the height remains consistent after any arbitrary shift in the coordinate system"""
         rectangle = drawable.Rectangle(5, 6, 6, 7)
         rectangle.shift(5, 2)
 
