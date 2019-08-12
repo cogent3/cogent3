44159261b43d1e1a8ec13639d6c78e59c7744b65
Stephen Ma <Stephen.Ma@anu.edu.au>
2019-08-12 17:30:24 +1000

    MAINT: optimised tox.ini and fixed building_allignments.rst, #154

--- doc/stack_test.py
+++ doc/stack_test.py
@@ -2,16 +2,24 @@ import glob
 import subprocess
 
 base_cmd = 'python -m doctest {}'
-for doctest_file in glob.glob("examples/*.rst"):
+
+relevant_folders = ['cookbook', 'examples']
+doctest_files = [rst_file for relevant_folder in relevant_folders for rst_file in glob.glob(relevant_folder + '/*.rst')]
+
+
+def add_relevant_folder(folder_name):
+    relevant_folders.append(folder_name)
+
+
+for doctest_file in doctest_files:
     cmd = base_cmd.format(doctest_file)
     process = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE)
     output, error = process.communicate()
     print('command: ', cmd)
-    print('========================================================')
+    print('=' * 100)
     print('output:\n')
-    print(output.decode('ascii'))
-    print('========================================================')
+    print(output.decode('ascii', 'ignore'))
+    print('=' * 100)
     print('error:\n')
     print(error)
     print('\n')
-
