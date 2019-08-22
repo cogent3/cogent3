f02f2bb9af8d3a68c7a7c8bf5c063aa2ff4a1f3f
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2019-08-22 15:46:42 +1000

    MAINT: added attribution, version and license info to test_init

--- tests/test_app/test_init.py
+++ tests/test_app/test_init.py
@@ -4,6 +4,16 @@ from unittest import TestCase, main
 from cogent3 import available_apps
 
 
+__author__ = "Gavin Huttley"
+__copyright__ = "Copyright 2007-2019, The Cogent Project"
+__credits__ = ["Gavin Huttley"]
+__license__ = "BSD-3"
+__version__ = "2019.8.20a"
+__maintainer__ = "Gavin Huttley"
+__email__ = "Gavin.Huttley@anu.edu.au"
+__status__ = "Alpha"
+
+
 class TestAvalableApps(TestCase):
     def test_available_apps(self):
         """available_apps returns a table"""
