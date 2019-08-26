5b714667230552cab15e8dbd5b6489fe1bd2a7d5
Stephen Ma <Stephen.Ma@anu.edu.au>
2019-08-26 18:34:20 +1000

    MAINT: optimised codes pattern in test_init and made codes more readable, maintains #169

--- tests/test_app/test_init.py
+++ tests/test_app/test_init.py
@@ -17,6 +17,39 @@ __email__ = "Gavin.Huttley@anu.edu.au"
 __status__ = "Alpha"
 
 
+def _get_all_composables():
+    test_model1 = evo.model("HKY85")
+    test_model2 = evo.model("GN")
+    test_hyp = evo.hypothesis(test_model1, test_model2)
+    test_num_reps = 100
+
+    applications = [
+        align.align_to_ref(),
+        align.progressive_align(model="GY94"),
+        evo.ancestral_states(),
+        evo.bootstrap(hyp=test_hyp, num_reps=test_num_reps),
+        evo.hypothesis(test_model1, test_model2),
+        evo.model("GN"),
+        evo.tabulate_stats(),
+        io.write_db(os.getcwd()),
+        io.write_json(os.getcwd()),
+        sample.fixed_length(100),
+        sample.min_length(100),
+        io.write_seqs(os.getcwd()),
+        sample.omit_bad_seqs(),
+        sample.omit_degenerates(),
+        sample.omit_duplicated(),
+        sample.take_codon_positions(1),
+        sample.take_named_seqs(),
+        sample.trim_stop_codons(gc=1),
+        translate.select_translatable(),
+        tree.quick_tree(),
+        tree.scale_branches(),
+        tree.uniformize_tree(),
+    ]
+    return applications
+
+
 class TestAvalableApps(TestCase):
     def test_available_apps(self):
         """available_apps returns a table"""
@@ -30,46 +63,16 @@ class TestAvalableApps(TestCase):
         """Properly compose two composable applications"""
         from cogent3.app.composable import Composable
 
-        # Initialise some data for the application configurations.
-        test_model1 = evo.model("HKY85")
-        test_model2 = evo.model("HKY85", name="hky85")
-        test_hyp = evo.hypothesis(test_model1, test_model2)
-        test_num_reps = 1000
-
-        applications = [
-            align.align_to_ref(),
-            align.progressive_align(model="nucleotide"),
-            evo.ancestral_states(),
-            evo.bootstrap(hyp=test_hyp, num_reps=test_num_reps),
-            evo.hypothesis(test_model1, test_model2),
-            evo.model("HKY85"),
-            evo.tabulate_stats(),
-            io.write_db(os.getcwd()),
-            io.write_json(os.getcwd()),
-            sample.fixed_length(100),
-            sample.min_length(100),
-            io.write_seqs(os.getcwd()),
-            sample.omit_bad_seqs(),
-            sample.omit_degenerates(),
-            sample.omit_duplicated(),
-            sample.take_codon_positions(1),
-            sample.take_named_seqs(),
-            sample.trim_stop_codons(gc=1),
-            translate.select_translatable(),
-            tree.quick_tree(),
-            tree.scale_branches(),
-            tree.uniformize_tree(),
-        ]
+        applications = _get_all_composables()
 
         for app in applications:
             self.assertIsInstance(app, Composable)
 
         composable_application_tuples = [
-            (fir_element, sec_element)
-            for fir_element in applications
-            for sec_element in applications
-            if fir_element != sec_element
-            and fir_element._output_type & sec_element._input_type != set()
+            (app1, app2)
+            for app1 in applications
+            for app2 in applications
+            if app1 != app2 and app1._output_type & app2._input_type != set()
         ]
 
         for composable_application_tuple in composable_application_tuples:
@@ -78,61 +81,31 @@ class TestAvalableApps(TestCase):
             # Compose two composable applications, there should not be exceptions.
             res = composable_application_tuple[0] + composable_application_tuple[1]
 
-    def test_uncomposable_pairwise_applications(self):
-        """Properly identify two uncomposable applications"""
+    def test_incompatible_pairwise_applications(self):
+        """Properly identify two incompatible applications"""
         from cogent3.app.composable import Composable
 
-        # Initialise some data for the application configurations.
-        test_model1 = evo.model("HKY85")
-        test_model2 = evo.model("GN")
-        test_hyp = evo.hypothesis(test_model1, test_model2)
-        test_num_reps = 100
-
-        applications = [
-            align.align_to_ref(),
-            align.progressive_align(model="GY94"),
-            evo.ancestral_states(),
-            evo.bootstrap(hyp=test_hyp, num_reps=test_num_reps),
-            evo.hypothesis(test_model1, test_model2),
-            evo.model("GN"),
-            evo.tabulate_stats(),
-            io.write_db(os.getcwd()),
-            io.write_json(os.getcwd()),
-            sample.fixed_length(100),
-            sample.min_length(100),
-            io.write_seqs(os.getcwd()),
-            sample.omit_bad_seqs(),
-            sample.omit_degenerates(),
-            sample.omit_duplicated(),
-            sample.take_codon_positions(1),
-            sample.take_named_seqs(),
-            sample.trim_stop_codons(gc=1),
-            translate.select_translatable(),
-            tree.quick_tree(),
-            tree.scale_branches(),
-            tree.uniformize_tree(),
-        ]
+        applications = _get_all_composables()
 
         for app in applications:
             self.assertIsInstance(app, Composable)
 
-        uncomposable_application_tuples = [
-            (fir_element, sec_element)
-            for fir_element in applications
-            for sec_element in applications
-            if fir_element != sec_element
-            and fir_element._output_type & sec_element._input_type == set()
+        incompatible_application_tuples = [
+            (app1, app2)
+            for app1 in applications
+            for app2 in applications
+            if app1 != app2 and app1._output_type & app2._input_type == set()
         ]
 
-        for uncomposable_application_tuple in uncomposable_application_tuples:
-            uncomposable_application_tuple[0].disconnect()
-            uncomposable_application_tuple[1].disconnect()
+        for incompatible_application_tuple in incompatible_application_tuples:
+            incompatible_application_tuple[0].disconnect()
+            incompatible_application_tuple[1].disconnect()
 
-            # Compose two uncomposable applications, there should be exceptions.
+            # Compose two incompatible applications, there should be exceptions.
             with self.assertRaises(TypeError):
                 res = (
-                    uncomposable_application_tuple[0]
-                    + uncomposable_application_tuple[1]
+                    incompatible_application_tuple[0]
+                    + incompatible_application_tuple[1]
                 )
 
 
