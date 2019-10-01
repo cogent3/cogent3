f01e223f2932db7ccfb7c1e1c48fd9835db84259
stephen <u6269649@anu.edu.au>
2019-10-01 17:35:51 +1000

    BUG: fixed parent issue in to_moltype method in Sequence, fixes #247

--- src/cogent3/core/sequence.py
+++ src/cogent3/core/sequence.py
@@ -724,7 +724,7 @@ class Sequence(_Annotatable, SequenceI):
         new_seq = make_seq(self)
         new = self.__class__(seq=new_seq)
         new.clear_annotations()
-        new.annotations = [ann.copy_to_seq(new_seq) for ann in self.annotations]
+        new.attach_annotations(([ann.copy_to_seq(new) for ann in self.annotations]))
         return new
 
     def _seq_filter(self, seq):
