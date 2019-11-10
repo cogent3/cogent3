6337738a0122ba951f250976267b5d81abfa1ac4
christopherbradley <u6379325@anu.edu.au>
2019-11-11 10:45:37 +1100

    TST: tested get_slice on nested annotations, fixes #366

--- tests/test_core/test_features.py
+++ tests/test_core/test_features.py
@@ -21,7 +21,6 @@ class FeaturesTest(TestCase):
 
     def setUp(self):
         # A Sequence with a couple of exons on it.
-
         self.s = DNA.make_seq(
             "AAGAAGAAGACCCCCAAAAAAAAAATTTTTTTTTTAAAAAAAAAAAAA", name="Orig"
         )
@@ -625,27 +624,10 @@ class FeaturesTest(TestCase):
         # annoyingly, comes back as list of lists
         self.assertEqual(got.xxy_list, [[list(xx), y] for xx, y in y_valued.xxy_list])
 
-    def test_nested_annotations(self):
-        """check annotations can be nested and features can be accessed recursively"""
-        feature = self.exon1.add_feature("repeat", "C", [(2, 5)])
-        self.assertEqual(feature.get_slice(), "CCC")
-        # a sequence cannot directly access a grandchild
-        self.assertEqual(self.s.get_annotations_matching("repeat"), [])
-
-        # a nested annotation can access its grandparent through recursion
-        def get_orig_parent(a):
-            return a if a.name is "Orig" else get_orig_parent(a.parent)
-
-        self.assertEqual(get_orig_parent(feature), self.s)
-
-        # a sequence can access a grandchild through recursion
-        def get_all_annotations_matching(a, type):
-            matches = a.get_annotations_matching(type)
-            for f in a.get_annotations_matching("*"):
-                matches.extend(get_all_annotations_matching(f, type))
-            return matches
-
-        self.assertEqual(get_all_annotations_matching(self.s, "repeat"), [feature])
+    def test_nested_get_slice(self):
+        """check the get_slice method works on nested annotations"""
+        nested_feature = self.exon1.add_feature("repeat", "C", [(2, 5)])
+        self.assertEqual(nested_feature.get_slice(), "CCC")
 
 
 if __name__ == "__main__":
