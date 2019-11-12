c573d8b4b01d9364fd47ab7780b197cb8175eb7a
christopherbradley <u6379325@anu.edu.au>
2019-11-13 10:38:51 +1100

    TST: made a test to expose the named_seqs bug, fixes #376

--- tests/test_core/test_alignment.py
+++ tests/test_core/test_alignment.py
@@ -871,9 +871,10 @@ class SequenceCollectionBaseTests(object):
 
     def test_annotate_from_gff(self):
         """SequenceCollection.annotate_from_gff should read gff features"""
-        aln = self.Class({"seq1": "ACGU", "seq2": "CGUA", "seq3": "CCGU"})
+        aln = self.Class({"seq1": "ACGU", "seq2": "CGUA", "seq3": "C-GU"})
         gff = [
             ["seq1", "prog1", "snp", "1", "2", "1.0", "+", "1", '"abc"'],
+            ["seq3", "prog2", "del", "1", "3", "1.0", "+", "1", '"xyz"'],
             ["seq5", "prog2", "snp", "2", "3", "1.0", "+", "1", '"yyy"'],
         ]
         gff = list(map("\t".join, gff))
@@ -887,6 +888,12 @@ class SequenceCollectionBaseTests(object):
         self.assertEqual(len(aln_seq_1.annotations), 1)
         self.assertEqual(aln_seq_1.annotations[0].name, "abc")
         self.assertEqual(len(aln_seq_2.annotations), 0)
+
+        aln_seq_3 = aln.named_seqs["seq3"]
+        if not hasattr(aln_seq_3, "annotations"):
+            aln_seq_3 = aln_seq_3.data
+        matches = [m for m in aln_seq_3.get_annotations_matching("*")]
+        self.assertFalse('-' in matches[0].get_slice())
 
     def test_add(self):
         """__add__ should concatenate sequence data, by name"""
