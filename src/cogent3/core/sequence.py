610d30d99339a8b9d7ea499beddcd1355495ff9f
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2020-02-08 12:20:09 +1100

    BUG: ArraySequenceBase fixed attribute reference

--- src/cogent3/core/sequence.py
+++ src/cogent3/core/sequence.py
@@ -1458,8 +1458,8 @@ class ArraySequenceBase(object):
         """iter returns characters of self, rather than slices."""
         if hasattr(self.alphabet, "to_string"):
             return iter(self.alphabet.to_string(self._data))
-        else:
-            return iter(self.Alpabet.from_indices(self._data))
+
+        return iter(self.alpabet.from_indices(self._data))
 
     def tostring(self):
         """to_string delegates to self._data."""
