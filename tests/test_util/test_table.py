c1b2563210108ffec1e0cad11cd3929638ca790d
wjjmjh <u6269649@anu.edu.au>
2020-08-09 18:17:33 +1000

    MAINT: redirected some stdout calls to null in test_head and test_tail regarding Table object, fixed #699

--- tests/test_util/test_table.py
+++ tests/test_util/test_table.py
@@ -2,6 +2,7 @@
 
 """Unit tests for table.
 """
+import contextlib
 import json
 import os
 import pathlib
@@ -1275,17 +1276,18 @@ class TableTests(TestCase):
         display = table.display
         head = TrapOutput()
         table.display = head
-        t = Table(header=self.t1_header, data=self.t1_rows)
-        t.head(nrows=3)
-        self.assertEqual(head.data.shape[0], 3)
-        self.assertEqual(len(head.output.splitlines()), 9)
-        self.assertEqual(head.data.tolist(), self.t1_rows[:3])
-        # tests when number of rows < default
-        t = make_table(data=dict(a=["a"], b=["b"]))
-        t.head()
-        self.assertEqual(head.data.shape[0], 1)
-        self.assertEqual(len(head.output.splitlines()), 7)
-        self.assertEqual(head.data.tolist(), [["a", "b"]])
+        with contextlib.redirect_stdout(None):
+            t = Table(header=self.t1_header, data=self.t1_rows)
+            t.head(nrows=3)
+            self.assertEqual(head.data.shape[0], 3)
+            self.assertEqual(len(head.output.splitlines()), 9)
+            self.assertEqual(head.data.tolist(), self.t1_rows[:3])
+            # tests when number of rows < default
+            t = make_table(data=dict(a=["a"], b=["b"]))
+            t.head()
+            self.assertEqual(head.data.shape[0], 1)
+            self.assertEqual(len(head.output.splitlines()), 7)
+            self.assertEqual(head.data.tolist(), [["a", "b"]])
         table.display = display
 
     def test_tail(self):
@@ -1295,17 +1297,18 @@ class TableTests(TestCase):
         display = table.display
         tail = TrapOutput()
         table.display = tail
-        t = Table(header=self.t1_header, data=self.t1_rows)
-        t.tail(nrows=3)
-        self.assertEqual(tail.data.shape[0], 3)
-        self.assertEqual(len(tail.output.splitlines()), 9)
-        self.assertEqual(tail.data.tolist(), self.t1_rows[-3:])
-        # tests when number of rows < default
-        t = make_table(data=dict(a=["a"], b=["b"]))
-        t.tail()
-        self.assertEqual(tail.data.shape[0], 1)
-        self.assertEqual(len(tail.output.splitlines()), 7)
-        self.assertEqual(tail.data.tolist(), [["a", "b"]])
+        with contextlib.redirect_stdout(None):
+            t = Table(header=self.t1_header, data=self.t1_rows)
+            t.tail(nrows=3)
+            self.assertEqual(tail.data.shape[0], 3)
+            self.assertEqual(len(tail.output.splitlines()), 9)
+            self.assertEqual(tail.data.tolist(), self.t1_rows[-3:])
+            # tests when number of rows < default
+            t = make_table(data=dict(a=["a"], b=["b"]))
+            t.tail()
+            self.assertEqual(tail.data.shape[0], 1)
+            self.assertEqual(len(tail.output.splitlines()), 7)
+            self.assertEqual(tail.data.tolist(), [["a", "b"]])
         table.display = display
 
     @skipIf(DataFrame is None, "pandas not installed")
