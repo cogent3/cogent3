a03adcbbdb0e0c5552e803bfabc8b90bae441a31
stephenrogers1 <stephenrogers4580@gmail.com>
2021-03-10 01:24:38 +1100

    ENH: function that gets likelihood of 3 seqs in unrooted tree, fixes #1
    [NEW] cogent3.evolve.alignment_likelihood.py given a rate matrix the ancestors proportion pi and a three sequence alignment
     gives the log-likelihood of the unrooted tree

---
+++ src/cogent3/evolve/alignment_likelihood.py
@@ -0,0 +1,52 @@
+from cogent3 import (
+    DNA,
+    make_aligned_seqs,
+
+)
+import math
+def logLikelihood(pi, rateMatrix, alignment):
+    """Produced a log-likelihood of an unrooted tree of three DNA sequences
+
+    Given a constant rate matrix and the ancestors ratios calculate the
+    log-likelihood of three sequences unrooted tree.
+
+    Paramters
+    ---------
+    pi: Sequence
+        An array of length 4, with ratios of ancestor in order T,C,G,A
+
+    rateMatrix: Sequence of Sequence
+          A 4X4 sequence representing each possible transition and their probabilities
+          ordered T,C,G,A where (2,4) entry represents P(C -> A)
+
+    Returns
+    -------
+    log(probability): The log of the likelihood of the unrooted tree of the
+                      three sequences
+
+    Examples
+    --------
+    >>> likelihoodCalculator(pi, rateMatrix, alignment2) # See above
+    -3.278666124
+    """
+    probability = 1
+    length = len(alignment)
+    for i in range(0,length):
+            probability *= probabilityPosition(pi, rateMatrix, alignment[i])
+    return math.log10(probability)
+
+def probabilityPosition(pi, rm, alnCol):
+    probability = 0
+    for i in range(0,4):
+        probability += pi[i]*rm[i][baseToPos(alnCol.seqs[0])]*rm[i][baseToPos(alnCol.seqs[1])]*rm[i][baseToPos(alnCol.seqs[2])]
+    return (probability)    
+
+def baseToPos(base):
+    if base == "T":
+        return 0
+    if base == "C":
+        return 1
+    if base == "G":
+        return 2
+    if base == "A":
+        return 3
---
+++ tests/test_evolve/test_alignment_likelihood.py
@@ -0,0 +1,35 @@
+from cogent3 import (
+    DNA,
+    make_aligned_seqs,
+)
+import math
+
+from unittest import TestCase, main
+
+from cogent3.evolve.alignment_likelihood import logLikelihood
+alignment = make_aligned_seqs(
+        data=[("s1", "A"), ("s2", "G"),("s3", "A")], moltype=DNA
+    )
+alignment2 = make_aligned_seqs(
+        data=[("s1", "AT"), ("s2", "AT"),("s3", "AC")], moltype=DNA
+)
+alignment3 = make_aligned_seqs(
+        data=[("s1", "AGC"), ("s2", "ATA"),("s3", "ACC")], moltype=DNA
+)     
+rateMatrix = [
+               [0.95,  0.01,  0.01,  0.03  ],
+               [0.005, 0.98,  0.01,  0.005 ],
+               [0.005, 0.02,  0.95,  0.015 ],
+               [0.001, 0.003, 0.006, 0.99  ]
+                                          ]
+pi = [0.3, 0.2, 0.3, 0.2]      
+
+import pytest
+from numpy.testing import assert_allclose, assert_almost_equal, assert_equal
+
+def test_alignment():
+     assert_allclose((logLikelihood(pi, rateMatrix, alignment2)), -3.278666124)
+def test_alignment2():
+     assert_almost_equal(logLikelihood(pi, rateMatrix, alignment3), -7.9027699)
+def test_alignment3():
+     assert_almost_equal(logLikelihood(pi, rateMatrix, alignment), -2.905530618)   
