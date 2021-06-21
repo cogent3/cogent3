caf972264138129f7ec860ecb5367e3aa2c73e93
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2021-06-21 13:47:24 +1000

    MAINT: removed unused imports, variables in numba alignment functions

--- src/cogent3/align/pairwise_pogs_numba.py
+++ src/cogent3/align/pairwise_pogs_numba.py
@@ -1,7 +1,6 @@
-import numba
 import numpy as np
 
-from numba import boolean, float64, int32, int64, njit, optional, types, uint8
+from numba import boolean, float64, int64, njit, optional, uint8
 from numba.core.types.containers import Tuple
 
 
@@ -85,18 +84,14 @@ def calc_rows(
     N = max(T.shape[0], T.shape[1])
 
     dest_states = max(0, state_directions.shape[0])
-    d4 = max(4, state_directions.shape[1])
 
     row_count = x_index.shape[0]
     row_length = y_index.shape[0]
 
-    bin_count = match_scores.shape[0]
     max_x = match_scores.shape[1]
     max_y = match_scores.shape[2]
 
-    bin_count = max(xgap_scores.shape[0], bin_count)
     max_x = max(xgap_scores.shape[1], max_x)
-    bin_count = max(ygap_scores.shape[0], bin_count)
     max_y = max(ygap_scores.shape[1], max_y)
 
     for i in range(row_count):
@@ -107,20 +102,10 @@ def calc_rows(
 
     assert j_low >= 0 and j_high > j_low and j_high <= row_length
 
-    j_link_count = j_sources.shape[0]
-    row_length1 = row_length + 1
-    row_length1 = max(row_length1, j_sources_offsets.shape[0])
-
-    i_link_count = i_sources.shape[0]
-    row_count1 = row_count + 1
-    row_count1 = max(row_count1, i_sources_offsets.shape[0])
-
-    tmp_rows = mantissas.shape[0]
     row_length = max(mantissas.shape[1], row_length)
     N = max(mantissas.shape[2], N)
 
     if use_scaling:
-        tmp_rows = max(exponents.shape[0], tmp_rows)
         row_length = max(exponents.shape[1], row_length)
         N = max(exponents.shape[2], N)
 
@@ -130,8 +115,6 @@ def calc_rows(
         impossible = 0.0
 
     if viterbi and track is not None and track_enc is not None:
-        row_count = max(track.shape[0], row_count)
-        row_length = max(track.shape[1], row_length)
         N = max(track.shape[2], N)
         (tcode_x, tcode_y, tcode_s) = track_enc
     else:
