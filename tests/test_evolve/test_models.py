741f8c0d1365be00eec7e34ed5ee8ddf0e669767
GavinHuttley <Gavin.Huttley@anu.edu.au>
2021-09-23 07:43:15 +1000

    MAINT: simplify logic in test
    [CHANGED] the `startswith()` check is more general than equals but includes it,
        so just use `startswith()`.

--- tests/test_evolve/test_models.py
+++ tests/test_evolve/test_models.py
@@ -104,10 +104,7 @@ class CannedModelsTest(TestCase):
         """name attribute matches model name"""
         for model_name in models:
             model = get_model(model_name)
-            if model.name is not model_name:
-                self.assertTrue(model.name.startswith(model_name))
-            else:
-                self.assertEqual(model.name, model_name)
+            self.assertTrue(model.name.startswith(model_name))
 
 
 def get_sample_model_types(mod_type=None):
