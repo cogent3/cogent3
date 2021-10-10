77fd3a25cd74a3b47aa2f7af1dc453a6e087d05a
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2021-10-11 08:00:21 +1100

    MAINT: ensure no left-over tinydb's when running MPI test

--- tests/test_app/test_app_mpi.py
+++ tests/test_app/test_app_mpi.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+from tempfile import TemporaryDirectory
 from unittest import TestCase, main, skipUnless
 
 from cogent3.app import align as align_app
@@ -24,27 +26,29 @@ class MPITests(TestCase):
         """writing with overwrite in MPI should reset db"""
         dstore = io_app.get_data_store("data", suffix="fasta")
         members = dstore.filtered(callback=lambda x: "brca1.fasta" not in x.split("/"))
-        reader = io_app.load_unaligned()
-        aligner = align_app.align_to_ref()
-        writer = write_db("delme.tinydb", create=True, if_exists="overwrite")
-        process = reader + aligner + writer
+        with TemporaryDirectory(dir=".") as dirname:
+            path = Path(dirname) / "delme.tinydb"
+            reader = io_app.load_unaligned()
+            aligner = align_app.align_to_ref()
+            writer = write_db(path, create=True, if_exists="overwrite")
+            process = reader + aligner + writer
 
-        r = process.apply_to(
-            members,
-            logger=False,
-            show_progress=False,
-            parallel=True,
-            par_kw=dict(use_mpi=True),
-        )
+            r = process.apply_to(
+                members,
+                logger=False,
+                show_progress=False,
+                parallel=True,
+                par_kw=dict(use_mpi=True),
+            )
 
-        expect = [str(m) for m in process.data_store]
-        process.data_store.close()
+            expect = [str(m) for m in process.data_store]
+            process.data_store.close()
 
-        # now get read only and check what's in there
-        result = io_app.get_data_store("delme.tinydb")
-        got = [str(m) for m in result]
+            # now get read only and check what's in there
+            result = io_app.get_data_store(path)
+            got = [str(m) for m in result]
 
-        assert got == expect
+            assert got == expect
 
 
 if __name__ == "__main__":
