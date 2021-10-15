710e2087cb427ed3e785ae4d3cd4918ec96d642c
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2021-10-15 17:16:43 +1100

    MAINT: removing unused variables from cogent3

--- src/cogent3/recalculation/calculation.py
+++ src/cogent3/recalculation/calculation.py
@@ -524,7 +524,7 @@ class Calculator(object):
                 t0 = time.time()
                 data[cell.rank] = cell.calc(*[data[a] for a in cell.arg_ranks])
                 t1 = time.time()
-            except (ParameterOutOfBoundsError, ArithmeticError) as exception:
+            except (ParameterOutOfBoundsError, ArithmeticError):
                 error_cell = cell
                 break
             elapsed[cell.rank] = t1 - t0
