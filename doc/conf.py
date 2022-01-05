272b01c9a6ac7cb6164655b5ba79d30fc3262002
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2022-01-05 14:02:29 +1100

    DOC: renaming draw_examples to draw
    [CHANGED] prior to refactor using nbsphinx to make image gallery
        instead of sphinx-gallery. NOTE: we still need the latter for
        its gallery style.

--- doc/conf.py
+++ doc/conf.py
@@ -51,9 +51,8 @@ exclude_patterns = [
     "**.ipynb_checkpoints",
     "cookbook/union_dict.rst",
     "cookbook/loading_tabular",
-    "draw_examples/README.rst",
-    "draw_examples/aln/README.rst",
-    "draw_examples/tree/README.rst",
+    "draw/aln/README.rst",
+    "draw/tree/README.rst",
     "COGENT3_LICENSE.rst",
     "*tmp*",
 ]
@@ -105,65 +104,6 @@ htmlhelp_basename = "cogent3doc"
 # -- Options for Sphinx Gallery
 
 nbsphinx_requirejs_path = "require.js"
-
-# This is a veryt ugly hack!
-# We have to set the following environment variable
-# for Sphinx-Gallery to work, but we need the default Plotly setting for correct
-# display in normal notebooks. This works because of an order of execution,
-# over which we have no control, putting the gallery generation first.
-os.environ["PLOTLY_RENDERER"] = "sphinx_gallery"
-
-
-def plotly_sg_scraper(block, block_vars, gallery_conf, *args, **kwargs):
-    examples_dirs = gallery_conf["examples_dirs"]
-    if isinstance(examples_dirs, (list, tuple)):
-        examples_dirs = examples_dirs[0]
-    pngs = sorted(glob(os.path.join(examples_dirs, "**", "*.png"), recursive=True))
-    htmls = sorted(glob(os.path.join(examples_dirs, "**", "*.html"), recursive=True))
-    image_path_iterator = block_vars["image_path_iterator"]
-    image_names = []
-    seen = set()
-    for html, png in zip(htmls, pngs):
-        if png not in seen:
-            seen |= {png}
-            this_image_path_png = next(image_path_iterator)
-            this_image_path_html = os.path.splitext(this_image_path_png)[0] + ".html"
-            image_names.append(this_image_path_html)
-            shutil.move(png, this_image_path_png)
-            shutil.move(html, this_image_path_html)
-    # Use the `figure_rst` helper function to generate rST for image files
-    from plotly.io._sg_scraper import figure_rst
-
-    return figure_rst(image_names, gallery_conf["src_dir"])
-
-
-def plotly_sg_scraper_nb(*args, **kwargs):
-    # set the plotly renderer
-    os.environ["PLOTLY_RENDERER"] = "sphinx_gallery"
-    try:
-        result = plotly_sg_scraper(*args, **kwargs)
-    except Exception:
-        result = ""
-
-    os.environ.pop("PLOTLY_RENDERER")
-    return result
-
-
-image_scrapers = (plotly_sg_scraper_nb,)
-
-example_dirs = ["draw_examples"]
-gallery_dirs = ["draw"]
-
-
-sphinx_gallery_conf = {
-    "examples_dirs": example_dirs,
-    "subsection_order": ExplicitOrder(["draw_examples/aln", "draw_examples/tree"]),
-    "abort_on_example_error": True,
-    "within_subsection_order": FileNameSortKey,
-    "gallery_dirs": gallery_dirs,
-    "image_scrapers": image_scrapers,
-    "download_all_examples": False,
-}
 
 # -- Options for LaTeX output --------------------------------------------------
 latex_documents = [
