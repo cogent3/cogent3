de05ecc8ade877721bf84c1e26833d972fd0b11a
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2022-04-29 14:16:57 +1000

    TST: remove test for unsupported python version

--- tests/test_util/test_parallel.py
+++ tests/test_util/test_parallel.py
@@ -38,7 +38,7 @@ class ParallelTests(TestCase):
     def test_create_processes(self):
         """Procressor pool should create multiple distingue processes"""
         max_worker_count = multiprocessing.cpu_count() - 1
-        index = [index for index in range(max_worker_count)]
+        index = list(range(max_worker_count))
         result = parallel.map(get_process_value, index, max_workers=None, use_mpi=False)
         result_processes = [v[0] for v in result]
         result_values = [v[1] for v in result]
@@ -70,14 +70,6 @@ class ParallelTests(TestCase):
             if result:
                 master_processes += 1
         self.assertEqual(master_processes, 0)
-
-    @skipIf(sys.version_info[1] >= 7, "exception test for Python 3.6")
-    def test_is_master_process_version_exception(self):
-        """
-        is_master_process() should throw an exception
-        for Python versions below 3.7
-        """
-        self.assertRaises(RuntimeError, parallel.is_master_process)
 
 
 if __name__ == "__main__":
