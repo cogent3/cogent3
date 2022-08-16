afe6673004fb208390321c920a7c65842bdde306
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2022-08-16 10:35:25 +1000

    BUG: fix __repr__ of composed functions

--- tests/test_app/test_tree.py
+++ tests/test_app/test_tree.py
@@ -69,7 +69,6 @@ class TestTree(TestCase):
         tree1 = quick1(dist_matrix)
         self.assertEqual(set(tree1.get_tip_names()), set(aln.names))
 
-    @pytest.mark.xfail
     def test_composable_apps(self):
         """checks the ability of these two apps(fast_slow_dist and quick_tree) to communicate"""
         path = os.path.join(data_path, "brca1_5.paml")
@@ -85,9 +84,6 @@ class TestTree(TestCase):
         )
         self.assertIsInstance(proc, tree_app.quick_tree)
         self.assertIsInstance(proc.input, dist.fast_slow_dist)
-        self.assertIsInstance(proc._input_types, frozenset)
-        self.assertIsInstance(proc._output_types, frozenset)
-        self.assertIsInstance(proc._in, dist.fast_slow_dist)
 
         tree1 = proc(aln1)
         self.assertIsInstance(tree1, PhyloNode)
