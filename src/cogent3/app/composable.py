a307fc91c7a708825a5953bae25ae6f8c0ef955e
Nick Shahmaras <ni.fotovat@gmail.com>
2022-10-17 15:26:55 +1100

    ENH: merge composable_new.py to composabe.py

--- src/cogent3/app/composable.py
+++ src/cogent3/app/composable.py
@@ -1000,7 +1000,7 @@ def _call(self, val, *args, **kwargs):
     try:
         result = self.main(val, *args, **kwargs)
     except Exception:
-        result = NotCompleted("ERROR", self, traceback.format_exc(), source=val)
+        result = NotCompleted("ERROR", self, traceback.format_exc(), source=source)
 
     if result is None:
         result = NotCompleted("BUG", self, "unexpected output value None", source=val)
