0fffbf1ea98741fe19728c377a16b9195c692d16
Nick Shahmaras <ni.fotovat@gmail.com>
2022-10-18 23:42:41 +1100

    DEV: add write_to_logger function to Datamember and DataStoreMember classes, move it into for loop

--- src/cogent3/app/data_store.py
+++ src/cogent3/app/data_store.py
@@ -125,6 +125,11 @@ class DataStoreMember(str):
     def md5(self):
         return self.parent.md5(self, force=True)
 
+    def write_to_logger(self, logger):
+        md5 = self.md5
+        logger.log_message(str(self), label="output")
+        logger.log_message(md5, label="output md5sum")
+
 
 class ReadOnlyDataStoreBase:
 
