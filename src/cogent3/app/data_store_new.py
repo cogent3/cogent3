38cc632b135f8162aeee147925f0e0866b6c53db
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2022-12-13 12:02:01 +1100

    ENH: DataMember equaltiy check includes type

--- src/cogent3/app/data_store_new.py
+++ src/cogent3/app/data_store_new.py
@@ -77,7 +77,10 @@ class DataMemberABC(ABC):
     def __eq__(self, other):
         """to check equality of members and check existence of a
         member in a list of members"""
-        return (self.data_store, self.unique_id) == (other.data_store, other.unique_id)
+        return type(self) == type(other) and (self.data_store, self.unique_id) == (
+            other.data_store,
+            other.unique_id,
+        )
 
     @property
     def md5(self):
