7820c8e0de4d4ecce826f89da1968574ef0b0d72
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2022-12-16 18:45:59 +1100

    MAINT: be more consistent in use of skip_not_completed

--- src/cogent3/app/composable.py
+++ src/cogent3/app/composable.py
@@ -1007,10 +1007,9 @@ def _call(self, val, *args, **kwargs):
     if isinstance(val, NotCompleted) and self._skip_not_completed:
         return val
 
-    # todo we should get the source information from val here
     if self.app_type is not LOADER and self.input:  # passing to connected app
         val = self.input(val, *args, **kwargs)
-        if isinstance(val, NotCompleted):
+        if isinstance(val, NotCompleted) and self._skip_not_completed:
             return val
 
     type_checked = self._validate_data_type(val)
