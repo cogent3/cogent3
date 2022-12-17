34f24cadf89eae3a476b074c2132d8654c89276a
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2022-12-18 10:56:10 +1100

    ENH: convert_tinydb_to_sqlite now uses write_db()
    [CHANGED] this ensures the result is written using default
        settings

--- tests/test_app/test_data_store_new.py
+++ tests/test_app/test_data_store_new.py
@@ -284,9 +284,24 @@ def test_convert_tinydb_to_sqlite_error(tmp_dir):
     path = tmp_dir / "sample_locked.tinydb"
     shutil.copy(DATA_DIR / path.name, path)
     dest = tmp_dir / "data1.sqlitedb"
+    # create a file at dest path to trigger error
     dest.write_text("blah")
     with pytest.raises(IOError):
         _ = convert_tinydb_to_sqlite(path, dest=dest)
+
+
+@pytest.mark.parametrize(
+    "orig",
+    (
+        DATA_DIR / "sample_locked.tinydb",
+        DATA_DIR.parent.parent / "doc" / "data" / "demo-locked.tinydb",
+    ),
+)
+def test_convert_tinydbs_to_sqlite(tmp_dir, orig):
+    src = tmp_dir / orig.name
+    shutil.copy(orig, src)
+    dest = tmp_dir / "data1.sqlitedb"
+    got = convert_tinydb_to_sqlite(src, dest=dest)
 
 
 def test_convert_directory_datastore(Sample_oldDirectoryDataStore, write_dir):
