ba0694250ef3b41f4a0d58045c7bb65ed2fd83f4
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2023-06-28 17:17:01 +1000

    MAINT: remove unused variable

--- tests/test_align/test_align.py
+++ tests/test_align/test_align.py
@@ -314,10 +314,10 @@ def test_tree_align_pwise_iter(seqs):
     kwargs = dict(
         model="F81", seqs=seqs, show_progress=False, indel_rate=1e-3, indel_length=1e-1
     )
-    aln, tree = cogent3.align.progressive.tree_align(iters=None, **kwargs)
+    aln, _ = cogent3.align.progressive.tree_align(iters=None, **kwargs)
     one = aln.alignment_quality(app_name="sp_score", calc="pdist")
     for _ in range(10):
-        aln, tree = cogent3.align.progressive.tree_align(
+        aln, _ = cogent3.align.progressive.tree_align(
             iters=1, approx_dists=True, **kwargs
         )
         two = aln.alignment_quality(app_name="sp_score", calc="pdist")
