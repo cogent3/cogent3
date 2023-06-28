0bb9e9ba27cfdddedc113b2632132071256ddb61
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2023-06-28 17:51:55 +1000

    DEV: mark the large lin rajan moret test as slow
    [NEW] added pytest.mark.slow, deocrated
        test_dist_matches_original_implementation_bigtree and don't evaluate
        that during github action runs
    
    [NEW] use `nox -s test_slow-{py version}` to execute tests marked as slow

--- tests/test_tree_distance.py
+++ tests/test_tree_distance.py
@@ -16,14 +16,19 @@ def test_different_trees():
     assert distance == 8
 
 
-@pytest.mark.parametrize(
-    "path,expect",
-    (
-        (DATA_DIR / "match_dist_100.tree", 1402),
-        (DATA_DIR / "match_dist_2000.tree", 17037),
-    ),
-)
-def test_dist_matches_original_implementation(path, expect):
+def test_dist_matches_original_implementation_small_tree():
+    path, expect = DATA_DIR / "match_dist_100.tree", 1402
+    data = pathlib.Path(path).read_text().splitlines()
+    t1 = make_tree(data[0])
+    t2 = make_tree(data[1])
+    distance = lin_rajan_moret(t1, t2)
+    # Assert that this is same value as original C implementation
+    assert distance == expect
+
+
+@pytest.mark.slow
+def test_dist_matches_original_implementation_bigtree():
+    path, expect = DATA_DIR / "match_dist_2000.tree", 17037
     data = pathlib.Path(path).read_text().splitlines()
     t1 = make_tree(data[0])
     t2 = make_tree(data[1])
