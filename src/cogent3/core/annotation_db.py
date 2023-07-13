20b998231c90f7b484cf06d807c6b9d5db140adf
KatherineCaley <u6096186@anu.edu.au>
2023-07-13 19:02:34 +1000

    MAINT: deindent code for readability

--- src/cogent3/core/annotation_db.py
+++ src/cogent3/core/annotation_db.py
@@ -691,21 +691,23 @@ class SqliteAnnotationDbMixin:
                 # multiple values for parent means this is better expressed
                 # as an OR clause
                 # todo modify the conditional SQL generation
-                if result["parent_id"]:
-                    for name in result["parent_id"].replace(" ", "").split(","):
-                        if parent := list(
-                            self._get_feature_by_id(
-                                table_name=table_name,
-                                columns=columns,
-                                column="name",
-                                name=name,
-                            )
-                        ):
-                            parent = parent[0]
-                            parent.pop(
-                                "parent_id"
-                            )  # remove invalid field for the FeatureDataType
-                            yield parent
+                if not result["parent_id"]:
+                    return 
+                
+                for name in result["parent_id"].replace(" ", "").split(","):
+                    if parent := list(
+                        self._get_feature_by_id(
+                            table_name=table_name,
+                            columns=columns,
+                            column="name",
+                            name=name,
+                        )
+                    ):
+                        parent = parent[0]
+                        parent.pop(
+                            "parent_id"
+                        )  # remove invalid field for the FeatureDataType
+                        yield parent
 
     def get_records_matching(
         self,
