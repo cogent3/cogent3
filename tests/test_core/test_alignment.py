60d08f6c05f47cefbbea9b57a2d830ba420da4c3
KatherineCaley <u6096186@anu.edu.au>
2023-11-24 10:09:58 +1100

    TST: tests added for aln.get_seq(), #1639

--- tests/test_core/test_alignment.py
+++ tests/test_core/test_alignment.py
@@ -3722,3 +3722,28 @@ def test_get_seq_returns_view():
     expect = str(a1.named_seqs["s1"])
     got = str(a1.get_seq("s1"))
     assert got == expect, (got, expect)
+
+
+def test_get_seq_returns_view_with_gaps():
+    seqs = {
+        "s1": "GTTGAAGTAGTAGAAGTTCCAAATAATGAA",
+        "s2": "GTG------GTAGAAGTTCCAAATAATGAA",
+        "s3": "GCTGAAGTAGTGGAAGTTGCAAAT---GAA",
+    }
+    aln = make_aligned_seqs(data=seqs, moltype="dna", array_align=False)
+    a1 = aln[1:5]
+    expect = str(a1.named_seqs["s2"])
+    got = str(a1.get_seq("s2"))
+    assert got == expect, (got, expect)
+
+
+def test_get_seq_returns_sequence():
+    seqs = {
+        "s1": "GTTGAAGTAGTAGAAGTTCCAAATAATGAA",
+        "s2": "GTG------GTAGAAGTTCCAAATAATGAA",
+        "s3": "GCTGAAGTAGTGGAAGTTGCAAAT---GAA",
+    }
+    aln = make_aligned_seqs(data=seqs, moltype="dna", array_align=False)
+    a1 = aln[1:5]
+    got = a1.get_seq("s1")
+    assert isinstance(got, Sequence), got
