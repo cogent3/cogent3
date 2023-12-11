a8856d04763476745fc200fd9e001761011f6ea0
Robert McArthur <robert.mcarthur@anu.edu.au>
2023-12-11 13:55:04 +1100

    TST: document derivations for tree distance test cases

--- tests/test_tree_distance.py
+++ tests/test_tree_distance.py
@@ -18,18 +18,22 @@ UNROOTED_DISTANCE_MEASURES = lin_rajan_moret, unrooted_robinson_foulds
 def test_unrooted_rf_different_trees():
     a = make_tree(treestring="(a, b, (c, (d, e)));")
 
+    # There are two splits not in common for the two trees
+    # abc-de in tree a, and ad-dce in tree b.
     b = make_tree(treestring="(a, b, (d, (c, e)));")
     distance = unrooted_robinson_foulds(a, b)
     assert distance == 2
     distance = unrooted_robinson_foulds(b, a)
     assert distance == 2
 
+    # Identical unrooted tree as before, but a different orientation.
     b = make_tree(treestring="((a, b), d, (c, e));")
     distance = unrooted_robinson_foulds(a, b)
     assert distance == 2
     distance = unrooted_robinson_foulds(b, a)
     assert distance == 2
 
+    # Identical unrooted tree as before, but a different orientation.
     b = make_tree(treestring="(((a, b), d), c, e);")
     distance = unrooted_robinson_foulds(a, b)
     assert distance == 2
@@ -38,6 +42,10 @@ def test_unrooted_rf_different_trees():
 
     a = make_tree(treestring="(a, (b, (c, d)), (e, (f, (g, h))));")
     b = make_tree(treestring="(a, (c, (b, d)), (h, (f, (g, e))));")
+
+    # There are six splits not in common.
+    # For tree a they are cd-abefgh, abcdef-gh, and abcde-fgh
+    # For tree b they are bd-acefgh, abcdhf-ge, and abcdh-fge
     distance = unrooted_robinson_foulds(a, b)
     assert distance == 6
     distance = unrooted_robinson_foulds(b, a)
@@ -45,6 +53,8 @@ def test_unrooted_rf_different_trees():
 
 
 def test_unrooted_rf_same_tree():
+    # Topology of the unrooted trees are identical
+    # though they are stored at a different orientation.
     a = make_tree(treestring="(a, b, (c, d));")
     b = make_tree(treestring="(c, d, (a, b));")
 
@@ -59,6 +69,8 @@ def test_unrooted_rf_distance_multifurcation():
     a = make_tree(treestring="(a, b, (c, (d, e)));")
     b = make_tree(treestring="(a, b, (c, d, e));")
 
+    # There is one split not in common.
+    # Tree a: abc-de
     distance = unrooted_robinson_foulds(a, b)
     assert distance == 1
 
@@ -68,6 +80,9 @@ def test_unrooted_rf_distance_multifurcation():
     a = make_tree(treestring="(a, b, (((c1, c2), (d1, (d2, d3))), (e1, (e2, e3))));")
     b = make_tree(treestring="(a, b, ((c1, c2), (d2, (d1, d3)), (e1, e2, e3)));")
 
+    # There are four splits not in common
+    # Tree a: e1-others, d2,d3-others, c's and d's - others
+    # Tree b: d1,d3-others
     distance = unrooted_robinson_foulds(a, b)
     assert distance == 4
     distance = unrooted_robinson_foulds(b, a)
@@ -104,6 +119,8 @@ def test_lrm_matches_original_implementation_big_tree(DATA_DIR):
 
 
 def test_lrm_same_tree():
+    # Topology of the unrooted trees are identical
+    # though they are stored at a different orientation.
     a = make_tree(treestring="(a, b, (c, d));")
     b = make_tree(treestring="(c, d, (a, b));")
 
@@ -119,6 +136,9 @@ def test_rooted_rf_multifurcation():
     a = make_tree(treestring="((a, b), (c, d));")
     b = make_tree(treestring="((a, c, b), d);")
 
+    # There are three clades not in common
+    # Tree a: ab, cd
+    # Tree b: acb
     distance = rooted_robinson_foulds(a, b)
     assert distance == 3
 
@@ -134,6 +154,9 @@ def test_rooted_rf_different_trees():
         treestring="((((a1,a2),(b1,b2,b3)),x,(c1,c2,c3),(d1,d2)),(e1,(e2,e3)));"
     )
 
+    # Moving the single x up one level creates two clades that are not in common.
+    # Tree a: ABx
+    # Tree b: AB
     distance = rooted_robinson_foulds(a, b)
     assert distance == 2
 
@@ -143,6 +166,9 @@ def test_rooted_rf_different_trees():
     a = make_tree(treestring="((a,b),c);")
     b = make_tree(treestring="((a,c),b);")
 
+    # There are two clades not in common
+    # Tree a: ab
+    # Tree b: ac
     distance = rooted_robinson_foulds(a, b)
     assert distance == 2
 
@@ -152,6 +178,9 @@ def test_rooted_rf_different_trees():
     a = make_tree(treestring="((a,(b1,(b2,b3))),((c1,c2),(c3,c4)));")
     b = make_tree(treestring="((a,((c1,c2),(c3,c4))),(b1,(b2,b3)));")
 
+    # There are two clades not in common
+    # Tree a: aB
+    # Tree b: aC
     distance = rooted_robinson_foulds(a, b)
     assert distance == 2
 
@@ -161,6 +190,9 @@ def test_rooted_rf_different_trees():
     a = make_tree(treestring="(a,(b,(c,(d,(e,f)))));")
     b = make_tree(treestring="(a,(f,(c,(d,(b,e)))));")
 
+    # There are six clades not in common
+    # Tree a: ef, def, cdef
+    # Tree b: be, bde, bcde
     distance = rooted_robinson_foulds(a, b)
     assert distance == 6
 
@@ -171,12 +203,16 @@ def test_rooted_rf_different_trees():
 def test_rooted_rf_same_tree():
     a = make_tree(treestring="((a, b), (c, d));")
     b = make_tree(treestring="((c, d), (a, b));")
+
+    # The distance between trees of the same topology is 0
     distance = rooted_robinson_foulds(a, b)
     assert distance == 0
 
 
 # matching cluster distance
 def test_matching_cluster_distance_multifurcation():
+    # The basic example from the matching cluster distance paper
+    # see reference in matching_cluster_distance
     a = make_tree(treestring="((a, b), (c, d));")
     b = make_tree(treestring="((a, c, b), d);")
 
@@ -188,6 +224,8 @@ def test_matching_cluster_distance_multifurcation():
 
 
 def test_matching_cluster_distance_properties():
+    # Test properties of the matching cluster distance paper shown as exampels
+    # see reference in matching_cluster_distance
     a = make_tree(
         treestring="((((a1,a2),(b1,b2,b3),x),(c1,c2,c3),(d1,d2)),(e1,(e2,e3)));"
     )
@@ -223,6 +261,8 @@ def test_matching_cluster_distance_properties():
 def test_matching_cluster_distance_same_tree():
     a = make_tree(treestring="((a, b), (c, d));")
     b = make_tree(treestring="((c, d), (a, b));")
+
+    # The distance between trees of the same topology is 0
     distance = matching_cluster_distance(a, b)
     assert distance == 0
 
@@ -296,7 +336,7 @@ def test_matching_cluster_fails_different_tips(b):
         matching_cluster_distance(t2, t1)
 
 
-# rooted/unrooted distance measures applied only to rooted/unrooted trees
+# unrooted distance measures applied to examples with rooted trees
 @pytest.mark.parametrize(
     "a,b",
     (
@@ -314,6 +354,7 @@ def test_unrooted_dist_fails_rooted_trees(a, b):
             unrooted_distance_measure(t1, t2)
 
 
+# rooted distance measures applied to examples with unrooted trees
 @pytest.mark.parametrize(
     "a,b",
     (
