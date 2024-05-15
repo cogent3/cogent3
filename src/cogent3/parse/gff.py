d562585d16b61795f960d52c3f834f7422710896
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2024-05-15 18:00:19 +1000

    API: added gff3 flag argumnent to gff_parser
    [CHANGED] this frees parser from having to always start at
        the top of a file to figure out the format version.

--- src/cogent3/parse/gff.py
+++ src/cogent3/parse/gff.py
@@ -19,6 +19,34 @@ OptionalBool = typing.Optional[bool]
 
 
 @functools.singledispatch
+def is_gff3(f) -> bool:
+    """True if gff-version is 3"""
+    raise TypeError(f"unsopported type type {type(f)}")
+
+
+@is_gff3.register
+def _(f: PathType) -> bool:
+    gff3_header = "gff-version 3"
+    with open_(f) as f:
+        is_gff3 = gff3_header in f.readline()
+        f.seek(0)
+    return is_gff3
+
+
+@is_gff3.register
+def _(f: io.IOBase) -> bool:
+    gff3_header = "gff-version 3"
+    is_gff3 = gff3_header in f.readline()
+    f.seek(0)
+    return is_gff3
+
+
+@is_gff3.register
+def _(f: typing.Union[list, tuple]) -> bool:
+    gff3_header = "gff-version 3"
+    return f and gff3_header in f[0]
+
+
 _gff_item_map = {
     "type": "biotype",
     "end": "stop",
@@ -111,10 +139,12 @@ class GffRecord:
         return {k: getattr(self, k) for k in self.__slots__}
 
 
+@functools.singledispatch
 def gff_parser(
     f: typing.Union[PathType, typing.IO, OptionalStrContainer],
     attribute_parser: OptionalCallable = None,
     seqids: OptionalStrContainer = None,
+    gff3: OptionalBool = None,
 ) -> typing.Iterable[GffRecord]:
     """parses a gff file
 
@@ -126,23 +156,20 @@ def gff_parser(
         callback function for custom handling of attributes field. Defaults to
         a function that converts Attributes field into a dict based on the gff
         version.
+    seqids
+        only gff records matching these sequence IDs are returned
+    gff3
+        True if the format is gff3
 
     Returns
     -------
     dict
         contains each of the 9 parameters specified by gff3, and comments.
     """
-    yield from _gff_parser(f, attribute_parser=attribute_parser, seqids=seqids)
-
-
-@gff_parser.register
-def _(
-    f: str,
-    attribute_parser: OptionalCallable = None,
-    seqids: OptionalStrContainer = None,
-) -> typing.Iterable[dict]:
-    with open_(f) as infile:
-        yield from gff_parser(infile, attribute_parser=attribute_parser, seqids=seqids)
+    gff3 = gff3 or is_gff3(f)
+    yield from _gff_parser(
+        f, attribute_parser=attribute_parser, seqids=seqids, gff3=gff3
+    )
 
 
 @gff_parser.register
@@ -150,27 +177,24 @@ def _(
     f: PathType,
     attribute_parser: OptionalCallable = None,
     seqids: OptionalStrContainer = None,
+    gff3: OptionalBool = None,
 ) -> typing.Iterable[GffRecord]:
 
     with open_(f) as infile:
-        yield from gff_parser(infile, attribute_parser=attribute_parser, seqids=seqids)
+        yield from gff_parser(
+            infile, attribute_parser=attribute_parser, seqids=seqids, gff3=gff3
+        )
 
 
 def _gff_parser(
     f: typing.Union[PathType, typing.IO, OptionalStrContainer],
     attribute_parser: OptionalCallable = None,
     seqids: OptionalStrContainer = None,
+    gff3: bool = True,
 ) -> typing.Iterable[GffRecord]:
     """parses a gff file"""
     seqids = seqids or set()
     seqids = {seqids} if isinstance(seqids, str) else set(seqids)
-
-    gff3_header = "gff-version 3"
-    if isinstance(f, list):
-        gff3 = f and gff3_header in f[0]
-    else:
-        gff3 = gff3_header in f.readline()
-        f.seek(0)
 
     if attribute_parser is None:
         attribute_parser = parse_attributes_gff3 if gff3 else parse_attributes_gff2
@@ -189,7 +213,7 @@ def _gff_parser(
         # the final column (attributes) may be empty
         if len(cols) == 8:
             cols.append("")
-        assert len(cols) == 9, len(line)
+        assert len(cols) == 9, repr(cols)
         seqid, source, type_, start, end, score, strand, phase, attributes = cols
 
         if seqids and seqid not in seqids:
