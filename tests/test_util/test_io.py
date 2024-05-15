5fb82ad03b75a43460319ea9b7e6f7a13870172f
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2024-05-16 09:12:13 +1000

    MAINT: fixed another py 3.9 test issue

--- tests/test_util/test_io.py
+++ tests/test_util/test_io.py
@@ -403,7 +403,8 @@ def test_iter_splitlines_one(tmp_path):
 def test_iter_splitlines_line_diff_newline(tmp_path, newline):
     path = tmp_path / "multi-line.txt"
     value = ["We have some", "text on different lines", "which load"]
-    path.write_text("\n".join(value), newline=newline)
+    with open_(path, mode="w", newline=newline) as out:
+        out.write("\n".join(value))
     # we use a very small chunk size
     got = list(iter_splitlines(path, chunk_size=5))
     assert got == value
@@ -413,7 +414,8 @@ def test_iter_splitlines_line_diff_newline(tmp_path, newline):
 def test_iter_splitlines_file_endswith_newline(tmp_path, newline):
     path = tmp_path / "multi-line.txt"
     value = ["We have some", "text on different lines", "which load"]
-    path.write_text("\n".join(value) + "\n", newline=newline)
+    with open_(path, mode="w", newline=newline) as out:
+        out.write("\n".join(value) + "\n")
     # we use a very small chunk size
     got = list(iter_splitlines(path, chunk_size=5))
     assert got == value
