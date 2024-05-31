2c5a0a616330af688efac833a6c7160810f096f1
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2024-05-31 14:56:47 +1000

    MAINT: address more review comments

--- tests/test_core/test_new_alphabet.py
+++ tests/test_core/test_new_alphabet.py
@@ -1,14 +1,7 @@
 import numpy
 import pytest
 
-from cogent3.core.new_alphabet import (
-    CharAlphabet,
-    array_to_bytes,
-    bytes_to_array,
-    consistent_words,
-    convert_alphabet,
-    get_array_type,
-)
+from cogent3.core import new_alphabet
 
 
 @pytest.mark.parametrize(
@@ -21,38 +14,38 @@ from cogent3.core.new_alphabet import (
     ),
 )
 def test_get_array_type(num, expect):
-    assert get_array_type(num) is expect
+    assert new_alphabet.get_array_type(num) is expect
 
 
 def test_get_array_type_invalid():
     with pytest.raises(NotImplementedError):
-        get_array_type(2**64)
+        new_alphabet.get_array_type(2**64)
 
 
 def test_make_char_alphabet():
-    alpha = CharAlphabet(list("TCAG"))
+    alpha = new_alphabet.CharAlphabet(list("TCAG"))
     assert len(alpha) == 4
 
 
 def test_convert_alphabet_mixed_length():
     with pytest.raises(ValueError):
-        convert_alphabet(src="ACG", dest="TGCA")
+        new_alphabet.convert_alphabet(src="ACG", dest="TGCA")
 
 
 @pytest.mark.parametrize("words", ("AACG", ["A", "CG"], ["CC", "AA"], "", (), []))
 def test_invalid_words(words):
     with pytest.raises(ValueError):
-        consistent_words(words, length=1)
+        new_alphabet.consistent_words(words, length=1)
 
 
 def test_bytes2arr():
-    b2a = bytes_to_array(b"TCAG", dtype=numpy.uint8)
+    b2a = new_alphabet.bytes_to_array(b"TCAG", dtype=numpy.uint8)
     got = b2a(b"AAGTA")
     assert (got == numpy.array([2, 2, 3, 0, 2], dtype=numpy.uint8)).all()
 
 
 def test_arr2bytes():
-    a2b = array_to_bytes(b"TCAG", dtype=numpy.uint8)
+    a2b = new_alphabet.array_to_bytes(b"TCAG", dtype=numpy.uint8)
     got = a2b(numpy.array([2, 2, 3, 0, 2], dtype=numpy.uint8))
     assert got == b"AAGTA"
 
@@ -61,60 +54,60 @@ def test_arr2bytes():
     "seq", (b"AAGTA", "AAGTA", numpy.array([2, 2, 3, 0, 2], dtype=numpy.uint8))
 )
 def test_charalphabet_to_indices(seq):
-    alpha = CharAlphabet(list("TCAG"))
+    alpha = new_alphabet.CharAlphabet(list("TCAG"))
     got = alpha.to_indices(seq)
     assert (got == numpy.array([2, 2, 3, 0, 2], dtype=numpy.uint8)).all()
 
 
 def test_charalphabet_to_indices_invalid():
-    alpha = CharAlphabet(list("TCAG"))
+    alpha = new_alphabet.CharAlphabet(list("TCAG"))
     with pytest.raises(TypeError):
         _ = alpha.to_indices(list("TCAG"))
 
 
 def test_charalphabet_from_indices():
-    alpha = CharAlphabet(list("TCAG"))
+    alpha = new_alphabet.CharAlphabet(list("TCAG"))
     got = alpha.from_indices(numpy.array([2, 2, 3, 0, 2], dtype=numpy.uint8))
     assert got == "AAGTA"
 
 
 def test_charalphabet_from_indices_invalid():
-    alpha = CharAlphabet(list("TCAG"))
+    alpha = new_alphabet.CharAlphabet(list("TCAG"))
     with pytest.raises(TypeError):
         _ = alpha.from_indices([0, 1])
 
 
 def test_convert_alphabet():
-    complement = convert_alphabet(b"ACGT", b"TGCA")
+    complement = new_alphabet.convert_alphabet(b"ACGT", b"TGCA")
     seq = b"AAGG"
     assert complement(seq) == b"TTCC"
 
 
 def test_convert_alphabet_with_delete():
-    complement = convert_alphabet(b"ACGT", b"TGCA", delete=b"-")
+    complement = new_alphabet.convert_alphabet(b"ACGT", b"TGCA", delete=b"-")
     seq = b"AAG--G"
     assert complement(seq) == b"TTCC"
 
 
 def test_charalphabet_with_gap():
-    alpha = CharAlphabet(list("TCAG-"), gap="-")
+    alpha = new_alphabet.CharAlphabet(list("TCAG-"), gap="-")
     got = alpha.from_indices(numpy.array([2, 2, 3, 0, 2], dtype=numpy.uint8))
     assert got == "AAGTA"
 
 
 def test_charalphabet_with_gap_missing():
     with pytest.raises(AssertionError):
-        _ = CharAlphabet(list("TCAG"), gap="-")
+        _ = new_alphabet.CharAlphabet(list("TCAG"), gap="-")
 
 
 def test_charalphabet_motif_len():
-    alpha = CharAlphabet(list("TCAG"))
+    alpha = new_alphabet.CharAlphabet(list("TCAG"))
     assert alpha.motif_len == 1
 
 
 @pytest.mark.parametrize("seq", ("ACGT", numpy.array([2, 1, 3, 0], dtype=numpy.uint8)))
 def test_is_valid(seq):
-    alpha = CharAlphabet(list("TCAG"))
+    alpha = new_alphabet.CharAlphabet(list("TCAG"))
     assert alpha.is_valid(seq)
 
 
@@ -122,11 +115,11 @@ def test_is_valid(seq):
     "seq", ("-ACGT", numpy.array([4, 2, 1, 3, 0], dtype=numpy.uint8))
 )
 def test_not_is_valid(seq):
-    alpha = CharAlphabet(list("TCAG"))
+    alpha = new_alphabet.CharAlphabet(list("TCAG"))
     assert not alpha.is_valid(seq)
 
 
 def test_is_valid_typerror():
-    alpha = CharAlphabet(list("TCAG"))
+    alpha = new_alphabet.CharAlphabet(list("TCAG"))
     with pytest.raises(TypeError):
         _ = alpha.is_valid(list("ACGT"))
