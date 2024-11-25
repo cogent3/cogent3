41db6b47b580950bad2cc4a52f0a37277ae40d74
Yapeng Lang <u7181074@gmail.com>
2024-11-26 09:39:50 +1100

    DEV: delete name_nodes and its usage

--- src/cogent3/__init__.py
+++ src/cogent3/__init__.py
@@ -773,7 +773,6 @@ def make_tree(
     tip_names=None,
     format=None,
     underscore_unmunge=False,
-    name_nodes=False,
 ):
     """Initialises a tree.
 
@@ -789,8 +788,6 @@ def make_tree(
     underscore_unmunge : bool
         replace underscores with spaces in all names read, i.e. "sp_name"
         becomes "sp name"
-    name_nodes: bool
-        whether to name unnamed nodes
 
     Notes
     -----
@@ -819,10 +816,6 @@ def make_tree(
         tree = parser(treestring, tree_builder)
     if not tree.name_loaded:
         tree.name = "root"
-
-    # ensure all nodes have names if name_nodes is True
-    if name_nodes:
-        tree.name_unnamed_nodes()
 
     return tree
 
