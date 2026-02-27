10a51989290adbc2bba3b3835382f75ed5848211
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2026-02-27 16:50:17 +1100

    ENH: adding support for app _citations
    [NEW] added citeable as a dependency, it's a simple library
        that depends only on the standard library
    
    [NEW] created a cogent3 citation object in a new private
        _citations.py module

---
+++ src/cogent3/app/_citations.py
@@ -0,0 +1,24 @@
+from citeable import Software
+
+cite_cogent3 = Software(
+    key="cogent3",
+    author=[
+        "Huttley, Gavin",
+        "Caley, Katherine",
+        "Fotovat, Nabi",
+        "Ma, Stephen Ka-Wah",
+        "Koh, Moses",
+        "Morris, Richard",
+        "McArthur, Robert",
+        "McDonald, Daniel",
+        "Jaya, Fred",
+        "Maxwell, Peter",
+        "Martini, James",
+        "La, Thomas",
+        "Lang, Yapeng",
+    ],
+    title="{cogent3}: making sense of sequence",
+    year=2025,
+    doi="10.5281/zenodo.16519079",
+    url="https://cogent3.org",
+)
